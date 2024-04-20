feafaa6c047e097def7b28c35ca22a51158d4852
Akshay Narisetti <akshaynarisettia@gmail.com>
2024-04-20 21:33:06 +0530

    UI Update for Get Started Screen

--- apps/AppWithWearable/lib/onboarding/welcome/welcome_widget.dart
+++ apps/AppWithWearable/lib/onboarding/welcome/welcome_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '/components/logo/logo_main/logo_main_widget.dart';
@@ -7,8 +9,9 @@ import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import '/pages/ble/blur_bot/blur_bot_widget.dart';
 import 'welcome_model.dart';
-
-export 'welcome_model.dart';
+import 'package:lottie/lottie.dart';
+import 'dart:math';
+import 'dart:ui';
 
 class WelcomeWidget extends StatefulWidget {
   const WelcomeWidget({super.key});
@@ -17,8 +20,10 @@ class WelcomeWidget extends StatefulWidget {
   State<WelcomeWidget> createState() => _WelcomeWidgetState();
 }
 
-class _WelcomeWidgetState extends State<WelcomeWidget> {
+class _WelcomeWidgetState extends State<WelcomeWidget> with SingleTickerProviderStateMixin {
   late WelcomeModel _model;
+  late AnimationController _animationController;
+  late Animation<double> _animation;
 
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
@@ -27,12 +32,28 @@ class _WelcomeWidgetState extends State<WelcomeWidget> {
     super.initState();
     _model = createModel(context, () => WelcomeModel());
 
+    // Hide the status bar
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+
+    // Initialize the animation controller and animation
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    )..repeat(reverse: true);
+
+    _animation = Tween<double>(begin: pi / 8.2, end: pi / 9).animate(_animationController);
+
     WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
   }
 
   @override
   void dispose() {
     _model.dispose();
+    _animationController.dispose();
+
+    // Show the status bar again when the widget is disposed
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
 
     super.dispose();
   }
@@ -46,168 +67,139 @@ class _WelcomeWidgetState extends State<WelcomeWidget> {
       child: Scaffold(
         key: scaffoldKey,
         backgroundColor: FlutterFlowTheme.of(context).primary,
-        body: SafeArea(
-          top: true,
-          child: Stack(
-            children: [
-              wrapWithModel(
-                model: _model.blurBotModel,
-                updateCallback: () => setState(() {}),
-                child: BlurBotWidget(),
-              ),
-              Align(
-                alignment: AlignmentDirectional(0.0, 1.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Align(
-                      alignment: AlignmentDirectional(0.0, 0.0),
-                      child: wrapWithModel(
-                        model: _model.logoMainModel,
-                        updateCallback: () => setState(() {}),
-                        child: LogoMainWidget(),
-                      ),
+        body: Stack(
+          children: [
+            wrapWithModel(
+              model: _model.blurBotModel,
+              updateCallback: () => setState(() {}),
+              child: BlurBotWidget(),
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: AnimatedBuilder(
+                    animation: _animation,
+                    builder: (context, child) {
+                      return Transform.rotate(
+                        angle: _animation.value,
+                        child: Image.asset(
+                          'assets/images/hero_image.png',
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Align(
+                  alignment: AlignmentDirectional(0.0, 1.0),
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(30.0, 80.0, 30.0, 40.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Friend helps you remember everything",
+                          textAlign: TextAlign.start,
+                          style: FlutterFlowTheme.of(context).bodyMedium.override(
+                                fontFamily: 'SF Pro Display',
+                                color: Colors.white,
+                                fontSize: 29.0,
+                                letterSpacing: 0.0,
+                                fontWeight: FontWeight.w900,
+                                useGoogleFonts:
+                                    GoogleFonts.asMap().containsKey('SF Pro Display'),
+                                lineHeight: 1.2,
+                              ),
+                        ),
+                        SizedBox(height: 10.0),
+                        Text(
+                          "Your Personal growth journey with AI that listens to your every word",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 101, 101, 101),
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w700,
+                            height: 1.5,
+                          ),
+                        ),
+                      ],
                     ),
-                    Padding(
-                      padding:
-                          EdgeInsetsDirectional.fromSTEB(0.0, 20.0, 0.0, 0.0),
-                      child: Text(
-                        'Friend',
-                        style: FlutterFlowTheme.of(context).bodyMedium.override(
-                              fontFamily: 'SF Pro Display',
-                              fontSize: 24.0,
-                              letterSpacing: 0.0,
-                              fontWeight: FontWeight.bold,
-                              useGoogleFonts: GoogleFonts.asMap()
-                                  .containsKey('SF Pro Display'),
-                            ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(40.0),
-                      child: Text(
-                        'The AI companion that helps you remember everything.',
-                        textAlign: TextAlign.center,
-                        style: FlutterFlowTheme.of(context).bodyMedium.override(
-                              fontFamily: 'SF Pro Display',
-                              color: Color(0xCCF7F4F4),
-                              fontSize: 20.0,
-                              letterSpacing: 0.0,
-                              fontWeight: FontWeight.w500,
-                              useGoogleFonts: GoogleFonts.asMap()
-                                  .containsKey('SF Pro Display'),
-                              lineHeight: 1.5,
-                            ),
-                      ),
-                    ),
-                    Padding(
-                      padding:
-                          EdgeInsetsDirectional.fromSTEB(0.0, 20.0, 0.0, 20.0),
-                      child: InkWell(
-                        splashColor: Colors.transparent,
-                        focusColor: Colors.transparent,
-                        hoverColor: Colors.transparent,
-                        highlightColor: Colors.transparent,
-                        onDoubleTap: () async {
-                          context.goNamed('PermissionPage');
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 20.0),
+                  child: Material(
+                    elevation: 4.0,
+                    shape: CircleBorder(),
+                    child: InkWell(
+                      splashColor: Colors.transparent,
+                      focusColor: Colors.transparent,
+                      hoverColor: Colors.transparent,
+                      highlightColor: Colors.transparent,
+                      mouseCursor: SystemMouseCursors.click,
+                      onTap: () async {
+                        context.goNamed('PermissionPage');
+                      },
+                      child: ShaderMask(
+                        shaderCallback: (Rect bounds) {
+                          return RadialGradient(
+                            center: Alignment.center,
+                            radius: 0.45,
+                            colors: [
+                              Colors.white,
+                              Colors.white.withOpacity(0.0)
+                            ],
+                            stops: [0.9, 1.0],
+                          ).createShader(bounds);
                         },
-                        child: FFButtonWidget(
-                          onPressed: () async {
-                            context.pushNamed('PermissionPage');
-                          },
-                          text: 'Get Started',
-                          options: FFButtonOptions(
-                            height: 60.0,
-                            padding: EdgeInsetsDirectional.fromSTEB(
-                                40.0, 0.0, 40.0, 0.0),
-                            iconPadding: EdgeInsetsDirectional.fromSTEB(
-                                0.0, 0.0, 0.0, 0.0),
-                            color: FlutterFlowTheme.of(context).secondary,
-                            textStyle: FlutterFlowTheme.of(context)
-                                .titleSmall
-                                .override(
-                                  fontFamily: 'SF Pro Display',
-                                  color: FlutterFlowTheme.of(context).primary,
-                                  fontSize: 20.0,
-                                  letterSpacing: 0.0,
-                                  fontWeight: FontWeight.bold,
-                                  useGoogleFonts: GoogleFonts.asMap()
-                                      .containsKey('SF Pro Display'),
-                                ),
-                            elevation: 3.0,
-                            borderSide: BorderSide(
-                              color: FlutterFlowTheme.of(context).secondary,
-                              width: 1.0,
+                        child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                          ),
+                          child: Transform(
+                            transform: Matrix4.identity()
+                              ..setEntry(3, 2, 0.001) // Perspective
+                              ..rotateX(0.1) // Rotation around X-axis
+                              ..rotateY(-0.1), // Rotation around Y-axis
+                            alignment: Alignment.center,
+                            child: ClipOval(
+                              child: Stack(
+                                children: [
+                                  Opacity(
+                                    opacity: 0.8,
+                                    child: Lottie.asset(
+                                      'assets/images/grid_wave.json',
+                                      width: 150,
+                                      height: 150,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                  Center(
+                                    child: Transform.rotate(
+                                      angle: -pi / 4,
+                                      child: Icon(
+                                        Icons.arrow_forward,
+                                        size: 60,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
-                            borderRadius: BorderRadius.circular(30.0),
                           ),
                         ),
                       ),
                     ),
-                    Padding(
-                      padding:
-                          EdgeInsetsDirectional.fromSTEB(40.0, 20.0, 40.0, 4.0),
-                      child: InkWell(
-                        splashColor: Colors.transparent,
-                        focusColor: Colors.transparent,
-                        hoverColor: Colors.transparent,
-                        highlightColor: Colors.transparent,
-                        onTap: () async {
-                          await launchURL(
-                              'https://samaprivacypolicy.notion.site/samaprivacypolicy/Sama-AI-Privacy-Policy-bfbbee90f18d4b8b9a0111d2d62cca54');
-                        },
-                        child: Text(
-                          'By clicking continue, I agree to the Privacy Policy.',
-                          textAlign: TextAlign.center,
-                          style:
-                              FlutterFlowTheme.of(context).bodyMedium.override(
-                                    fontFamily: 'SF Pro Display',
-                                    fontSize: 12.0,
-                                    letterSpacing: 0.0,
-                                    fontWeight: FontWeight.w500,
-                                    decoration: TextDecoration.underline,
-                                    useGoogleFonts: GoogleFonts.asMap()
-                                        .containsKey('SF Pro Display'),
-                                    lineHeight: 1.5,
-                                  ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding:
-                          EdgeInsetsDirectional.fromSTEB(40.0, 0.0, 40.0, 40.0),
-                      child: InkWell(
-                        splashColor: Colors.transparent,
-                        focusColor: Colors.transparent,
-                        hoverColor: Colors.transparent,
-                        highlightColor: Colors.transparent,
-                        onTap: () async {
-                          await launchURL(
-                              'https://coda.io/d/_dNtSv1z5gNh/END-USER-LICENSE-AGREEMENT_suK7N');
-                        },
-                        child: Text(
-                          'I also agree to the Terms of Service (EULA).',
-                          textAlign: TextAlign.center,
-                          style:
-                              FlutterFlowTheme.of(context).bodyMedium.override(
-                                    fontFamily: 'SF Pro Display',
-                                    fontSize: 12.0,
-                                    letterSpacing: 0.0,
-                                    fontWeight: FontWeight.w500,
-                                    decoration: TextDecoration.underline,
-                                    useGoogleFonts: GoogleFonts.asMap()
-                                        .containsKey('SF Pro Display'),
-                                    lineHeight: 1.5,
-                                  ),
-                        ),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
       ),
     );
--- apps/AppWithWearable/lib/pages/ble/blur_bot/blur_bot_widget.dart
+++ apps/AppWithWearable/lib/pages/ble/blur_bot/blur_bot_widget.dart
@@ -1,12 +1,10 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'blur_bot_model.dart';
-
 export 'blur_bot_model.dart';
+import 'package:lottie/lottie.dart';
+
 
 class BlurBotWidget extends StatefulWidget {
   const BlurBotWidget({super.key});
@@ -28,14 +26,12 @@ class _BlurBotWidgetState extends State<BlurBotWidget> {
   void initState() {
     super.initState();
     _model = createModel(context, () => BlurBotModel());
-
     WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
   }
 
   @override
   void dispose() {
     _model.maybeDispose();
-
     super.dispose();
   }
 
@@ -44,55 +40,10 @@ class _BlurBotWidgetState extends State<BlurBotWidget> {
     return Container(
       width: double.infinity,
       height: double.infinity,
-      decoration: BoxDecoration(),
-      child: ClipRect(
-        child: ImageFiltered(
-          imageFilter: ImageFilter.blur(
-            sigmaX: 48.0,
-            sigmaY: 48.0,
-          ),
-          child: Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: BoxDecoration(
-              color: FlutterFlowTheme.of(context).primary,
-            ),
-            child: Container(
-              width: double.infinity,
-              height: double.infinity,
-              child: Stack(
-                alignment: AlignmentDirectional(0.0, 0.0),
-                children: [
-                  Align(
-                    alignment: AlignmentDirectional(1.0, 1.0),
-                    child: Container(
-                      width: 240.0,
-                      height: 240.0,
-                      decoration: BoxDecoration(
-                        color: Color(0xFFFE480D),
-                        shape: BoxShape.circle,
-                      ),
-                    ),
-                  ),
-                  Align(
-                    alignment: AlignmentDirectional(-1.0, 1.0),
-                    child: Padding(
-                      padding:
-                          EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 40.0),
-                      child: Container(
-                        width: 200.0,
-                        height: 200.0,
-                        decoration: BoxDecoration(
-                          color: Color(0xFF97ADAA),
-                          shape: BoxShape.circle,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/background.png'),
+          fit: BoxFit.cover,
         ),
       ),
     );
