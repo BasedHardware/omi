fad0b47806818fe29dbd59fcc3bb5011c1adc87e
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-04-27 22:49:01 -0600

    refactor chat

--- apps/mobile/server/functions/chat/main.py
+++ apps/mobile/server/functions/chat/main.py
@@ -1,58 +1,71 @@
 import os
-import certifi
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
+headers = {"Access-Control-Allow-Origin": "*"}
 load_dotenv()
-cred = None
+
+# Google clouds file structure is different
 if os.getenv('LOCAL_DEV') == 'True':
     from .ChatService import ChatService
 else:
-    from apps.mobile.server.functions.chat.ChatService import ChatService
+    from ChatService import ChatService
 
-# MongoDB URI
 mongo_uri = os.getenv('MONGO_URI')
-# Create a new MongoClient and connect to the server
-client = MongoClient(mongo_uri, tlsCAFile=certifi.where())
-
+client = MongoClient(mongo_uri)
 db = client['paxxium']
-
 chat_service = ChatService(db)
 
-def chat(request):
-    response = {}
-    if request.method == "OPTIONS":
-        headers = {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, OPTIONS, DELETE, PUT, PATCH",
-            "Access-Control-Allow-Headers": "Authorization, Content-Type",
-            "Access-Control-Max-Age": "3600",
-        }
+def cors_preflight_response():
+    cors_headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS, DELETE, PUT, PATCH",
+        "Access-Control-Allow-Headers": "Authorization, Content-Type, Project-ID",
+        "Access-Control-Max-Age": "3600",
+    }
+    return ("", 204, cors_headers)
 
-        return ("", 204, headers)
-    headers = {"Access-Control-Allow-Origin": "*"}
+def handle_fetch_chats(request):
     uid = request.headers.get('uid')
-    if request.path in ('/', '/chat'):
-        chat_data_list = chat_service.get_all_chats(uid)
-        return (chat_data_list, 200, headers)
-    
-    if request.path in ('/create', '/chat/create'):
-        data = request.get_json()
-        chat_name = data['chatName']
-        agent_model = data['agentModel']
+    # Get all moments from the database
+    chat_data_list = chat_service.get_all_chats(uid)
+    return chat_data_list
 
-        chat_id = chat_service.create_chat_in_db(uid, chat_name, agent_model, )
-        chat_data = {
-            'chatId': chat_id,
-            'chat_name': chat_name,
-            'agent_model': agent_model,
-        }
+def handle_add_chat(request):
+    data = request.get_json()
+    uid = request.headers.get('uid')
+    chat_name = data['chatName']
+    agent_model = data['agentModel']
+
+    chat_id = chat_service.create_chat_in_db(uid, chat_name, agent_model, )
+    chat_data = {
+        'chatId': chat_id,
+        'chat_name': chat_name,
+        'agent_model': agent_model,
+    }
+    return chat_data
+
+def handle_delete_chat(request):
+    data = request.get_json()
+    uid = request.headers.get('uid')
+    chat_id = data['chatId']
+    chat_service.delete_conversation(uid, chat_id)
+    return 'Conversation deleted'
+
+def chat(request):
+    if request.method == "OPTIONS":
+        return cors_preflight_response()
+    
+    if request.method == 'GET':
+        chat_data = handle_fetch_chats(request)
         return (chat_data, 200, headers)
     
-    if request.path in ('/delete', '/chat/delete'):
-        data = request.get_json()
-        chat_id = data['chatId']
-        chat_service.delete_conversation(uid, chat_id)
+    if request.method == 'POST':
+        chat_data = handle_add_chat(request)
+        return (chat_data, 200, headers)
+    
+    if request.method == 'DELETE':
+        handle_delete_chat(request)
         return ('Conversation deleted', 200, headers)
 
     
