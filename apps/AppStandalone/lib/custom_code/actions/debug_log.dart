6de1ee145fac0a2c2a4682e8cb655738de2c3124
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-04-28 09:04:47 -0500

    added debug_log.dart fix merge conflict

---
+++ apps/AppStandalone/lib/custom_code/actions/debug_log.dart
@@ -0,0 +1,17 @@
+// Automatic FlutterFlow imports
+import '/backend/backend.dart';
+import '/backend/schema/structs/index.dart';
+import '/backend/schema/enums/enums.dart';
+import '/actions/actions.dart' as action_blocks;
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'index.dart'; // Imports other custom actions
+import '/flutter_flow/custom_functions.dart'; // Imports custom functions
+import 'package:flutter/material.dart';
+// Begin custom action code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+Future debugLog(String value) async {
+  // Add your function code here!
+  print("DEBUGLOG" + value);
+}
