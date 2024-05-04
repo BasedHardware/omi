848b4084242a76d76a73ef79ce5b4441a9ef6bf5
Viktar Hushchynski <viktar.hushchynski@gmail.com>
2024-05-04 00:00:29 -0400

    feat(firmware): Conditional codec compilation
    feat(firmware): Address warning with utils

--- firmware/src/codec.c
+++ firmware/src/codec.c
@@ -4,7 +4,7 @@
 #include "config.h"
 #include "utils.h"
 #ifdef CODEC_OPUS
-#include "lib/pus-1.2.1/opus.h"
+#include "lib/opus-1.2.1/opus.h"
 #endif
 
 //
--- firmware/src/utils.h
+++ firmware/src/utils.h
@@ -16,17 +16,6 @@
         return -1;                                                 \
     }
 
-static const char *phy2str(uint8_t phy)
-{
-	switch (phy) {
-	case 0: return "No packets";
-	case BT_GAP_LE_PHY_1M: return "LE 1M";
-	case BT_GAP_LE_PHY_2M: return "LE 2M";
-	case BT_GAP_LE_PHY_CODED: return "LE Coded";
-	default: return "Unknown";
-	}
-}
-
 // #define WAIT_LOG k_sleep(K_MSEC(200));
 // #define WAIT_LOG do {} while(0);
 // #define WAIT_LOG z_impl_log_process()
