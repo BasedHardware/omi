2b39f53f8865299e9f9426b0e0cd8b1ecaa20106
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-05-08 16:29:00 -0600

    cleaned up comments

--- apps/mobile/reactNative/client/src/contexts/SnackbarContext.js
+++ apps/mobile/reactNative/client/src/contexts/SnackbarContext.js
@@ -3,19 +3,16 @@ import { useState, createContext } from 'react';
 export const SnackbarContext = createContext();
 
 export const SnackbarProvider = ({ children }) => {
-    // Snackbar state
     const [snackbarInfo, setSnackbarInfo] = useState({
         open: false,
         message: '',
         severity: 'info',
     });
 
-    // Function to show snackbar
     const showSnackbar = (message, severity) => {
         setSnackbarInfo({ open: true, message, severity });
     };
 
-    // Function to hide snackbar
     const hideSnackbar = () => {
         setSnackbarInfo({ ...snackbarInfo, open: false });
     };
