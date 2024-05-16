12eda199cf68898be45d2d311b42183e50145d12
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-05-15 19:23:14 -0500

    improved websocket management + not triggering websocket closed when device disconnected

--- apps/AppWithWearable/lib/utils/sentry_log.dart
+++ apps/AppWithWearable/lib/utils/sentry_log.dart
@@ -5,15 +5,13 @@ import 'package:sentry_flutter/sentry_flutter.dart';
 final log = Logger('SentryLogger');
 
 addEventToContext(String event) {
-  // TODO: instead of doing this, include a way to add single events that can be traced and combined
-  // click x, bluetooth disconnected, ws disconnected, click, ws connected ... etc..
   debugPrint(event);
   log.info(event);
-  // Sentry.configureScope((scope) {
-  //   var deepgramData = (scope.contexts['events'] ?? {});
-  //   var currentEvents = deepgramData['values'] ?? [];
-  //   currentEvents.add(event);
-  //   deepgramData['values'] = currentEvents;
-  //   scope.setContexts('events', deepgramData);
-  // });
+  Sentry.configureScope((scope) {
+    var deepgramData = (scope.contexts['events'] ?? {});
+    var currentEvents = deepgramData['values'] ?? [];
+    currentEvents.add(event);
+    deepgramData['values'] = currentEvents;
+    scope.setContexts('events', deepgramData);
+  });
 }
