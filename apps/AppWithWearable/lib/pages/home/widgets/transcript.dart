618da45dd1f5b984960180ede3925da49dfbe260
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-05-17 00:56:32 -0500

    memory creation when on the background, doesn't trigger tons of Snackbars

--- apps/AppWithWearable/lib/pages/home/widgets/transcript.dart
+++ apps/AppWithWearable/lib/pages/home/widgets/transcript.dart
@@ -258,7 +258,7 @@ class TranscriptWidgetState extends State<TranscriptWidget> with WidgetsBindingO
 
   _initiateMemoryCreationTimer() {
     _memoryCreationTimer?.cancel();
-    _memoryCreationTimer = Timer(const Duration(seconds: 120), () async {
+    _memoryCreationTimer = Timer(const Duration(seconds: 5), () async {
       setState(() {
         memoryCreating = true;
       });
