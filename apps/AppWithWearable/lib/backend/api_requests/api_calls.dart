fa541628089aea58e4d264c553739de11a63cb7e
Akshay Narisetti <akshaynarisettia@gmail.com>
2024-06-03 09:12:38 +0300

    Parallel Requests to GPT for plugins

--- apps/AppWithWearable/lib/backend/api_requests/api_calls.dart
+++ apps/AppWithWearable/lib/backend/api_requests/api_calls.dart
@@ -90,9 +90,11 @@ Future<dynamic> gptApiCall({
     body = jsonEncode(bodyData);
   }
 
-  var response = await makeApiCall(url: url, headers: headers, body: body, method: 'POST');
+  var response =
+      await makeApiCall(url: url, headers: headers, body: body, method: 'POST');
   return extractContentFromResponse(response,
-      isEmbedding: urlSuffix == 'embeddings', isFunctionCalling: tools.isNotEmpty);
+      isEmbedding: urlSuffix == 'embeddings',
+      isFunctionCalling: tools.isNotEmpty);
 }
 
 Future<String> executeGptPrompt(String? prompt) async {
@@ -125,22 +127,16 @@ _getPrevMemoriesStr(List<MemoryRecord> previousMemories) {
       : '';
 }
 
-Future<Structured> generateTitleAndSummaryForMemory(String transcript, List<MemoryRecord> previousMemories) async {
-  if (transcript.isEmpty || transcript.split(' ').length < 7) return Structured(actionItems: [], pluginsResponse: []);
+Future<Structured> generateTitleAndSummaryForMemory(
+    String transcript, List<MemoryRecord> previousMemories) async {
+  if (transcript.isEmpty || transcript.split(' ').length < 7)
+    return Structured(actionItems: [], pluginsResponse: []);
   final languageCode = SharedPreferencesUtil().recordingsLanguage;
   final pluginsEnabled = SharedPreferencesUtil().pluginsEnabled;
   // final plugin = SharedPreferencesUtil().pluginsList.firstWhereOrNull((e) => pluginsEnabled.contains(e.id));
   final pluginsList = SharedPreferencesUtil().pluginsList;
-  final enabledPlugins = pluginsList.where((e) => pluginsEnabled.contains(e.id)).toList();
-  List<String> responses = [];
-
-  for (var plugin in enabledPlugins) {
-    // Execute the GPT prompt for the current plugin and wait for the result.
-    String response = await executeGptPrompt('''Your are ${plugin.name}, ${plugin.prompt}, Conversation: ```${transcript.trim()} ${_getPrevMemoriesStr(previousMemories)}, you must start your output with heading as ${plugin.name}, you must only use valid english alphabets and words for your response, use pain text without markdown```. '''); 
-
-    // Add the returned response to the list of responses.
-    responses.add(response);
-  }
+  final enabledPlugins =
+      pluginsList.where((e) => pluginsEnabled.contains(e.id)).toList();
 
   var prompt =
       '''Based on the following recording transcript of a conversation, provide structure and clarity to the memory in JSON according rules stated below.
@@ -169,13 +165,26 @@ Future<Structured> generateTitleAndSummaryForMemory(String transcript, List<Memo
           .replaceAll('    ', '')
           .trim();
   // debugPrint(prompt);
-  var structured = extractJson(await executeGptPrompt(prompt));
-  return Structured.fromJson(jsonDecode(structured)..['pluginsResponse'] = responses);
+  List<Future<String>> pluginPrompts = enabledPlugins.map((plugin) async {
+    String response = await executeGptPrompt(
+        '''Your are ${plugin.name}, ${plugin.prompt}, Conversation: ```${transcript.trim()} ${_getPrevMemoriesStr(previousMemories)}, you must start your output with heading as ${plugin.name}, you must only use valid english alphabets and words for your response, use pain text without markdown```. ''');
+    return response;
+  }).toList();
+
+  Future<List<String>> allPluginResponses = Future.wait(pluginPrompts);
+
+  var structuredResponse = extractJson(await executeGptPrompt(prompt));
+
+  List<String> responses = await allPluginResponses;
+
+  return Structured.fromJson(
+      jsonDecode(structuredResponse)..['pluginsResponse'] = responses);
 }
 
 Future<String> adviseOnCurrentConversation(String transcript) async {
   if (transcript.isEmpty) return '';
-  if (transcript.split(' ').length < 20) return ''; // not enough to extract something out of it
+  if (transcript.split(' ').length < 20)
+    return ''; // not enough to extract something out of it
   // if (transcript.contains('Speaker 0') &&
   //     (!transcript.contains('Speaker 1') && !transcript.contains('Speaker 2') && !transcript.contains('Speaker 3'))) {
   //   return '';
@@ -221,13 +230,19 @@ Future<String> requestSummary(List<MemoryRecord> memories) async {
 }
 
 Future<List<double>> getEmbeddingsFromInput(String input) async {
-  var vector = await gptApiCall(model: 'text-embedding-3-large', urlSuffix: 'embeddings', contentToEmbed: input);
-  return vector.map<double>((item) => double.tryParse(item.toString()) ?? 0.0).toList();
+  var vector = await gptApiCall(
+      model: 'text-embedding-3-large',
+      urlSuffix: 'embeddings',
+      contentToEmbed: input);
+  return vector
+      .map<double>((item) => double.tryParse(item.toString()) ?? 0.0)
+      .toList();
 }
 
 // ------
 
-Future<String?> determineRequiresContext(String lastMessage, List<Message> messages) async {
+Future<String?> determineRequiresContext(
+    String lastMessage, List<Message> messages) async {
   var tools = [
     {
       "type": "function",
@@ -270,18 +285,21 @@ Future<String?> determineRequiresContext(String lastMessage, List<Message> messa
   return null;
 }
 
-Future<dynamic> pineconeApiCall({required String urlSuffix, required String body}) async {
+Future<dynamic> pineconeApiCall(
+    {required String urlSuffix, required String body}) async {
   var url = '${Env.pineconeIndexUrl}/$urlSuffix';
   final headers = {
     'Api-Key': Env.pineconeApiKey,
     'Content-Type': 'application/json',
   };
-  var response = await makeApiCall(url: url, headers: headers, body: body, method: 'POST');
+  var response =
+      await makeApiCall(url: url, headers: headers, body: body, method: 'POST');
   var responseBody = jsonDecode(response?.body ?? '{}');
   return responseBody;
 }
 
-Future<bool> createPineconeVectors(List<String> memoriesId, List<List<double>> vectors) async {
+Future<bool> createPineconeVectors(
+    List<String> memoriesId, List<List<double>> vectors) async {
   var body = jsonEncode({
     'vectors': memoriesId.mapIndexed((index, id) {
       return {
@@ -296,12 +314,14 @@ Future<bool> createPineconeVectors(List<String> memoriesId, List<List<double>> v
     }).toList(),
     'namespace': Env.pineconeIndexNamespace
   });
-  var responseBody = await pineconeApiCall(urlSuffix: 'vectors/upsert', body: body);
+  var responseBody =
+      await pineconeApiCall(urlSuffix: 'vectors/upsert', body: body);
   debugPrint('createVectorPinecone response: $responseBody');
   return true;
 }
 
-Future<bool> createPineconeVector(String? memoryId, List<double>? vectorList) async {
+Future<bool> createPineconeVector(
+    String? memoryId, List<double>? vectorList) async {
   var body = jsonEncode({
     'vectors': [
       {
@@ -316,7 +336,8 @@ Future<bool> createPineconeVector(String? memoryId, List<double>? vectorList) as
     ],
     'namespace': Env.pineconeIndexNamespace
   });
-  var responseBody = await pineconeApiCall(urlSuffix: 'vectors/upsert', body: body);
+  var responseBody =
+      await pineconeApiCall(urlSuffix: 'vectors/upsert', body: body);
   debugPrint('createVectorPinecone response: $responseBody');
   return (responseBody['upserted_count'] ?? 0) > 0;
 }
@@ -334,7 +355,10 @@ Future<List<String>> queryPineconeVectors(List<double>? vectorList) async {
   });
   var responseBody = await pineconeApiCall(urlSuffix: 'query', body: body);
   debugPrint(responseBody.toString());
-  return (responseBody['matches'])?.map<String>((e) => e['id'].toString()).toList() ?? [];
+  return (responseBody['matches'])
+          ?.map<String>((e) => e['id'].toString())
+          .toList() ??
+      [];
 }
 
 Future<bool> deleteVector(String memoryId) async {
@@ -349,7 +373,8 @@ Future<bool> deleteVector(String memoryId) async {
 
 Future<List<Plugin>> retrievePlugins() async {
   var response = await makeApiCall(
-      url: 'https://raw.githubusercontent.com/BasedHardware/Friend/main/community-plugins.json',
+      url:
+          'https://raw.githubusercontent.com/BasedHardware/Friend/main/community-plugins.json',
       headers: {},
       body: '',
       method: 'GET');
@@ -365,13 +390,15 @@ Future<List<Plugin>> retrievePlugins() async {
 
 // TODO: update vectors when fields updated
 
-Future<List<TranscriptSegment>> transcribeAudioFile(File file, String uid) async {
+Future<List<TranscriptSegment>> transcribeAudioFile(
+    File file, String uid) async {
   var request = http.MultipartRequest(
     'POST',
     Uri.parse(
         '${Env.customTranscriptApiBaseUrl}transcribe?language=${SharedPreferencesUtil().recordingsLanguage}&uid=$uid'),
   );
-  request.files.add(await http.MultipartFile.fromPath('file', file.path, filename: basename(file.path)));
+  request.files.add(await http.MultipartFile.fromPath('file', file.path,
+      filename: basename(file.path)));
 
   try {
     var streamedResponse = await request.send();
@@ -383,7 +410,8 @@ Future<List<TranscriptSegment>> transcribeAudioFile(File file, String uid) async
       return TranscriptSegment.fromJsonList(data);
     } else {
       debugPrint('Failed to upload file. Status code: ${response.statusCode}');
-      throw Exception('Failed to upload file. Status code: ${response.statusCode}');
+      throw Exception(
+          'Failed to upload file. Status code: ${response.statusCode}');
     }
   } catch (e) {
     debugPrint('An error occurred transcribeAudioFile: $e');
