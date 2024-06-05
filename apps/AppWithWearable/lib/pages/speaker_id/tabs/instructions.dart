dca88bfc2460443929ddb79cc481e88d75321f09
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-06-05 09:52:45 -0500

    improved UI + initial recording tab wave animation

--- apps/AppWithWearable/lib/pages/speaker_id/tabs/instructions.dart
+++ apps/AppWithWearable/lib/pages/speaker_id/tabs/instructions.dart
@@ -10,16 +10,67 @@ class InstructionsTab extends StatefulWidget {
 class _InstructionsTabState extends State<InstructionsTab> {
   @override
   Widget build(BuildContext context) {
-    return const Column(children: [
-      SizedBox(height: 48),
-      Text(
-        'Setup Your Speaker Profile',
-        style: TextStyle(color: Colors.white, fontSize: 20),
-      ),
-      SizedBox(height: 24),
-      Text('Instructions ....', style: TextStyle(color: Colors.white),),
-      SizedBox(height: 24),
-
-    ]);
+    return ListView(
+      children: [
+        const SizedBox(height: 48),
+        const Center(
+          child: Text(
+            'Setup Your Speech Profile',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        const SizedBox(height: 24),
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Text(
+              'You will record samples of your voice to create a speech profile. This will help the device recognize your voice.',
+              style: TextStyle(color: Colors.grey.shade300, fontSize: 15),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+        const SizedBox(height: 48),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Text(
+            'Instructions:',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '1. Wear the device and make sure it is connected to the app 🛜',
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                '2. Make sure you are in a very quiet environment 🤫',
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                '3. Repeat the phrases that will be shown on the screen 🗣️',
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 24),
+      ],
+    );
   }
 }
