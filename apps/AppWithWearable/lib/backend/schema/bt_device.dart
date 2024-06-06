147e9bcb56f312e9bcad0956cbbc453c9c717e4d
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-06-06 12:58:43 -0500

    minor fixes + comments

--- apps/AppWithWearable/lib/backend/schema/bt_device.dart
+++ apps/AppWithWearable/lib/backend/schema/bt_device.dart
@@ -1,70 +1,27 @@
 // ignore_for_file: unnecessary_getters_setters
 
-// TODO: go
 class BTDeviceStruct {
+  String name;
+  String id;
+  int? rssi;
+
   BTDeviceStruct({
-    String? name,
-    String? id,
-    int? rssi,
-  })  : _name = name,
-        _id = id,
-        _rssi = rssi;
+    required this.id,
+    required this.name,
+    this.rssi,
+  });
 
-  // "name" field.
-  String? _name;
-
-  String get name => _name ?? '';
-
-  set name(String? val) => _name = val;
-
-  bool hasName() => _name != null;
-
-  // "id" field.
-  String? _id;
-
-  String get id => _id ?? '';
-
-  set id(String? val) => _id = val;
-
-  bool hasId() => _id != null;
-
-  // "rssi" field.
-  int? _rssi;
-
-  int get rssi => _rssi ?? 0;
-
-  set rssi(int? val) => _rssi = val;
-
-  void incrementRssi(int amount) => _rssi = rssi + amount;
-
-  bool hasRssi() => _rssi != null;
-
-  static BTDeviceStruct fromMap(Map<String, dynamic> data) => BTDeviceStruct(
-        name: data['name'] as String?,
-        id: data['id'] as String?,
-        rssi: castToType<int>(data['rssi']),
-      );
-
-  static BTDeviceStruct? maybeFromMap(dynamic data) =>
-      data is Map ? BTDeviceStruct.fromMap(data.cast<String, dynamic>()) : null;
-
-  Map<String, dynamic> toMap() => {
-        'name': _name,
-        'id': _id,
-        'rssi': _rssi,
-      }.withoutNulls;
-
-  @override
-  String toString() => 'BTDeviceStruct(${toMap()})';
-}
-
-BTDeviceStruct createBTDeviceStruct({
-  String? name,
-  String? id,
-  int? rssi,
-}) =>
-    BTDeviceStruct(
-      name: name,
-      id: id,
-      rssi: rssi,
+  // Factory constructor to create a new Message instance from a map
+  factory BTDeviceStruct.fromJson(Map<String, dynamic> json) {
+    return BTDeviceStruct(
+      id: json['text'] as String,
+      name: json['type'] as String,
+      rssi: json['id'] as int,
     );
+  }
+
+  // Method to convert a Message instance into a map
+  Map<String, dynamic> toJson() {
+    return {'id': id, 'name': name, 'rssi': rssi};
+  }
+}
