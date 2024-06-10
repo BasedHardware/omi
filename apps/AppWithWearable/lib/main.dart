bd603f8bcc468f10e1b883a73d39b6b7eacc85f3
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-06-09 20:18:00 -0700

    -

--- apps/AppWithWearable/lib/main.dart
+++ apps/AppWithWearable/lib/main.dart
@@ -72,7 +72,7 @@ class _MyAppState extends State<MyApp> {
             selectionColor: Colors.deepPurple,
           )),
       themeMode: ThemeMode.dark,
-      home: !SharedPreferencesUtil().onboardingCompleted ? const HomePageWrapper(btDevice: null) : const WelcomePage(),
+      home: SharedPreferencesUtil().onboardingCompleted ? const HomePageWrapper(btDevice: null) : const WelcomePage(),
     );
   }
 }
