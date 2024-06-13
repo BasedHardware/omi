a4d7c90ea058d01d9b45907d7a5dac0c5e908a7a
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-06-13 14:30:57 -0700

    improved chat page UI performance

--- apps/AppWithWearable/lib/pages/memories/page.dart
+++ apps/AppWithWearable/lib/pages/memories/page.dart
@@ -27,60 +27,65 @@ class _MemoriesPageState extends State<MemoriesPage> with AutomaticKeepAliveClie
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        const SizedBox(height: 16),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 18.0),
-          child: Text(
-            'Welcome back.',
-            style: Theme.of(context).textTheme.titleLarge!.copyWith(color: Colors.grey, fontSize: 20),
+    return CustomScrollView(
+      slivers: [
+        const SliverToBoxAdapter(child: SizedBox(height: 16)),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18.0),
+            child: Text(
+              'Welcome back.',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(color: Colors.grey, fontSize: 20),
+            ),
           ),
         ),
-        const SizedBox(height: 16),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Text(
-              widget.displayDiscardMemories ? 'Hide Discarded' : 'Show Discarded',
-              style: const TextStyle(color: Colors.white, fontSize: 16),
-            ),
-            const SizedBox(width: 8),
-            IconButton(
+        const SliverToBoxAdapter(child: SizedBox(height: 16)),
+        SliverToBoxAdapter(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                widget.displayDiscardMemories ? 'Hide Discarded' : 'Show Discarded',
+                style: const TextStyle(color: Colors.white, fontSize: 16),
+              ),
+              const SizedBox(width: 8),
+              IconButton(
                 onPressed: () {
                   widget.toggleDiscardMemories();
                 },
                 icon: Icon(
                   widget.displayDiscardMemories ? Icons.cancel_outlined : Icons.filter_list,
                   color: Colors.white,
-                )),
-          ],
+                ),
+              ),
+            ],
+          ),
         ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-          child: (widget.memories.isEmpty)
-              ? const Center(
+        widget.memories.isEmpty
+            ? const SliverToBoxAdapter(
+                child: Center(
                   child: Padding(
                     padding: EdgeInsets.only(top: 32.0),
                     child: EmptyMemoriesWidget(),
                   ),
-                )
-              : ListView.builder(
-                  itemCount: widget.memories.length,
-                  primary: false,
-                  shrinkWrap: true,
-                  scrollDirection: Axis.vertical,
-                  itemBuilder: (context, index) {
+                ),
+              )
+            : SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
                     return MemoryListItem(
                       memoryIdx: index,
                       memory: widget.memories[index],
                       loadMemories: widget.refreshMemories,
                     );
                   },
+                  childCount: widget.memories.length,
                 ),
+              ),
+        const SliverToBoxAdapter(
+          child: SizedBox(height: 80),
         ),
-        const SizedBox(height: 80),
       ],
     );
   }
