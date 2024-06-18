62726c2c193e2887ec9dd8779bccbcf2843aeb64
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-06-17 23:12:00 -0700

    removed speech profile temporally

--- apps/AppWithWearable/lib/pages/settings/page.dart
+++ apps/AppWithWearable/lib/pages/settings/page.dart
@@ -395,33 +395,33 @@ class _SettingsPageState extends State<SettingsPage> {
                       ),
                     ),
                   ),
-                  InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (c) => const SpeakerIdPage()));
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 12, 8, 0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: const Color.fromARGB(255, 29, 29, 29), // Replace with your desired color
-                          borderRadius: BorderRadius.circular(10.0), // Adjust for desired rounded corners
-                        ),
-                        child: const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                'Speech Profile Set Up',
-                                style: TextStyle(color: Color.fromARGB(255, 150, 150, 150), fontSize: 16),
-                              ),
-                              Icon(Icons.arrow_forward_ios, color: Colors.white, size: 16),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
+                  // InkWell(
+                  //   onTap: () {
+                  //     Navigator.of(context).push(MaterialPageRoute(builder: (c) => const SpeakerIdPage()));
+                  //   },
+                  //   child: Padding(
+                  //     padding: const EdgeInsets.fromLTRB(0, 12, 8, 0),
+                  //     child: Container(
+                  //       decoration: BoxDecoration(
+                  //         color: const Color.fromARGB(255, 29, 29, 29), // Replace with your desired color
+                  //         borderRadius: BorderRadius.circular(10.0), // Adjust for desired rounded corners
+                  //       ),
+                  //       child: const Padding(
+                  //         padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                  //         child: Row(
+                  //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  //           children: [
+                  //             Text(
+                  //               'Speech Profile Set Up',
+                  //               style: TextStyle(color: Color.fromARGB(255, 150, 150, 150), fontSize: 16),
+                  //             ),
+                  //             Icon(Icons.arrow_forward_ios, color: Colors.white, size: 16),
+                  //           ],
+                  //         ),
+                  //       ),
+                  //     ),
+                  //   ),
+                  // ),
                   const SizedBox(height: 32),
                   Padding(
                     padding: const EdgeInsets.all(8),
