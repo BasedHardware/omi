1e04aa1c1ef10b50ccfb1e789e47868651798267
diegoasua <baxuaa@gmail.com>
2024-06-21 02:45:22 +0700

    prompt improvement

--- apps/AppWithWearable/lib/backend/api_requests/stream_api_response.dart
+++ apps/AppWithWearable/lib/backend/api_requests/stream_api_response.dart
@@ -123,12 +123,11 @@ String qaStreamedBody(
   var personalityPrompt = '';
   if (personality is String) {
     personalityPrompt = personality.isNotEmpty
-        ? 'This is your personality, you should try to answer in this style: $personality\n'
-        : '';
+        ? '$personality\n'
+        : 'You are an assistant for question-answering tasks.';
   }
   var prompt = '''
-    $personalityPrompt
-    You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. 
+    $personalityPrompt Use the following pieces of retrieved context to answer the question. 
     If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
     If the message doesn't require context, it will be empty, so answer the question casually.
     
