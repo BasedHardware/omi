b283dce7eddcd3661d538e468dec64ba1677b3ab
Franci Penov <francip@gmail.com>
2024-06-25 15:07:45 -0700

    Removing whitespace changes

--- apps/AppWithWearable/lib/main.dart
+++ apps/AppWithWearable/lib/main.dart
@@ -61,8 +61,7 @@ class MyApp extends StatefulWidget {
   @override
   State<MyApp> createState() => _MyAppState();
 
-  static _MyAppState of(BuildContext context) =>
-      context.findAncestorStateOfType<_MyAppState>()!;
+  static _MyAppState of(BuildContext context) => context.findAncestorStateOfType<_MyAppState>()!;
 }
 
 class _MyAppState extends State<MyApp> {
@@ -92,8 +91,7 @@ class _MyAppState extends State<MyApp> {
           // ),
           snackBarTheme: SnackBarThemeData(
             backgroundColor: Colors.grey.shade900,
-            contentTextStyle: const TextStyle(
-                fontSize: 16, color: Colors.white, fontWeight: FontWeight.w500),
+            contentTextStyle: const TextStyle(fontSize: 16, color: Colors.white, fontWeight: FontWeight.w500),
           ),
           textTheme: TextTheme(
             titleLarge: const TextStyle(fontSize: 18, color: Colors.white),
@@ -107,8 +105,7 @@ class _MyAppState extends State<MyApp> {
           )),
       themeMode: ThemeMode.dark,
       // home: const HasBackupPage(),
-      home: (SharedPreferencesUtil()
-              .onboardingCompleted) //  && SharedPreferencesUtil().deviceId != ''
+      home: (SharedPreferencesUtil().onboardingCompleted) //  && SharedPreferencesUtil().deviceId != ''
           ? const HomePageWrapper()
           : const OnboardingWrapper(),
     );
