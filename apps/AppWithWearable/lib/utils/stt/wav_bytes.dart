290e70e065d1bb32a16f06b7ae41e764deb104be
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-06-26 23:31:13 -0700

    getBleAudioBytesListener determines if opus or pcm + improved pcm bytes handling + initial steps for opus

--- apps/AppWithWearable/lib/utils/stt/wav_bytes.dart
+++ apps/AppWithWearable/lib/utils/stt/wav_bytes.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:math';
 import 'dart:typed_data';
-import 'package:intl/intl.dart';
-import 'package:path_provider/path_provider.dart';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:opus_dart/opus_dart.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:tuple/tuple.dart';
 
 const int sampleRate = 8000;
 const int channelCount = 1;
@@ -103,5 +107,119 @@ class WavBytesUtil {
     byteData.setUint32(40, dataLength, Endian.little);
 
     return byteData.buffer.asUint8List();
+  }
+}
+
+class WavBytesUtil2 {
+  int lastPacketIndex = -1;
+  int lastFrameId = -1;
+  List<int> pending = [];
+  List<List<int>> frames = [];
+  int lost = 0;
+  final SimpleOpusDecoder opusDecoder = SimpleOpusDecoder(sampleRate: 48000, channels: 1);
+
+  // TODO: initialize with opus or PCM
+  // TODO: create wav file differently if using opus
+  // TODO: try pcm again but 16k sample rate
+
+  void storeBytes(value) {
+    int index = value[0] + (value[1] << 8);
+    int internal = value[2];
+    List<int> content = value.sublist(3);
+    // debugPrint('Received: $index ($internal) - ${content.length} bytes');
+
+    // Start of a new frame
+    if (lastPacketIndex == -1 && internal == 0) {
+      lastPacketIndex = index;
+      lastFrameId = internal;
+      pending = content;
+      return;
+    }
+
+    if (lastPacketIndex == -1) return;
+
+    // Lost frame - reset state
+    if (index != lastPacketIndex + 1 || (internal != 0 && internal != lastFrameId + 1)) {
+      debugPrint('Lost frame');
+      lastPacketIndex = -1;
+      pending = [];
+      lost += 1;
+      return;
+    }
+
+    // Start of a new frame
+    if (internal == 0) {
+      frames.add(pending); // Save frame
+      pending = content; // Start new frame
+      lastFrameId = internal; // Update internal frame id
+      lastPacketIndex = index; // Update packet id
+      // debugPrint('Frames received: ${frames.length} && Lost: $lost');
+      return;
+    }
+
+    // Continue frame
+    pending.addAll(content);
+    lastFrameId = internal; // Update internal frame id
+    lastPacketIndex = index; // Update packet id
+  }
+
+  Future<Tuple2<File, List<List<int>>>> createWavFile({String? filename}) async {
+    Int16List samples;
+    var format = 'pcm';
+    if (format == 'pcm') {
+      samples = getSamples();
+    } else if (format == 'opus') {
+      samples = await decodeOpusFrames();
+      debugPrint('Decoded samples: ${samples.length}');
+    } else {
+      throw Exception('Invalid format');
+    }
+    var framesCopy = List<List<int>>.from(frames);
+    trimFrames(untilSecond: frames.length ~/ 100); // basically clearing them all
+    File file = await WavBytesUtil.createWavFile(samples, filename: filename);
+    return Tuple2(file, framesCopy);
+  }
+
+  Int16List getSamples() {
+    int totalLength = frames.fold(0, (sum, frame) => sum + frame.length);
+
+    // Create an Int16List to store the samples
+    Int16List samples = Int16List(totalLength ~/ 2);
+    int sampleIndex = 0;
+
+    // Iterate through each frame and each byte in the frame
+    for (int i = 0; i < frames.length; i++) {
+      for (int j = 0; j < frames[i].length; j += 2) {
+        int byte1 = frames[i][j];
+        int byte2 = frames[i][j + 1];
+        int sample = (byte2 << 8) | byte1;
+        samples[sampleIndex++] = sample;
+      }
+    }
+    return samples;
+  }
+
+  trimFrames({required int untilSecond}) {
+    frames.removeRange(0, min(untilSecond * 100, frames.length));
+  }
+
+  void insertAudioBytes(List<List<int>> bytes) {
+    frames.insertAll(0, bytes);
+    debugPrint('insertAudioBytes: ${frames.length} frames');
+  }
+
+  void clearAudioBytes() {
+    frames.clear();
+  }
+
+  /// OPUS
+
+  Future<Int16List> decodeOpusFrames() async {
+    List<int> decodedSamples = [];
+    for (var frame in frames) {
+      var decoded = opusDecoder.decode(input: Uint8List.fromList(frame));
+      decodedSamples.addAll(decoded);
+    }
+    return Int16List.fromList(decodedSamples);
   }
 }
