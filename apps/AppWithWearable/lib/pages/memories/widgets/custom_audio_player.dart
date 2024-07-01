b33c2bcdd3f49a16c93c6c32645be7eae2984570
SHAH-JI <shahji1998@gmail.com>
2024-07-01 22:45:28 +0500

    feat: audio waveform and audio player initial setup added

---
+++ apps/AppWithWearable/lib/pages/memories/widgets/custom_audio_player.dart
@@ -0,0 +1,495 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'package:audio_session/audio_session.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:just_waveform/just_waveform.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:upgrader/upgrader.dart';
+import 'package:path/path.dart' as p;
+import 'package:rxdart/rxdart.dart';
+
+class CustomAudioPlayer extends StatefulWidget {
+  const CustomAudioPlayer({super.key});
+
+  @override
+  State<CustomAudioPlayer> createState() => _CustomAudioPlayerState();
+}
+
+class _CustomAudioPlayerState extends State<CustomAudioPlayer> with WidgetsBindingObserver {
+  final _player = AudioPlayer();
+  final progressStream = BehaviorSubject<WaveformProgress>();
+  @override
+  void initState() {
+    super.initState();
+    ambiguate(WidgetsBinding.instance)!.addObserver(this);
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.black,
+    ));
+    _init();
+  }
+
+  Future<void> _init() async {
+    final session = await AudioSession.instance;
+    await session.configure(const AudioSessionConfiguration.music());
+    _player.playbackEventStream.listen((event) {}, onError: (Object e, StackTrace stackTrace) {
+      debugPrint('A stream error occurred: $e');
+    });
+    try {
+      await _player.setAudioSource(AudioSource.asset('assets/audios/PinkPanther30.wav'));
+    } on PlayerException catch (e) {
+      debugPrint('Error loading audio source: $e');
+    }
+
+    ///read audio file from assets
+    final audioFile = File(p.join((await getTemporaryDirectory()).path, 'PinkPanther30.wav'));
+    try {
+      await audioFile.writeAsBytes((await rootBundle.load('assets/audios/PinkPanther30.wav')).buffer.asUint8List());
+      final waveFile = File(p.join((await getTemporaryDirectory()).path, 'waveform.wave'));
+      JustWaveform.extract(audioInFile: audioFile, waveOutFile: waveFile)
+          .listen(progressStream.add, onError: progressStream.addError);
+    } catch (e) {
+      progressStream.addError(e);
+    }
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      _player.stop();
+    }
+  }
+
+  @override
+  void dispose() {
+    ambiguate(WidgetsBinding.instance)!.removeObserver(this);
+    _player.dispose();
+    super.dispose();
+  }
+
+  Stream<PositionData> get _positionDataStream => Rx.combineLatest3<Duration, Duration, Duration?, PositionData>(
+      _player.positionStream,
+      _player.bufferedPositionStream,
+      _player.durationStream,
+      (position, bufferedPosition, duration) => PositionData(position, bufferedPosition, duration ?? Duration.zero));
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              StreamBuilder<PositionData>(
+                stream: _positionDataStream,
+                builder: (context, snapshot) {
+                  final positionData = snapshot.data;
+                  return SeekBar(
+                    duration: positionData?.duration ?? Duration.zero,
+                    position: positionData?.position ?? Duration.zero,
+                    bufferedPosition: positionData?.bufferedPosition ?? Duration.zero,
+                    onChangeEnd: _player.seek,
+                  );
+                },
+              ),
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                  height: 150.0,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        offset: Offset(0.0, 2.0),
+                        blurRadius: 2.0,
+                      ),
+                    ],
+                  ),
+                  width: double.maxFinite,
+                  child: StreamBuilder<WaveformProgress>(
+                    stream: progressStream,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasError) {
+                        return Center(
+                          child: Text(
+                            'Error: ${snapshot.error}',
+                            style: Theme.of(context).textTheme.titleLarge,
+                            textAlign: TextAlign.center,
+                          ),
+                        );
+                      }
+                      final progress = snapshot.data?.progress ?? 0.0;
+                      final waveform = snapshot.data?.waveform;
+                      if (waveform == null) {
+                        return Center(
+                          child: Text(
+                            '${(100 * progress).toInt()}%',
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                        );
+                      }
+                      return AudioWaveformWidget(
+                        waveform: waveform,
+                        start: Duration.zero,
+                        duration: waveform.duration,
+                        positionDataStream: _positionDataStream,
+                        player: _player,
+                      );
+                    },
+                  ),
+                ),
+              ),
+              ControlButtons(_player),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/// Displays the play/pause button and volume/speed sliders.
+class ControlButtons extends StatelessWidget {
+  final AudioPlayer player;
+
+  const ControlButtons(this.player, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        // Opens volume slider dialog
+        IconButton(
+          icon: const Icon(Icons.replay_10),
+          iconSize: 48.0,
+          onPressed: () => player.seek(
+            Duration(
+              seconds: player.position.inSeconds <= 15 ? 0 : player.position.inSeconds - 15,
+            ),
+          ),
+        ),
+
+        StreamBuilder<PlayerState>(
+          stream: player.playerStateStream,
+          builder: (context, snapshot) {
+            final playerState = snapshot.data;
+            final processingState = playerState?.processingState;
+            final playing = playerState?.playing;
+            if (processingState == ProcessingState.loading || processingState == ProcessingState.buffering) {
+              return Container(
+                margin: const EdgeInsets.all(8.0),
+                width: 64.0,
+                height: 64.0,
+                child: const CircularProgressIndicator(),
+              );
+            } else if (playing != true) {
+              return IconButton(
+                icon: const Icon(Icons.play_arrow),
+                iconSize: 64.0,
+                onPressed: player.play,
+              );
+            } else if (processingState != ProcessingState.completed) {
+              return IconButton(
+                icon: const Icon(Icons.pause),
+                iconSize: 64.0,
+                onPressed: player.pause,
+              );
+            } else {
+              return IconButton(
+                icon: const Icon(Icons.replay),
+                iconSize: 64.0,
+                onPressed: () => player.seek(Duration.zero),
+              );
+            }
+          },
+        ),
+        IconButton(
+          icon: const Icon(Icons.forward_10),
+          iconSize: 48.0,
+          onPressed: () => player.seek(
+            Duration(
+              seconds: player.position.inSeconds >= player.duration!.inSeconds - 15
+                  ? player.bufferedPosition.inSeconds
+                  : player.position.inSeconds + 15,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class PositionData {
+  final Duration position;
+  final Duration bufferedPosition;
+  final Duration duration;
+
+  PositionData(this.position, this.bufferedPosition, this.duration);
+}
+
+T? ambiguate<T>(T? value) => value;
+
+class SeekBar extends StatefulWidget {
+  final Duration duration;
+  final Duration position;
+  final Duration bufferedPosition;
+  final ValueChanged<Duration>? onChanged;
+  final ValueChanged<Duration>? onChangeEnd;
+
+  const SeekBar({
+    Key? key,
+    required this.duration,
+    required this.position,
+    required this.bufferedPosition,
+    this.onChanged,
+    this.onChangeEnd,
+  }) : super(key: key);
+
+  @override
+  SeekBarState createState() => SeekBarState();
+}
+
+class SeekBarState extends State<SeekBar> {
+  double? _dragValue;
+  late SliderThemeData _sliderThemeData;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    _sliderThemeData = SliderTheme.of(context).copyWith(
+      trackHeight: 2.0,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SliderTheme(
+          data: _sliderThemeData.copyWith(
+            thumbShape: HiddenThumbComponentShape(),
+            activeTrackColor: Colors.transparent,
+            inactiveTrackColor: Colors.transparent,
+            thumbColor: Colors.red,
+          ),
+          child: ExcludeSemantics(
+            child: Slider(
+              min: 0.0,
+              max: widget.duration.inMilliseconds.toDouble(),
+              value: min(widget.bufferedPosition.inMilliseconds.toDouble(), widget.duration.inMilliseconds.toDouble()),
+              onChanged: (value) {
+                setState(() {
+                  _dragValue = value;
+                });
+                if (widget.onChanged != null) {
+                  widget.onChanged!(Duration(milliseconds: value.round()));
+                }
+              },
+              onChangeEnd: (value) {
+                if (widget.onChangeEnd != null) {
+                  widget.onChangeEnd!(Duration(milliseconds: value.round()));
+                }
+                _dragValue = null;
+              },
+            ),
+          ),
+        ),
+        SliderTheme(
+          data: _sliderThemeData.copyWith(
+            thumbColor: Colors.red,
+            activeTrackColor: Colors.transparent,
+            inactiveTrackColor: Colors.transparent,
+          ),
+          child: Slider(
+            min: 0.0,
+            max: widget.duration.inMilliseconds.toDouble(),
+            value:
+                min(_dragValue ?? widget.position.inMilliseconds.toDouble(), widget.duration.inMilliseconds.toDouble()),
+            onChanged: (value) {
+              setState(() {
+                _dragValue = value;
+              });
+              if (widget.onChanged != null) {
+                widget.onChanged!(Duration(milliseconds: value.round()));
+              }
+            },
+            onChangeEnd: (value) {
+              if (widget.onChangeEnd != null) {
+                widget.onChangeEnd!(Duration(milliseconds: value.round()));
+              }
+              _dragValue = null;
+            },
+          ),
+        ),
+        Positioned(
+          right: 16.0,
+          bottom: 0.0,
+          child: Text(
+              RegExp(r'((^0*[1-9]\d*:)?\d{2}:\d{2})\.\d+$').firstMatch("$_remaining")?.group(1) ?? '$_remaining',
+              style: Theme.of(context).textTheme.bodySmall),
+        ),
+      ],
+    );
+  }
+
+  Duration get _remaining => widget.duration - widget.position;
+}
+
+class HiddenThumbComponentShape extends SliderComponentShape {
+  @override
+  Size getPreferredSize(bool isEnabled, bool isDiscrete) => Size.zero;
+
+  @override
+  void paint(
+    PaintingContext context,
+    Offset center, {
+    required Animation<double> activationAnimation,
+    required Animation<double> enableAnimation,
+    required bool isDiscrete,
+    required TextPainter labelPainter,
+    required RenderBox parentBox,
+    required SliderThemeData sliderTheme,
+    required TextDirection textDirection,
+    required double value,
+    required double textScaleFactor,
+    required Size sizeWithOverflow,
+  }) {}
+}
+
+class AudioWaveformWidget extends StatefulWidget {
+  final Color waveColor;
+  final double scale;
+  final double strokeWidth;
+  final double pixelsPerStep;
+  final Waveform waveform;
+  final Duration start;
+  final Duration duration;
+  final Stream<PositionData> positionDataStream;
+  final AudioPlayer player;
+
+  const AudioWaveformWidget({
+    super.key,
+    required this.waveform,
+    required this.start,
+    required this.duration,
+    this.waveColor = Colors.black,
+    this.scale = 4.0,
+    this.strokeWidth = 1.0,
+    this.pixelsPerStep = 3.0,
+    required this.positionDataStream,
+    required this.player,
+  });
+
+  @override
+  _AudioWaveformState createState() => _AudioWaveformState();
+}
+
+class _AudioWaveformState extends State<AudioWaveformWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        ClipRect(
+          child: CustomPaint(
+            painter: AudioWaveformPainter(
+              waveColor: widget.waveColor,
+              waveform: widget.waveform,
+              start: widget.start,
+              duration: widget.duration,
+              scale: widget.scale,
+              strokeWidth: widget.strokeWidth,
+              pixelsPerStep: widget.pixelsPerStep,
+            ),
+          ),
+        ),
+        StreamBuilder<PositionData>(
+          stream: widget.positionDataStream,
+          builder: (context, snapshot) {
+            final positionData = snapshot.data;
+            return SeekBar(
+              duration: positionData?.duration ?? Duration.zero,
+              position: positionData?.position ?? Duration.zero,
+              bufferedPosition: positionData?.bufferedPosition ?? Duration.zero,
+              onChangeEnd: widget.player.seek,
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class AudioWaveformPainter extends CustomPainter {
+  final double scale;
+  final double strokeWidth;
+  final double pixelsPerStep;
+  final Paint wavePaint;
+  final Waveform waveform;
+  final Duration start;
+  final Duration duration;
+
+  AudioWaveformPainter({
+    required this.waveform,
+    required this.start,
+    required this.duration,
+    Color waveColor = Colors.black,
+    required this.scale,
+    required this.strokeWidth,
+    required this.pixelsPerStep,
+  }) : wavePaint = Paint()
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = strokeWidth
+          ..strokeCap = StrokeCap.round
+          ..color = waveColor;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (duration == Duration.zero) return;
+
+    double width = size.width;
+    double height = size.height;
+
+    final waveformPixelsPerWindow = waveform.positionToPixel(duration).toInt();
+    final waveformPixelsPerDevicePixel = waveformPixelsPerWindow / width;
+    final waveformPixelsPerStep = waveformPixelsPerDevicePixel * pixelsPerStep;
+    final sampleOffset = waveform.positionToPixel(start);
+    final sampleStart = -sampleOffset % waveformPixelsPerStep;
+    for (var i = sampleStart.toDouble(); i <= waveformPixelsPerWindow + 1.0; i += waveformPixelsPerStep) {
+      final sampleIdx = (sampleOffset + i).toInt();
+      final x = i / waveformPixelsPerDevicePixel;
+      final minY = normalise(waveform.getPixelMin(sampleIdx), height);
+      final maxY = normalise(waveform.getPixelMax(sampleIdx), height);
+      canvas.drawLine(
+        Offset(x + strokeWidth / 2, max(strokeWidth * 0.75, minY)),
+        Offset(x + strokeWidth / 2, min(height - strokeWidth * 0.75, maxY)),
+        wavePaint,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant AudioWaveformPainter oldDelegate) {
+    return false;
+  }
+
+  double normalise(int s, double height) {
+    if (waveform.flags == 0) {
+      final y = 32768 + (scale * s).clamp(-32768.0, 32767.0).toDouble();
+      return height - 1 - y * height / 65536;
+    } else {
+      final y = 128 + (scale * s).clamp(-128.0, 127.0).toDouble();
+      return height - 1 - y * height / 256;
+    }
+  }
+}
