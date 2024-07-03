ba3c1bd03e98f96ed9bd9192a9e5f98456e40cf2
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-07-02 23:53:18 -0700

    minor ui fixes

--- apps/AppWithWearable/lib/widgets/dialog.dart
+++ apps/AppWithWearable/lib/widgets/dialog.dart
@@ -3,29 +3,24 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-getDialog(BuildContext context, Function onCancel, Function onConfirm, String title, String content) {
+getDialog(BuildContext context, Function onCancel, Function onConfirm, String title, String content,
+    {bool singleButton = false}) {
+  var actions = singleButton
+      ? [
+          TextButton(
+            onPressed: () => onCancel(),
+            child: const Text('Ok', style: TextStyle(color: Colors.white)),
+          )
+        ]
+      : [
+          TextButton(
+            onPressed: () => onCancel(),
+            child: const Text('Cancel', style: TextStyle(color: Colors.white)),
+          ),
+          TextButton(onPressed: () => onConfirm(), child: const Text('Confirm', style: TextStyle(color: Colors.white))),
+        ];
   if (Platform.isIOS) {
-    return CupertinoAlertDialog(
-      title: Text(title),
-      content: Text(content),
-      actions: [
-        TextButton(
-          onPressed: () => onCancel(),
-          child: const Text('Cancel', style: TextStyle(color: Colors.white)),
-        ),
-        TextButton(onPressed: () => onConfirm(), child: const Text('Confirm', style: TextStyle(color: Colors.white))),
-      ],
-    );
+    return CupertinoAlertDialog(title: Text(title), content: Text(content), actions: actions);
   }
-  return AlertDialog(
-    title: Text(title),
-    content: Text(content),
-    actions: [
-      TextButton(
-        onPressed: () => onCancel(),
-        child: const Text('Cancel', style: TextStyle(color: Colors.white)),
-      ),
-      TextButton(onPressed: () => onConfirm(), child: const Text('Confirm', style: TextStyle(color: Colors.white))),
-    ],
-  );
+  return AlertDialog(title: Text(title), content: Text(content), actions: actions);
 }
