c3131cbcfd3425ded3caf3a91e9db0350e0f1bde
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-07-09 17:45:16 -0700

    language selected != 'en', show dialog explaining limitation

--- apps/AppWithWearable/lib/backend/utils.dart
+++ apps/AppWithWearable/lib/backend/utils.dart
@@ -1,22 +1,36 @@
 final Map<String, String> availableLanguages = {
-  // server doesn't support the ones commented.
+  'Bulgarian': 'bg',
+  'Catalan': 'ca',
   'Chinese': 'zh',
+  'Czech': 'cs',
+  'Danish': 'da',
   'Dutch': 'nl',
   'English': 'en',
-  'French': 'fr',
   'Finnish': 'fi',
+  'French': 'fr',
   'German': 'de',
   'Greek': 'el',
   'Hindi': 'hi',
+  'Hungarian': 'hu',
+  'Indonesian': 'id',
   'Italian': 'it',
   'Japanese': 'ja',
   'Korean': 'ko',
+  'Latvian': 'lv',
+  'Lithuanian': 'lt',
   'Malay': 'ms',
   'Norwegian': 'no',
   'Polish': 'pl',
   'Portuguese': 'pt',
   'Russian': 'ru',
   'Spanish': 'es',
+  'Swedish': 'sv',
+  'Thai': 'th',
   'Turkish': 'tr',
+  'Ukrainian': 'uk',
   'Vietnamese': 'vi',
 };
+
+getLanguageName(String code) {
+  return availableLanguages.entries.firstWhere((element) => element.value == code).key;
+}
