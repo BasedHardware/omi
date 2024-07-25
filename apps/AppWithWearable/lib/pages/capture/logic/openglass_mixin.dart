805a94ceb84201cf8e229a35dcc63774f7de3960
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-07-25 00:28:16 -0700

    more code cleaning

--- apps/AppWithWearable/lib/pages/capture/logic/openglass_mixin.dart
+++ apps/AppWithWearable/lib/pages/capture/logic/openglass_mixin.dart
@@ -19,21 +19,24 @@ mixin OpenGlassMixin {
     Function(List<Tuple2<String, String>>) onPhotosUpdated,
     Function(bool) setHasTranscripts,
   ) async {
-    _bleBytesStream = await getBleImageBytesListener(device.id, onImageBytesReceived: (List<int> value) async {
-      if (value.isEmpty) return;
-      Uint8List data = Uint8List.fromList(value);
-      // print(data);
-      Uint8List? completedImage = imageBytesUtil.processChunk(data);
-      if (completedImage != null && completedImage.isNotEmpty) {
-        debugPrint('Completed image bytes length: ${completedImage.length}');
-        getPhotoDescription(completedImage).then((description) {
-          photos.add(Tuple2(base64Encode(completedImage), description));
-          onPhotosUpdated(photos);
-          debugPrint('photos: ${photos.length}');
-          setHasTranscripts(true);
-        });
-      }
-    });
+    _bleBytesStream = await getBleImageBytesListener(
+      device.id,
+      onImageBytesReceived: (List<int> value) async {
+        if (value.isEmpty) return;
+        Uint8List data = Uint8List.fromList(value);
+        // print(data);
+        Uint8List? completedImage = imageBytesUtil.processChunk(data);
+        if (completedImage != null && completedImage.isNotEmpty) {
+          debugPrint('Completed image bytes length: ${completedImage.length}');
+          getPhotoDescription(completedImage).then((description) {
+            photos.add(Tuple2(base64Encode(completedImage), description));
+            onPhotosUpdated(photos);
+            debugPrint('photos: ${photos.length}');
+            setHasTranscripts(true);
+          });
+        }
+      },
+    );
     await cameraStopPhotoController(device.id);
     await cameraStartPhotoController(device.id);
   }
--- apps/AppWithWearable/lib/pages/capture/page.dart
+++ apps/AppWithWearable/lib/pages/capture/page.dart
@@ -9,11 +9,9 @@ import 'package:friend_private/backend/database/memory.dart';
 import 'package:friend_private/backend/database/message.dart';
 import 'package:friend_private/backend/database/message_provider.dart';
 import 'package:friend_private/backend/database/transcript_segment.dart';
-import 'package:friend_private/backend/growthbook.dart';
 import 'package:friend_private/backend/preferences.dart';
 import 'package:friend_private/backend/schema/bt_device.dart';
 import 'package:friend_private/pages/capture/location_service.dart';
-import 'package:friend_private/pages/capture/logic/chunks_mixin.dart';
 import 'package:friend_private/pages/capture/logic/openglass_mixin.dart';
 import 'package:friend_private/pages/capture/widgets/widgets.dart';
 import 'package:friend_private/utils/audio/wav_bytes.dart';
@@ -50,57 +48,16 @@ class CapturePage extends StatefulWidget {
 }
 
 class CapturePageState extends State<CapturePage>
-    with
-        AutomaticKeepAliveClientMixin,
-        WidgetsBindingObserver,
-        PhoneRecorderMixin,
-        WebSocketMixin,
-        OpenGlassMixin,
-        AudioChunksMixin {
+    with AutomaticKeepAliveClientMixin, WidgetsBindingObserver, PhoneRecorderMixin, WebSocketMixin, OpenGlassMixin {
   @override
   bool get wantKeepAlive => true;
 
   BTDeviceStruct? btDevice;
   bool _hasTranscripts = false;
-  final record = AudioRecorder();
   static const quietSecondsForMemoryCreation = 120;
 
   /// ----
-  List<TranscriptSegment> segments = [
-//     TranscriptSegment(text: '''
-//     Speaker 0: we they comprehend
-//
-// Speaker 1: i'd love to sir dig into each of those x risk s risk and irisk so can can you like linger in i risk what is that
-//
-// Speaker 0: so japanese concept of ikigai you find something which allows you to make money you are good at it and the society says we need it so like you have this awesome job you are podcaster gives you a lot of meaning you have a good life i assume you had that's what we want more people to find to have for many intellectuals it is their occupation which gives them a lot of meaning i'm a researcher philosopher scholar that means something to me in a world where an artist is not feeling appreciated because his art is just not competitive with what is produced by machines so a writer or scientist will lose a lot of that and that's a laurel we're talking about complete technological unemployment not losing 10% of jobs we're losing all jobs what do people do with all that free time what happens when everything society is built on is completely modified in 1 generation it's not a small process when we get to figure out how to live that new lifestyle but it's pretty quick in that world can't humans do what humans currently do with chess play each other even though ai systems are far superior at this time than just so we just create artificial fun fun focus maximize the fun and and let the the ai focus on the productivity it's an option i have a paper where i try to solve the value alignment problem for multiple agents and the solution to our work comp is to give everyone a personal virtual universe you can do whatever you want in that world you could be king you could be slave you decide what happens so it's basically a glorified visual game where you get to enjoy yourself and someone else takes care of your needs and substrate alignment is the only thing we need to solve we don't have to get 8 000 000 000 humans to agree on anything so okay so what why is that not a likely outcome why can't ai systems create video games for us to lose ourselves in each each with an individual video game universe
-//
-// Speaker 1: some people say that's what happened with the simulation
-//
-// Speaker 0: and we're playing that video again and now we're creating what maybe we're creating artificial threats for ourselves to be scared about because because fear is really exciting it allows us to play the video game more more vigorously
-//
-// Speaker 1: and some people choose to play on a more difficult level with more constraints some say okay i'm just gonna enjoy the game
-//
-// Speaker 0: privilege level absolutely okay what was that paper on multi agent value alignment personal universes personal universes so that's 1 of the possible outcomes but what what what in general is the idea of the paper so it's looking at multiple agents that are human ai like a hybrid system where there's humans and ai or they're looking at humans or just so this intelligent agents in order to solve alignment problem i'm trying to formalize this a little better usually we're talking about getting ais to do what we want which is not well defined i'm talking about creator of the system owner of that ai humanex as a whole we don't agree on much there is no universally accepted ethics morals across cultures religions people have individually very different preferences politically and such so even if we somehow manage all the other aspects programming those fuzzy concepts and getting to follow them closely we don't agree on what to program in so my solution was k we don't have to compromise on room temperature you have your universe i have mine whatever you want and if you like me you can invite me to visit the universe we don't have to be independent but the point is you can be and virtual reality is getting pretty good it's gonna hit a point where you can't tell the difference and if you can't tell if it's real or not what's the difference
-//
-// Speaker 1: so basically give up on value alignment create and size like the the multiverse theory this is create an entire universe for you where
-//
-// Speaker 0: your values
-//
-// Speaker 1: you still have to align with that individual they have to be happy in that simulation but it's a much easier problem to align with 1 agent versus 8 000 000 000 agents plus animals aliens
-//
-// Speaker 0: so you convert the multi agent problem into a single agent problem i'm proud to do that yeah okay is there any way to so okay that's giving up on the on the value of the problem but is there any way to solve the value of the problem there's a bunch of humans multiple human tens of humans or 8 000 000 000 humans that have very different set of values it seems contradicting
-//
-// Speaker 1: i haven't seen anyone explain what it means outside of kinda words which pack a lot make it good make it desirable make it something they don't regret but how do you specifically formalize those notions how do you program them in i haven't seen anyone make progress on that so far
-//
-// Speaker 0: but isn't that optimization journey that we're doing as a human civilization we're looking at geopolitics nations are in a state of anarchy with each other they start wars there's conflict and oftentimes they have a very different to that so we're essentially trying to solve the value on the problem with humans right but the examples you gave some for example 2 different religions saying this is our holy site and we are not willing to compromise it in any way if you can make 2 holy sites in virtual worlds you solve the problem but if you only have 1 it's not divisible you're kinda stuck there
-//'
-// Speaker 1: but what if we want to be a tension with each other and that through that tension we understand ourselves and we understand the world so that that's the intellectual journey we're on we're on as a human civilization it will create into
-//
-// Speaker 0: and physical conflict and through that figure stuff out if we go back to that idea of simulation and this is entertainment kinda giving meaning to us the question is how much suffering is reasonable for a video game so yeah i don't mind you know a video game where i get haptic feedback there's a little bit of shaking unethical at least by our human standards it's possible to remove suffering if we're looking at human civilization as an optimization problem
-//
-// Speaker 1: so we know there are some humans who because of a mutation don't experience physical pain so at least physical pain can be mutated out reengineered out suffering in terms of meaning like you burn the only copy of my book is a little harder but even there you can manipulate your hedonic set point you can change change defaults you can reset
-//     ''', speaker: 'SPEAKER_00', isUser: false, start: 0, end: 30)
-  ];
+  List<TranscriptSegment> segments = [];
 
   StreamSubscription? _bleBytesStream;
   WavBytesUtil? audioStorage;
@@ -196,9 +153,8 @@ class CapturePageState extends State<CapturePage>
 
   Future<void> startOpenGlass() async {
     if (btDevice == null) return;
-    print(btDevice!.name);
     isGlasses = await hasPhotoStreamingCharacteristic(btDevice!.id);
-    print('isGlasses: $isGlasses');
+    debugPrint('startOpenGlass isGlasses: $isGlasses');
     if (isGlasses) await openGlassProcessing(btDevice!, (p) => setState(() {}), setHasTranscripts);
     closeWebSocket();
   }
@@ -231,8 +187,6 @@ class CapturePageState extends State<CapturePage>
     if (memoryCreating) return;
     // TODO: should clean variables here? and keep them locally?
     setState(() => memoryCreating = true);
-    String transcript = TranscriptSegment.segmentsAsString(segments);
-    debugPrint('_createMemory transcript: \n$transcript');
     File? file;
     if (audioStorage?.frames.isNotEmpty == true) {
       try {
@@ -243,7 +197,7 @@ class CapturePageState extends State<CapturePage>
     }
     Memory? memory = await processTranscriptContent(
       context,
-      transcript,
+      TranscriptSegment.segmentsAsString(segments),
       segments,
       file?.path,
       startedAt: currentTranscriptStartedAt,
@@ -258,7 +212,7 @@ class CapturePageState extends State<CapturePage>
     if (memory != null && !memory.discarded) executeBackupWithUid();
     if (memory != null && !memory.discarded && SharedPreferencesUtil().postMemoryNotificationIsChecked) {
       postMemoryCreationNotification(memory).then((r) {
-        // r = 'Hi there testing notifications stuff';
+        // TODO: this should be a plugin instead.
         debugPrint('Notification response: $r');
         if (r.isEmpty) return;
         sendMessageToChat(Message(DateTime.now(), r, 'ai'), memory);
@@ -401,23 +355,6 @@ class CapturePageState extends State<CapturePage>
     }
   }
 
-  // @override
-  // void didChangeAppLifecycleState(AppLifecycleState state) async {
-  //   final backgroundService = FlutterBackgroundService();
-  //   if (state == AppLifecycleState.paused) {
-  //     backgroundTranscriptTimer?.cancel();
-  //     if (await backgroundService.isRunning()) {
-  //       _memoryCreationTimer?.cancel();
-  //     }
-  //   }
-  //   if (state == AppLifecycleState.resumed) {
-  //     if (await backgroundService.isRunning()) {
-  //       await onAppIsResumed(_processFileToTranscript);
-  //     }
-  //   }
-  //   super.didChangeAppLifecycleState(state);
-  // }
-
   @override
   Widget build(BuildContext context) {
     super.build(context);
@@ -440,7 +377,6 @@ class CapturePageState extends State<CapturePage>
       await stopStreamRecording(wsConnectionState, websocketChannel);
       setState(() => recordingState = RecordingState.stop);
       _memoryCreationTimer?.cancel();
-      // _memoryCreationTimer = Timer(const Duration(milliseconds: 500), () => _createMemory());
       _createMemory();
     } else if (recordingState == RecordingState.initialising) {
       debugPrint('initialising, have to wait');
@@ -453,8 +389,6 @@ class CapturePageState extends State<CapturePage>
           () async {
             Navigator.pop(context);
             setState(() => recordingState = RecordingState.initialising);
-            // service.invoke("stateUpdate", {"state": 'recording'});
-            // await startRecording(_processFileToTranscript);
             closeWebSocket();
             await initiateWebsocket(BleAudioCodec.pcm16, 16000);
             await startStreamRecording(wsConnectionState, websocketChannel);
