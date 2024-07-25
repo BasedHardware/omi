5351b1bdf1ed21fc542b7281a73e76b8aaf8d2dc
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-07-25 00:17:00 -0700

    restored open glass

--- apps/AppWithWearable/lib/pages/capture/logic/openglass_mixin.dart
+++ apps/AppWithWearable/lib/pages/capture/logic/openglass_mixin.dart
@@ -14,14 +14,18 @@ mixin OpenGlassMixin {
   ImageBytesUtil imageBytesUtil = ImageBytesUtil();
   StreamSubscription? _bleBytesStream;
 
-  Future<void> openGlassProcessing(BTDeviceStruct device, Function(List<Tuple2<String, String>>) onPhotosUpdated,
-      Function(bool) setHasTranscripts) async {
+  Future<void> openGlassProcessing(
+    BTDeviceStruct device,
+    Function(List<Tuple2<String, String>>) onPhotosUpdated,
+    Function(bool) setHasTranscripts,
+  ) async {
     _bleBytesStream = await getBleImageBytesListener(device.id, onImageBytesReceived: (List<int> value) async {
       if (value.isEmpty) return;
       Uint8List data = Uint8List.fromList(value);
+      // print(data);
       Uint8List? completedImage = imageBytesUtil.processChunk(data);
       if (completedImage != null && completedImage.isNotEmpty) {
-        debugPrint('Completed image size: ${completedImage.length}');
+        debugPrint('Completed image bytes length: ${completedImage.length}');
         getPhotoDescription(completedImage).then((description) {
           photos.add(Tuple2(base64Encode(completedImage), description));
           onPhotosUpdated(photos);
