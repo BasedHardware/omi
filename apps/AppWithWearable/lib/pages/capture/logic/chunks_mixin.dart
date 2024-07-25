2f9619c546c1fc4f992f33d3d66726dfd652dd90
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-07-25 00:19:34 -0700

    removed 30 seconds chunks logic

--- apps/AppWithWearable/lib/pages/capture/logic/chunks_mixin.dart
+++ apps/AppWithWearable/lib/pages/capture/logic/chunks_mixin.dart
@@ -6,13 +6,78 @@ import 'package:flutter/material.dart';
 import 'package:friend_private/backend/api_requests/api/other.dart';
 import 'package:friend_private/backend/api_requests/api/server.dart';
 import 'package:friend_private/backend/database/transcript_segment.dart';
-import 'package:friend_private/backend/growthbook.dart';
 import 'package:friend_private/backend/preferences.dart';
 import 'package:friend_private/utils/audio/wav_bytes.dart';
 import 'package:friend_private/utils/ble/communication.dart';
 import 'package:instabug_flutter/instabug_flutter.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 import 'package:tuple/tuple.dart';
+
+// Future<void> initiateBytesProcessing() async {
+//   debugPrint('initiateBytesProcessing: $btDevice');
+//   BleAudioCodec codec = await getAudioCodec(btDevice!.id);
+//   if (codec == BleAudioCodec.unknown) {} // TODO: disconnect and show error
+//
+//   bool firstTranscriptMade = SharedPreferencesUtil().firstTranscriptMade;
+//
+//   audioStorage = WavBytesUtil(codec: codec);
+//   await initiateChunksProcessing(
+//     btDevice!.id,
+//     codec,
+//     audioStorage,
+//     (newSegments, processedBytes) {
+//       _handleNewSegments(newSegments);
+//       if (segments.isEmpty) audioStorage!.removeFramesRange(fromSecond: 0, toSecond: processedBytes.length ~/ 100);
+//       if (segments.isNotEmpty) elapsedSeconds += processedBytes.length ~/ 100;
+//       if (segments.isNotEmpty && !firstTranscriptMade) {
+//         SharedPreferencesUtil().firstTranscriptMade = true;
+//         MixpanelManager().firstTranscriptMade();
+//         firstTranscriptMade = true;
+//       }
+//     },
+//     (bool value) => setState(() => isTranscribing = value),
+//     _internetStatus,
+//   );
+// }
+
+// _printFileSize(File file) async {
+//   int bytes = await file.length();
+//   var i = (log(bytes) / log(1024)).floor();
+//   const suffixes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+//   var size = '${(bytes / pow(1024, i)).toStringAsFixed(2)} ${suffixes[i]}';
+//   debugPrint('File size: $size');
+// }
+
+// _processFileToTranscript(File f, {bool forceDeepgramTranscription = true}) async {
+//   setState(() => isTranscribing = true);
+//   print('transcribing file: ${f.path}');
+//   await _printFileSize(f);
+//   List<TranscriptSegment> newSegments;
+//   if (SharedPreferencesUtil().useTranscriptServer && !forceDeepgramTranscription) {
+//     newSegments = await transcribe(f);
+//   } else {
+//     newSegments = await deepgramTranscribe(f);
+//   }
+//   _handleNewSegments(newSegments);
+//   setState(() => isTranscribing = false);
+// }
+
+// void _handleNewSegments(List<TranscriptSegment> newSegments) {
+//   TranscriptSegment.combineSegments(segments, newSegments, toAddSeconds: elapsedSeconds); // combines b into a
+//   if (newSegments.isNotEmpty) {
+//     triggerTranscriptSegmentReceivedEvents(newSegments, conversationId, sendMessageToChat: sendMessageToChat);
+//     SharedPreferencesUtil().transcriptSegments = segments;
+//     setState(() {});
+//     setHasTranscripts(true);
+//     debugPrint('Memory creation timer restarted');
+//     _memoryCreationTimer?.cancel();
+//     _memoryCreationTimer = Timer(const Duration(seconds: quietSecondsForMemoryCreation), () => _createMemory());
+//     currentTranscriptStartedAt ??= DateTime.now().subtract(const Duration(seconds: 30));
+//     currentTranscriptFinishedAt = DateTime.now();
+//   }
+//   // _doProcessingOfInstructions();
+//   setState(() => isTranscribing = false);
+// }
 
 mixin AudioChunksMixin {
   Future<StreamSubscription?> initiateChunksProcessing(
--- apps/AppWithWearable/lib/pages/capture/page.dart
+++ apps/AppWithWearable/lib/pages/capture/page.dart
@@ -1,19 +1,15 @@
 import 'dart:async';
 import 'dart:io';
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-import 'package:friend_private/backend/api_requests/api/other.dart';
 import 'package:friend_private/backend/api_requests/api/prompt.dart';
-import 'package:friend_private/backend/api_requests/api/server.dart';
 import 'package:friend_private/backend/api_requests/cloud_storage.dart';
 import 'package:friend_private/backend/database/memory.dart';
 import 'package:friend_private/backend/database/message.dart';
 import 'package:friend_private/backend/database/message_provider.dart';
 import 'package:friend_private/backend/database/transcript_segment.dart';
 import 'package:friend_private/backend/growthbook.dart';
-import 'package:friend_private/backend/mixpanel.dart';
 import 'package:friend_private/backend/preferences.dart';
 import 'package:friend_private/backend/schema/bt_device.dart';
 import 'package:friend_private/pages/capture/location_service.dart';
@@ -68,7 +64,6 @@ class CapturePageState extends State<CapturePage>
   bool _hasTranscripts = false;
   final record = AudioRecorder();
   static const quietSecondsForMemoryCreation = 120;
-  bool _streamingTranscriptEnabled = false;
 
   /// ----
   List<TranscriptSegment> segments = [
@@ -208,72 +203,6 @@ class CapturePageState extends State<CapturePage>
     closeWebSocket();
   }
 
-  // Future<void> initiateBytesProcessing() async {
-  //   debugPrint('initiateBytesProcessing: $btDevice');
-  //   BleAudioCodec codec = await getAudioCodec(btDevice!.id);
-  //   if (codec == BleAudioCodec.unknown) {} // TODO: disconnect and show error
-  //
-  //   bool firstTranscriptMade = SharedPreferencesUtil().firstTranscriptMade;
-  //
-  //   audioStorage = WavBytesUtil(codec: codec);
-  //   await initiateChunksProcessing(
-  //     btDevice!.id,
-  //     codec,
-  //     audioStorage,
-  //     (newSegments, processedBytes) {
-  //       _handleNewSegments(newSegments);
-  //       if (segments.isEmpty) audioStorage!.removeFramesRange(fromSecond: 0, toSecond: processedBytes.length ~/ 100);
-  //       if (segments.isNotEmpty) elapsedSeconds += processedBytes.length ~/ 100;
-  //       if (segments.isNotEmpty && !firstTranscriptMade) {
-  //         SharedPreferencesUtil().firstTranscriptMade = true;
-  //         MixpanelManager().firstTranscriptMade();
-  //         firstTranscriptMade = true;
-  //       }
-  //     },
-  //     (bool value) => setState(() => isTranscribing = value),
-  //     _internetStatus,
-  //   );
-  // }
-
-  // _printFileSize(File file) async {
-  //   int bytes = await file.length();
-  //   var i = (log(bytes) / log(1024)).floor();
-  //   const suffixes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-  //   var size = '${(bytes / pow(1024, i)).toStringAsFixed(2)} ${suffixes[i]}';
-  //   debugPrint('File size: $size');
-  // }
-
-  // _processFileToTranscript(File f, {bool forceDeepgramTranscription = true}) async {
-  //   setState(() => isTranscribing = true);
-  //   print('transcribing file: ${f.path}');
-  //   await _printFileSize(f);
-  //   List<TranscriptSegment> newSegments;
-  //   if (SharedPreferencesUtil().useTranscriptServer && !forceDeepgramTranscription) {
-  //     newSegments = await transcribe(f);
-  //   } else {
-  //     newSegments = await deepgramTranscribe(f);
-  //   }
-  //   _handleNewSegments(newSegments);
-  //   setState(() => isTranscribing = false);
-  // }
-
-  // void _handleNewSegments(List<TranscriptSegment> newSegments) {
-  //   TranscriptSegment.combineSegments(segments, newSegments, toAddSeconds: elapsedSeconds); // combines b into a
-  //   if (newSegments.isNotEmpty) {
-  //     triggerTranscriptSegmentReceivedEvents(newSegments, conversationId, sendMessageToChat: sendMessageToChat);
-  //     SharedPreferencesUtil().transcriptSegments = segments;
-  //     setState(() {});
-  //     setHasTranscripts(true);
-  //     debugPrint('Memory creation timer restarted');
-  //     _memoryCreationTimer?.cancel();
-  //     _memoryCreationTimer = Timer(const Duration(seconds: quietSecondsForMemoryCreation), () => _createMemory());
-  //     currentTranscriptStartedAt ??= DateTime.now().subtract(const Duration(seconds: 30));
-  //     currentTranscriptFinishedAt = DateTime.now();
-  //   }
-  //   // _doProcessingOfInstructions();
-  //   setState(() => isTranscribing = false);
-  // }
-
   void resetState({bool restartBytesProcessing = true, BTDeviceStruct? btDevice}) {
     debugPrint('resetState: $restartBytesProcessing');
     _bleBytesStream?.cancel();
@@ -284,10 +213,6 @@ class CapturePageState extends State<CapturePage>
       startOpenGlass();
       initiateBytesStreamingProcessing();
       // restartWebSocket(); // DO NOT USE FOR NOW, this ties the websocket to the device, and logic is much more complex
-      // if (_streamingTranscriptEnabled) {
-      // } else {
-      //   initiateBytesProcessing();
-      // }
     }
   }
 
@@ -367,7 +292,7 @@ class CapturePageState extends State<CapturePage>
     setState(() => _hasTranscripts = hasTranscripts);
   }
 
-  _processCachedTranscript() async {
+  processCachedTranscript() async {
     debugPrint('_processCachedTranscript');
     var segments = SharedPreferencesUtil().transcriptSegments;
     if (segments.isEmpty) return;
@@ -389,21 +314,14 @@ class CapturePageState extends State<CapturePage>
   @override
   void initState() {
     btDevice = widget.device;
+    WavBytesUtil.clearTempWavFiles();
+    initiateWebsocket();
+    startOpenGlass();
+    initiateBytesStreamingProcessing();
+    processCachedTranscript();
+
     WidgetsBinding.instance.addObserver(this);
     SchedulerBinding.instance.addPostFrameCallback((_) async {
-      debugPrint('SchedulerBinding.instance');
-      // await phoneRecorderInit(_processFileToTranscript);
-      _streamingTranscriptEnabled = GrowthbookUtil().hasStreamingTranscriptFeatureOn();
-      WavBytesUtil.clearTempWavFiles();
-      debugPrint('SchedulerBinding.instance');
-
-      initiateWebsocket();
-      startOpenGlass();
-      initiateBytesStreamingProcessing();
-      // if (_streamingTranscriptEnabled) {
-      // } else {
-      //   initiateBytesProcessing();
-      // }
       if (await LocationService().displayPermissionsDialog()) {
         showDialog(
           context: context,
@@ -421,7 +339,6 @@ class CapturePageState extends State<CapturePage>
         );
       }
     });
-    _processCachedTranscript();
     _internetListener = InternetConnection().onStatusChange.listen((InternetStatus status) {
       switch (status) {
         case InternetStatus.connected:
@@ -434,7 +351,6 @@ class CapturePageState extends State<CapturePage>
           break;
       }
     });
-    // processTranscriptContent(context, '''a''', null);
     super.initState();
   }
 
@@ -521,10 +437,6 @@ class CapturePageState extends State<CapturePage>
 
   _recordingToggled() async {
     if (recordingState == RecordingState.record) {
-      //  await stopRecording(_processFileToTranscript, segments, () {
-      //   _memoryCreationTimer?.cancel();
-      //   _memoryCreationTimer = Timer(const Duration(seconds: 5), () => _createMemory());
-      // });
       await stopStreamRecording(wsConnectionState, websocketChannel);
       setState(() => recordingState = RecordingState.stop);
       _memoryCreationTimer?.cancel();
