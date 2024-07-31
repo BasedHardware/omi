b2b7e27418e8d206a3c42d60b8f87f894f191abe
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-07-30 19:48:19 -0700

    fix delete memory by id endpoint

--- backend/routers/memories.py
+++ backend/routers/memories.py
@@ -7,11 +7,11 @@ from fastapi import APIRouter, Depends, HTTPException
 
 import database.memories as memories_db
 from database.vector import upsert_vector, delete_vector
-from models.memory import Memory, CreateMemory, PluginResponse, CreateMemoryResponse
+from models.memory import Memory, CreateMemory, PluginResult, CreateMemoryResponse
 from models.plugin import Plugin
 from routers.plugins import get_plugins_data
 from utils import auth
-from utils.llm import generate_embedding, get_transcript_structure, get_plugin_result, advise_post_memory_creation
+from utils.llm import generate_embedding, get_transcript_structure, get_plugin_result
 from utils.plugins import trigger_external_integrations
 
 router = APIRouter()
@@ -42,7 +42,7 @@ def _process_memory(uid: str, language_code: str, memory: Union[Memory, CreateMe
     if not discarded:
         structured_str = str(structured)
         vector = generate_embedding(structured_str)
-        upsert_vector(memory.id, vector, uid, structured_str)
+        upsert_vector(memory.id, vector, uid, structured_str)  # TODO: should include more details in vector metadata?
 
         plugins: List[Plugin] = get_plugins_data(uid, include_reviews=False)
         filtered_plugins = [plugin for plugin in plugins if plugin.works_with_memories() and plugin.enabled]
@@ -50,7 +50,7 @@ def _process_memory(uid: str, language_code: str, memory: Union[Memory, CreateMe
 
         def execute_plugin(plugin):
             if result := get_plugin_result(transcript, plugin).strip():
-                memory.plugins_response.append(PluginResponse(plugin_id=plugin.id, content=result))
+                memory.plugins_results.append(PluginResult(plugin_id=plugin.id, content=result))
 
         for plugin in filtered_plugins:
             threads.append(threading.Thread(target=execute_plugin, args=(plugin,)))
@@ -65,9 +65,7 @@ def _process_memory(uid: str, language_code: str, memory: Union[Memory, CreateMe
 @router.post("/v1/memories", response_model=CreateMemoryResponse, tags=['memories'])
 def create_memory(create_memory: CreateMemory, language_code: str, uid: str = Depends(auth.get_current_user_uid)):
     memory = _process_memory(uid, language_code, create_memory)
-    results = trigger_external_integrations(uid, memory)
-    if message := advise_post_memory_creation(memory):  # TODO: this should be a plugin
-        results['memory-feedback-advisor'] = message
+    results = trigger_external_integrations(uid, memory)  # TODO: include as part of plugin response
     return CreateMemoryResponse(memory=memory, messages=results)
 
 
@@ -89,7 +87,7 @@ def get_memories(limit: int = 100, offset: int = 0, uid: str = Depends(auth.get_
 @router.get("/v1/memories/{memory_id}", response_model=Memory, tags=['memories'])
 def get_memory_by_id(memory_id: str, uid: str = Depends(auth.get_current_user_uid)):
     memory = memories_db.get_memory(uid, memory_id)
-    if memory is None:
+    if memory is None or memory.get('deleted', False):
         raise HTTPException(status_code=404, detail="Memory not found")
     return memory
 
