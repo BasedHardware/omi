1fd2fa25104a3715b23693540816a6bf3eeb900d
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-07-30 17:53:34 -0700

    initial refactor + backend added to folder

--- deprecated/backend/app-server/sources/modules/crypto/generateSafeToken.ts
+++
@@ -1,5 +0,0 @@
-import * as crypto from 'crypto';
-
-export async function generateSafeToken() {
-    return crypto.randomBytes(32).toString('hex');
-}
--- firmware/firmware_v1.0/src/mic.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-typedef void (*mix_handler)(int16_t *);
-int mic_start();
-void set_mic_callback(mix_handler _callback);
