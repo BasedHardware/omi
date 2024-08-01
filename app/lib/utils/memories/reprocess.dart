004b5a8fb73eed079c2e4a16dbc2d62ad321833d
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-07-31 20:00:46 -0700

    process and reprocess memory moved to back

--- app/lib/utils/memories/reprocess.dart
+++ app/lib/utils/memories/reprocess.dart
@@ -1,73 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:friend_private/backend/api_requests/api/pinecone.dart';
-import 'package:friend_private/backend/api_requests/api/prompt.dart';
-import 'package:friend_private/backend/database/memory.dart';
-import 'package:friend_private/backend/database/memory_provider.dart';
+import 'package:friend_private/backend/api_requests/api/memories.dart';
+import 'package:friend_private/backend/mixpanel.dart';
+import 'package:friend_private/backend/server/memory.dart';
+import 'package:instabug_flutter/instabug_flutter.dart';
 
-Future<Memory?> reProcessMemory(
+Future<ServerMemory?> reProcessMemory(
   BuildContext context,
-  Memory memory,
+  ServerMemory memory,
   Function onFailedProcessing,
   Function changeLoadingState,
 ) async {
   debugPrint('_reProcessMemory');
   changeLoadingState();
-  SummaryResult summaryResult;
   try {
-    summaryResult = await summarizeMemory(
-      memory.transcript,
-      [],
-      forceProcess: true,
-      conversationDate: memory.createdAt,
-    );
+    var updatedMemory = await reProcessMemoryServer(memory.id);
+    // TODO: replace the current memory field
+    // TODO: handle errors
+    MixpanelManager().reProcessMemory(memory);
+    changeLoadingState();
+    return updatedMemory;
   } catch (err, stacktrace) {
     print(err);
-    // RESTORE ME
-    // var memoryReporting = MixpanelManager().getMemoryEventProperties(memory);
-    // CrashReporting.reportHandledCrash(err, stacktrace, level: NonFatalExceptionLevel.critical, userAttributes: {
-    //   'memory_transcript_length': memoryReporting['transcript_length'].toString(),
-    //   'memory_transcript_word_count': memoryReporting['transcript_word_count'].toString(),
-    //   // 'memory_transcript_language': memoryReporting['transcript_language'], // TODO: this is incorrect
-    // });
+    var memoryReporting = MixpanelManager().getMemoryEventProperties(memory);
+    CrashReporting.reportHandledCrash(err, stacktrace, level: NonFatalExceptionLevel.critical, userAttributes: {
+      'memory_transcript_length': memoryReporting['transcript_length'].toString(),
+      'memory_transcript_word_count': memoryReporting['transcript_word_count'].toString(),
+    });
     onFailedProcessing();
     changeLoadingState();
     return null;
   }
-  // TODO: move this to a method from structured?
-  Structured structured = memory.structured.target!;
-  Structured newStructured = summaryResult.structured;
-  structured.title = newStructured.title;
-  structured.overview = newStructured.overview;
-  structured.emoji = newStructured.emoji;
-  structured.category = newStructured.category;
-
-  structured.actionItems.clear();
-  structured.actionItems.addAll(newStructured.actionItems.map<ActionItem>((i) => ActionItem(i.description)).toList());
-
-  structured.events.clear();
-  for (var event in newStructured.events) {
-    structured.events.add(Event(event.title, event.startsAt, event.duration, description: event.description));
-  }
-
-  memory.structured.target = structured;
-  memory.discarded = false;
-  memory.pluginsResponse.clear();
-  memory.pluginsResponse.addAll(
-    summaryResult.pluginsResponse.map<PluginResponse>((e) => PluginResponse(e.item2, pluginId: e.item1.id)).toList(),
-  );
-
-  // Add Calendar Events
-
-  getEmbeddingsFromInput(structured.toString()).then((vector) {
-    // TODO: update instead if it wasn't "discarded"
-    upsertPineconeVector(memory.id.toString(), vector, memory.createdAt);
-  });
-
-  MemoryProvider().updateMemoryStructured(structured);
-  MemoryProvider().updateMemory(memory);
-  debugPrint('MemoryProvider().updateMemory');
-  changeLoadingState();
-  // RESTORE ME
-  // MixpanelManager().reProcessMemory(memory);
-  return memory;
 }
