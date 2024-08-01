e6651f757cdcf0194a3b4ebad9fcfaf8df1bca93
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-07-31 22:49:54 -0700

    initial message endpoint + plugin message

--- backend/routers/chat.py
+++ backend/routers/chat.py
@@ -1,20 +1,21 @@
 import uuid
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 
 from fastapi import APIRouter, Depends
 
 import database.chat as chat_db
 from models.chat import Message, SendMessageRequest
 from utils import auth
-from utils.llm import qa_rag_prompt
+from utils.llm import qa_rag, initial_chat_message
+from utils.plugins import get_plugins_data
 from utils.rag import retrieve_rag_context
 
 router = APIRouter()
 
 
 @router.post('/v1/messages', tags=['chat'], response_model=Message)
-async def send_message(data: SendMessageRequest, uid: str = Depends(auth.get_current_user_uid)):
+def send_message(data: SendMessageRequest, uid: str = Depends(auth.get_current_user_uid)):
     message = Message(
         id=str(uuid.uuid4()),
         text=data.text,
@@ -30,7 +31,7 @@ async def send_message(data: SendMessageRequest, uid: str = Depends(auth.get_cur
     messages = [Message(**msg) for msg in chat_db.get_messages(uid, limit=10)]
     context_str, memories_id = retrieve_rag_context(uid, messages)
     # print('context_str', context_str)
-    response: str = qa_rag_prompt(context_str, messages)
+    response: str = qa_rag(context_str, messages)
     ai_message = Message(
         id=str(uuid.uuid4()),
         text=response,
@@ -46,6 +47,35 @@ async def send_message(data: SendMessageRequest, uid: str = Depends(auth.get_cur
     return ai_message
 
 
+def initial_message_util(uid: str, plugin_id: Optional[str] = None):
+    plugin = None
+    if plugin_id:
+        plugins = get_plugins_data(uid)
+        plugin = next((p for p in plugins if p.id == plugin_id), None)
+
+    text = initial_chat_message(plugin)
+    ai_message = Message(
+        id=str(uuid.uuid4()),
+        text=text,
+        created_at=datetime.utcnow(),
+        sender='ai',
+        plugin_id=plugin_id,
+        from_external_integration=False,
+        type='text',
+        memories=[],
+    )
+    chat_db.add_message(uid, ai_message.dict())
+    return ai_message
+
+
+@router.post('/v1/initial-message', tags=['chat'], response_model=Message)
+def send_message(plugin_id: str, uid: str = Depends(auth.get_current_user_uid)):
+    return initial_message_util(uid, plugin_id)
+
+
 @router.get('/v1/messages', response_model=List[Message], tags=['chat'])
 def get_messages(uid: str = Depends(auth.get_current_user_uid)):
-    return chat_db.get_messages(uid)
+    messages = chat_db.get_messages(uid)
+    if not messages:
+        return [initial_message_util(uid)]
+    return messages
