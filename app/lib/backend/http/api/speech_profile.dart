4525a124ac05aa9b918f6814ba3b5e4bebe1b1be
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-08-02 23:42:08 -0700

    merge fixes

--- app/lib/backend/http/api/speech_profile.dart
+++ app/lib/backend/http/api/speech_profile.dart
@@ -3,14 +3,15 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:friend_private/backend/http/shared.dart';
+import 'package:friend_private/backend/preferences.dart';
 import 'package:friend_private/backend/schema/sample.dart';
 import 'package:friend_private/env/env.dart';
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart';
 
-Future<bool> userHasSpeakerProfile(String uid) async {
+Future<bool> userHasSpeakerProfile() async {
   var response = await makeApiCall(
-    url: '${Env.apiBaseUrl}v1/speech-profile?uid=$uid',
+    url: '${Env.apiBaseUrl}v1/speech-profile?uid=${SharedPreferencesUtil().uid}',
     // url: 'https://5818-107-3-134-29.ngrok-free.app/v1/speech-profile',
     headers: {},
     method: 'GET',
@@ -21,10 +22,10 @@ Future<bool> userHasSpeakerProfile(String uid) async {
   return jsonDecode(response.body)['has_profile'] ?? false;
 }
 
-Future<List<SpeakerIdSample>> getUserSamplesState(String uid) async {
-  debugPrint('getUserSamplesState for uid: $uid');
+Future<List<SpeakerIdSample>> getUserSamplesState() async {
+  debugPrint('getUserSamplesState for uid: ${SharedPreferencesUtil().uid}');
   var response = await makeApiCall(
-    url: '${Env.apiBaseUrl}samples?uid=$uid',
+    url: '${Env.apiBaseUrl}samples?uid=${SharedPreferencesUtil().uid}',
     headers: {},
     method: 'GET',
     body: '',
@@ -34,11 +35,11 @@ Future<List<SpeakerIdSample>> getUserSamplesState(String uid) async {
   return SpeakerIdSample.fromJsonList(jsonDecode(response.body));
 }
 
-Future<bool> uploadSample(File file, String uid) async {
-  debugPrint('uploadSample ${file.path} for uid: $uid');
+Future<bool> uploadSample(File file) async {
+  debugPrint('uploadSample ${file.path} for uid: ${SharedPreferencesUtil().uid}');
   var request = http.MultipartRequest(
     'POST',
-    Uri.parse('${Env.apiBaseUrl}samples/upload?uid=$uid'),
+    Uri.parse('${Env.apiBaseUrl}samples/upload?uid=${SharedPreferencesUtil().uid}'),
   );
   request.files.add(await http.MultipartFile.fromPath('file', file.path, filename: basename(file.path)));
 
