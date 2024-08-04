b2557b7de38c8c4d3eb010e39782621420a4b58b
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-08-04 19:39:15 +0530

    foreground task ios setup and cleaning

--- app/lib/pages/capture/location_service.dart
+++ app/lib/pages/capture/location_service.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:math';
 
 import 'package:friend_private/backend/database/geolocation.dart';
 import 'package:friend_private/backend/preferences.dart';
@@ -8,47 +7,10 @@ import 'package:friend_private/env/env.dart';
 import 'package:http/http.dart' as http;
 import 'package:location/location.dart';
 
-// Function to calculate the distance between two coordinates using the Haversine formula
-double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
-  // Radius of the Earth in meters
-  const double earthRadius = 6371000;
-  // Convert latitude and longitude from degrees to radians
-  double dLat = (lat2 - lat1) * pi / 180;
-  double dLon = (lon2 - lon1) * pi / 180;
-  // Haversine formula for calculating distance
-  double a =
-      sin(dLat / 2) * sin(dLat / 2) + cos(lat1 * pi / 180) * cos(lat2 * pi / 180) * sin(dLon / 2) * sin(dLon / 2);
-  double c = 2 * atan2(sqrt(a), sqrt(1 - a));
-  // Distance in meters using the Earth's radius
-  double distance = earthRadius * c;
-  // Return the calculated distance
-  return distance;
-}
-
-// Function to check if a coordinate is within a certain range (in meters) of another coordinate
-bool isWithinRange(double lat1, double lon1, double lat2, double lon2, double rangeInMeters) {
-  double distance = calculateDistance(lat1, lon1, lat2, lon2);
-  return distance <= rangeInMeters;
-}
-
 class LocationService {
   Location location = Location();
   StreamSubscription<LocationData>? _locationSubscription;
   List<LocationData> locationDataList = [];
-
-  Future<void> startLocationService() async {
-    if (await hasPermission()) {
-      _locationSubscription = location.onLocationChanged.listen((LocationData locationData) {
-        // if the current location is 20 meters away from the last location, add it to the list
-        if (locationDataList.isEmpty ||
-            isWithinRange(locationDataList.last.latitude!, locationDataList.last.longitude!, locationData.latitude!,
-                locationData.longitude!, 20)) {
-          locationDataList.add(locationData);
-          print("Location data added: ${locationData.latitude}, ${locationData.longitude}");
-        }
-      });
-    }
-  }
 
   void stopLocationService() {
     _locationSubscription?.cancel();
@@ -84,7 +46,6 @@ class LocationService {
     if (permissionGranted == PermissionStatus.denied) {
       permissionGranted = await location.requestPermission();
     }
-    await startLocationService();
     return permissionGranted;
   }
 
