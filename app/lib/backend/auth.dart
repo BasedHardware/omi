2ac9da53005841e99e833ad118b7feb371aaf4b4
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-08-04 21:23:50 -0700

    minor changes

--- app/lib/backend/auth.dart
+++ app/lib/backend/auth.dart
@@ -110,7 +110,10 @@ listenAuthTokenChanges() {
     } else {
       debugPrint('User is signed in!');
       try {
-        SharedPreferencesUtil().authToken = await getIdToken() ?? '';
+        if (SharedPreferencesUtil().authToken.isEmpty ||
+            DateTime.now().millisecondsSinceEpoch > SharedPreferencesUtil().tokenExpirationTime) {
+          await getIdToken();
+        }
       } catch (e) {
         debugPrint('Failed to get token: $e');
       }
@@ -143,18 +146,4 @@ Future<void> signOut(BuildContext context) async {
   // context.pushReplacementNamed('auth');
 }
 
-listenAuthStateChanges() {
-  FirebaseAuth.instance.authStateChanges().listen((User? user) {
-    if (user == null) {
-      debugPrint('User is currently signed out!');
-      SharedPreferencesUtil().onboardingCompleted = false;
-      SharedPreferencesUtil().authToken = '';
-    } else {
-      debugPrint('User is signed in!');
-    }
-  });
-}
-
-Future isSignedIn() async {
-  return FirebaseAuth.instance.currentUser != null;
-}
+bool isSignedIn() => FirebaseAuth.instance.currentUser != null;
