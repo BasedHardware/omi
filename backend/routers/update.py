fd81197afd649cde4484e6477095da98c3440850
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-08-07 01:10:12 +0530

    add firmware version endpoint

---
+++ backend/routers/update.py
@@ -0,0 +1,64 @@
+import os
+import re
+import httpx
+from fastapi import APIRouter, HTTPException, Depends
+from utils import auth
+
+
+router = APIRouter()
+
+@router.get("/latest-version")
+async def get_latest_version():
+    async with httpx.AsyncClient() as client:
+        url = "https://api.github.com/repos/mdmohsin7/Friend/releases/latest"
+        headers = {
+            "Accept": "application/vnd.github+json",
+            "X-GitHub-Api-Version": "2022-11-28",
+            "Authorization": f"Bearer {os.getenv('GITHUB_TOKEN')}",
+        }
+        response = await client.get(url, headers=headers)
+        if response.status_code != 200:
+            raise HTTPException(status_code=response.status_code, detail="Failed to fetch latest release")
+
+        release_data = response.json()
+        kv = extract_key_value_pairs(release_data.get("body"))
+        assets = release_data.get("assets")
+        asset = None
+        for asset in assets:
+            if asset.get("content_type") == "application/zip":
+                asset = asset
+                break
+        return {
+            "version": release_data.get("tag_name"),
+            "id": release_data.get("id"),
+            "tag_name": release_data.get("tag_name"),
+            "published_at": release_data.get("published_at"),
+            "draft": release_data.get("draft"),
+            "prerelease": release_data.get("prerelease"),
+            "zip_url": asset.get("browser_download_url"),
+            "zip_name": asset.get("name"),
+            "zip_size": asset.get("size"),
+            "release_name": release_data.get("name"),
+            "body": kv,
+        }
+
+
+def extract_key_value_pairs(markdown_content):
+    key_value_pattern = re.compile(r'<!-- KEY_VALUE_START\s*(.*?)\s*KEY_VALUE_END -->', re.DOTALL)
+    key_value_match = key_value_pattern.search(markdown_content)
+
+    if not key_value_match:
+        return {}
+
+    key_value_string = key_value_match.group(1)
+    lines = key_value_string.split('\n')
+    key_value_map = {}
+
+    for line in lines:
+        key_value = line.split(':')
+        if len(key_value) == 2:
+            key = key_value[0].strip()
+            value = key_value[1].strip()
+            key_value_map[key] = value
+
+    return key_value_map
