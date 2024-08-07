16229f6afbc8058a318acae977fdbf18c9796204
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-08-07 23:34:06 +0530

    add GITHUB_TOKEN to env

--- backend/routers/update.py
+++ backend/routers/update.py
@@ -11,7 +11,7 @@ router = APIRouter()
 async def get_latest_version():
     async with httpx.AsyncClient() as client:
         # TODO: Change this to the official repo
-        url = "https://api.github.com/repos/mdmohsin7/Friend/releases/latest"
+        url = "https://api.github.com/repos/basedhardware/Friend/releases/latest"
         headers = {
             "Accept": "application/vnd.github+json",
             "X-GitHub-Api-Version": "2022-11-28",
