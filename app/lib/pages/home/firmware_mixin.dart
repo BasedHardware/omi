ae3470850bae5be1480c1975838df0e6ad24ffa3
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-08-10 22:19:57 +0530

    ui improvements

--- app/lib/pages/home/firmware_mixin.dart
+++ app/lib/pages/home/firmware_mixin.dart
@@ -70,16 +70,18 @@ mixin FirmwareMixin<T extends StatefulWidget> on State<T> {
     );
   }
 
-  Future getLatestVersion() async {
-    var res = await makeApiCall(url: "${Env.apiBaseUrl}latest-version", headers: {}, body: '', method: 'GET');
+  Future getLatestVersion({required String deviceName}) async {
+    int device = deviceName == 'Friend' ? 1 : 2;
+    var res = await makeApiCall(
+        url: "${Env.apiBaseUrl}v1/firmware/latest?device=$device", headers: {}, body: '', method: 'GET');
     var body = jsonDecode(res!.body);
     latestFirmwareDetails = body;
   }
 
-  Future<(String, bool)> shouldUpdateFirmware(String currentFirmware) async {
+  Future<(String, bool)> shouldUpdateFirmware({required String currentFirmware, required String deviceName}) async {
     Version currentVersion = Version.parse(currentFirmware);
     if (latestFirmwareDetails.isEmpty) {
-      await getLatestVersion();
+      await getLatestVersion(deviceName: deviceName);
     }
     if (latestFirmwareDetails.isEmpty || latestFirmwareDetails['version'] == null) {
       return ('Latest Version Not Available', false);
