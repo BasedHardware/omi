cfd439315efab756c0e3b241ae179992a5b3be80
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-08-11 12:31:09 -0700

    notifications PR fixes P2

--- app/lib/backend/http/api/notifications.dart
+++ app/lib/backend/http/api/notifications.dart
@@ -4,20 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:friend_private/backend/http/shared.dart';
 import 'package:friend_private/env/env.dart';
 
-Future<void> saveTokenToBackend({
-  required String userId,
-  required String token,
-  required String timeZone,
-}) async {
+Future<void> saveFcmTokenServer({required String token, required String timeZone}) async {
   var response = await makeApiCall(
     url: '${Env.apiBaseUrl}v1/users/fcm-token',
     headers: {'Content-Type': 'application/json'},
     method: 'POST',
-    body: jsonEncode({
-      'user_id': userId,
-      'token': token,
-      'time_zone': timeZone,
-    }),
+    body: jsonEncode({'token': token, 'time_zone': timeZone}),
   );
 
   debugPrint('saveToken: ${response?.body}');
