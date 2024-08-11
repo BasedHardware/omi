530255b8135ac13721557e9605dba64d4b1bf63d
Vincent Koc <vincentkoc@ieee.org>
2024-08-10 18:16:35 -0700

    Update sdcard.h

--- Friend/firmware/firmware_v1.0/src/sdcard.h
+++ Friend/firmware/firmware_v1.0/src/sdcard.h
@@ -7,10 +7,12 @@
 
 #define MAX_PATH_SIZE 32 //1024
 #define MAX_DATA_SIZE 2048 //2048
+#define MAX_PATH_SIZE 64
 
 extern bool sd_card_mounted;
-bool is_sd_card_inserted(void);
+
 int mount_sd_card(void);
+int create_directory(const char *dir_path);
 int create_file(const char *file_path);
 int write_file(const char *file_path, const uint8_t *data, size_t length, bool append);
 int read_file(const char *file_path, uint8_t *buffer, size_t buffer_size, size_t *bytes_read);
