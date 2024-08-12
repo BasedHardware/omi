3a7084f0d6994594c0b146dfa50c51cc161e6dbf
Vincent Koc <vincentkoc@ieee.org>
2024-08-11 23:18:29 -0700

    Update utils.h

--- Friend/firmware/firmware_v1.0/src/utils.h
+++ Friend/firmware/firmware_v1.0/src/utils.h
@@ -5,18 +5,13 @@
 #define ASSERT_OK(result)                                          \
     if ((result) < 0)                                              \
     {                                                              \
-        printk("Error at %s:%d:%d\n", __FILE__, __LINE__, result); \
+        LOG_ERR("Error at %s:%d:%d", __FILE__, __LINE__, result); \
         return (result);                                           \
     }
 
 #define ASSERT_TRUE(result)                                        \
     if (!result)                                                   \
     {                                                              \
-        printk("Error at %s:%d:%d\n", __FILE__, __LINE__, result); \
+        LOG_ERR("Error at %s:%d:%d", __FILE__, __LINE__, result); \
         return -1;                                                 \
     }
-
-// #define WAIT_LOG k_sleep(K_MSEC(200));
-// #define WAIT_LOG do {} while(0);
-// #define WAIT_LOG z_impl_log_process()
-// #define WAIT_LOG while (z_impl_log_process() == true) { }
