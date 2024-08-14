df4bd06bc939f6d6f4c8ecb1e740b56fc840baca
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-08-13 22:26:21 -0700

    fucking attempt to make opus speech profile work

--- backend/utils/stt/deepgram_util.py
+++ backend/utils/stt/deepgram_util.py
@@ -1,19 +1,17 @@
 import asyncio
 import os
+import subprocess
 import threading
 import time
-from typing import Tuple, Optional
 
+import pyogg
 import requests
 from deepgram import DeepgramClient, DeepgramClientOptions, LiveTranscriptionEvents
 from deepgram.clients.live.v1 import LiveOptions
-from pydub import AudioSegment
 from starlette.websockets import WebSocket
 
 import database.notifications as notification_db
 from utils.plugins import trigger_realtime_integrations
-from utils.storage import retrieve_all_samples
-from utils.stt.vad import vad_is_empty
 
 headers = {
     "Authorization": f"Token {os.getenv('DEEPGRAM_API_KEY')}",
@@ -68,9 +66,6 @@ def transcribe_file_deepgram(file_path: str, language: str = 'en'):
 async def send_initial_file(file_path, transcript_socket):
     with open(file_path, "rb") as file:
         data = file.read()
-    # increase chunk size
-    # 2.5 seconds per second.
-    #
     start = time.time()
     chunk_size = 4096  # Adjust as needed
     for i in range(0, len(data), chunk_size):
@@ -78,82 +73,25 @@ async def send_initial_file(file_path, transcript_socket):
         transcript_socket.send(chunk)
         await asyncio.sleep(0.01)  # Small delay to prevent overwhelming the socket
     print('send_initial_file', time.time() - start)
-    # os.remove(file_path)
 
 
-# def remove_downloaded_samples(uid):
-#     path = f'_samples/{uid}/'
-#     for file in os.listdir(path):
-#         # remove except joined_output.wav
-#         if file != 'joined_output.wav':
-#             os.remove(f"{path}/{file}")
+async def send_initial_file2(file_path, transcript_socket):
+    subprocess.run(['ffmpeg', '-i', file_path, '-c:a', 'libopus', file_path.replace('.wav', '.opus')], check=True)
+    opus_file = pyogg.OpusFile(file_path.replace('.wav', '.opus'))
+    chunk_size = 4096  # Adjust as needed
+    start = time.time()
+    print('send_initial_file', len(opus_file.buffer))
+    # Reading and sending in chunks
+    for i in range(0, len(opus_file.buffer), chunk_size):
+        opus_chunk = opus_file.buffer[i:i + chunk_size]
+        print(opus_chunk)
+        transcript_socket.send(opus_chunk)
+        await asyncio.sleep(0.01)  # Small delay to prevent overwhelming the socket
 
-
-def get_single_file(dir_path: str, target_sample_rate: int = 8000):
-    output_path = f'{dir_path}joined_output.wav'
-
-    files_to_join = []
-    for sample in os.listdir(dir_path):
-        if '.wav' not in sample:
-            continue
-        if 'joined_output.wav' in sample:
-            continue
-        path = f'{dir_path}{sample}'
-        aseg = AudioSegment.from_file(path)
-        # print(path, aseg.frame_rate)
-        if aseg.frame_rate == target_sample_rate:
-            files_to_join.append(aseg)
-
-    if not files_to_join:
-        return None
-    output = files_to_join[0]
-    for audio in files_to_join[1:]:
-        output += audio
-
-    if os.path.exists(output_path):
-        os.remove(output_path)
-    output.export(output_path, format='wav')
-    return output_path
+    print('send_initial_file', time.time() - start)
 
 
 # Add this new function to handle initial file sending
-def get_speaker_audio_file(uid: str, target_sample_rate: int = 8000) -> Tuple[Optional[str], int]:
-    path = retrieve_all_samples(uid)
-    files_at_path = len(os.listdir(path))
-    print('get_speaker_audio_file', uid, path, 'Files:', files_at_path)
-    if files_at_path < 5:  # means user did less than 5 samples unfortunately, so not completed
-        return None, 0
-    single_file_path = f'{path}joined_output.wav'
-    if os.path.exists(single_file_path):
-        aseg = AudioSegment.from_wav(single_file_path)
-        if aseg.frame_rate == target_sample_rate:  # sample sample rate
-            print('get_speaker_audio_file Cached Duration:', aseg.duration_seconds)
-            if aseg.duration_seconds > 30:
-                aseg = aseg[:30 * 1000]
-                aseg.export(single_file_path, format="wav")
-            return single_file_path, aseg.duration_seconds
-
-    single_file_path = get_single_file(path, target_sample_rate)
-    if not single_file_path:
-        return None, 0  # no files for this codec
-
-    aseg = AudioSegment.from_wav(single_file_path)
-    print('get_speaker_audio_file Initial Duration:', aseg.duration_seconds, 'Sample rate:', aseg.frame_rate / 1000)
-    output = AudioSegment.empty()
-    segments = vad_is_empty(single_file_path, return_segments=True)
-    for segment in segments:
-        start = segment['start'] * 1000
-        end = segment['end'] * 1000
-        output += aseg[start:end]
-
-    seconds = 30
-    if output.duration_seconds < seconds:
-        print('get_speaker_audio_file Output Duration:', output.duration_seconds)
-        return single_file_path, output.duration_seconds  # < 30
-
-    output = output[:seconds * 1000]
-    output.export(single_file_path, format="wav")
-    return single_file_path, seconds
 
 
 deepgram = DeepgramClient(os.getenv('DEEPGRAM_API_KEY'), DeepgramClientOptions(options={"keepalive": "true"}))
@@ -167,11 +105,11 @@ async def process_audio_dg(
     loop = asyncio.get_event_loop()
 
     def on_message(self, result, **kwargs):
-        # print(f"Received message from Deepgram")  # Log when message is received
+        print(f"Received message from Deepgram")  # Log when message is received
         sentence = result.channel.alternatives[0].transcript
         if len(sentence) == 0:
             return
-        # print(sentence)
+        print(sentence)
         segments = []
         for word in result.channel.alternatives[0].words:
             is_user = True if word.speaker == 0 and preseconds > 0 else False
