a166994bdb70ca461482ec2eeafebc3977460a96
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-08-14 16:44:59 -0700

    vad code restored

--- backend/models/other.py
+++ backend/models/other.py
@@ -1,6 +1,13 @@
+from typing import List
+
 from pydantic import BaseModel
 
 
 class SaveFcmTokenRequest(BaseModel):
     fcm_token: str
     time_zone: str
+
+
+class UploadProfile(BaseModel):
+    bytes: List[List[int]]
+    duration: int
