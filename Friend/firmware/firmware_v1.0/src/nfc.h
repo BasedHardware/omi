59d0be576b66b8ac37c353e009374736431e0abd
Vincent Koc <vincentkoc@ieee.org>
2024-08-17 23:14:16 -0700

    Update nfc.h

--- Friend/firmware/firmware_v1.0/src/nfc.h
+++ Friend/firmware/firmware_v1.0/src/nfc.h
@@ -1,6 +1,9 @@
 #ifndef NFC_H
 #define NFC_H
 
+int nfc_sleep(void);
+int nfc_wake(void);
+
 #include <zephyr/kernel.h>
 
 /**
@@ -22,5 +25,16 @@ int nfc_init(void);
  * @return 0 if successful, negative errno code if error
  */
 int nfc_update_payload(const uint8_t *new_data, size_t len);
+
+/**
+ * @brief Get Device ID
+ *
+ * This function fetches the hardware device ID.
+ *
+ * @param device_id Device ID pointer
+ * @param len Length of the device ID
+ * @return 0 if successful, negative errno code if error
+ */
+int get_device_id(char *device_id, size_t len);
 
 #endif /* NFC_H */
