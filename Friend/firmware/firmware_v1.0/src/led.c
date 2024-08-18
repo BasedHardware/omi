b1861e551f1dc9adbf04539cd7dae920bdb4ce77
Neo <54811660+Neotastisch@users.noreply.github.com>
2024-08-18 15:44:32 +1000

    white led for debugging #2

--- Friend/firmware/firmware_v1.0/src/led.c
+++ Friend/firmware/firmware_v1.0/src/led.c
@@ -4,14 +4,21 @@
 #include "utils.h"
 
 LOG_MODULE_REGISTER(led, CONFIG_LOG_DEFAULT_LEVEL);
+
 int led_start()
 {
     ASSERT_TRUE(gpio_is_ready_dt(&led_red));
     ASSERT_OK(gpio_pin_configure_dt(&led_red, GPIO_OUTPUT_INACTIVE));
+
     ASSERT_TRUE(gpio_is_ready_dt(&led_green));
     ASSERT_OK(gpio_pin_configure_dt(&led_green, GPIO_OUTPUT_INACTIVE));
+
     ASSERT_TRUE(gpio_is_ready_dt(&led_blue));
     ASSERT_OK(gpio_pin_configure_dt(&led_blue, GPIO_OUTPUT_INACTIVE));
+
+    ASSERT_TRUE(gpio_is_ready_dt(&led_white));  // Add this line for the white LED
+    ASSERT_OK(gpio_pin_configure_dt(&led_white, GPIO_OUTPUT_INACTIVE));  // Configure the white LED
+
     LOG_INF("LEDs started");
     return 0;
 }
@@ -29,4 +36,9 @@ void set_led_green(bool on)
 void set_led_blue(bool on)
 {
     gpio_pin_set_dt(&led_blue, on);
+}
+
+void set_led_white(bool on)
+{
+    gpio_pin_set_dt(&led_white, on);  // Add this function to control the white LED
 }
