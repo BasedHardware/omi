d55ccf7c5f3b97da0fc991cba55d002098f75cab
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-08-18 20:02:17 -0700

    quick test minor restore of speaker_profile matching

--- backend/utils/other/storage.py
+++ backend/utils/other/storage.py
@@ -24,6 +24,19 @@ def upload_profile_audio(file_path: str, uid: str):
     return f'https://storage.googleapis.com/{speech_profiles_bucket}/{path}'
 
 
+def get_profile_audio_if_exists(uid: str) -> str:
+    bucket = storage_client.bucket(speech_profiles_bucket)
+    path = f'{uid}/speech_profile.wav'
+    blob = bucket.blob(path)
+    if blob.exists():
+        # download and return file path
+        file_path = f'_temp/{uid}_speech_profile.wav'
+        blob.download_to_filename(file_path)
+        return file_path
+
+    return None
+
+
 def upload_postprocessing_audio(file_path: str):
     bucket = storage_client.bucket(postprocessing_audio_bucket)
     blob = bucket.blob(file_path)
