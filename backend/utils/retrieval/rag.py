2ef2276fc0957e2dddc2cdba4e5eee13ef96dcde
thịnh <ngocthinh@lozi.vn>
2024-08-21 14:45:36 +0700

    Updates: Use transcript to detemize the context instead of dump fetch latest memories

--- backend/utils/retrieval/rag.py
+++ backend/utils/retrieval/rag.py
@@ -8,7 +8,7 @@ from models.chat import Message
 from models.memory import Memory
 from models.transcript_segment import TranscriptSegment
 from utils.llm import requires_context, retrieve_context_params, retrieve_context_dates, chunk_extraction, \
-    num_tokens_from_string, determine_requires_memory_context
+    num_tokens_from_string, retrieve_memory_context_params
 
 
 def retrieve_for_topic(uid: str, topic: str, start_timestamp, end_timestamp, k: int, memories_id) -> List[str]:
@@ -109,27 +109,37 @@ def retrieve_rag_context(
     return context_str, (memories if context_str else [])
 
 
-def retrieve_rag_memory_context(uid: str, prev_memories: List[Memory]) -> Tuple[str, List[Memory]]:
-    context = determine_requires_memory_context(prev_memories)
-    if not context or (not context[0] and not context[1]):
+def retrieve_rag_memory_context(uid: str, memory: Memory) -> Tuple[str, List[Memory]]:
+    topics = retrieve_memory_context_params(memory)
+    print('retrieve_memory_rag_context', topics)
+    if not topics:
         return '', []
 
-    topics = context[0]
-    dates_range = context[1]
-    start_timestamp = dates_range[0].timestamp() if dates_range else None
-    end_timestamp = dates_range[1].timestamp() if dates_range else None
+    if len(topics) > 5:
+        topics = topics[:5]
 
-    def retrieve_for_topic(topic: str) -> List[str]:
-        return query_vectors(topic, uid, starts_at=start_timestamp, ends_at=end_timestamp)
-
-    memories_id = []
+    memories_id_to_topics = {}
     if topics:
-        for topic in topics:
-            memories_id.extend(retrieve_for_topic(topic))
-        memories = get_memories_by_id(uid, memories_id)
-    else:
-        memories = filter_memories_by_date(uid, dates_range[0], dates_range[1])
-    print('Found', len(memories), 'memories for context')
-    memories = [Memory(**memory) for memory in memories]
+        memories_id_to_topics, memories = retrieve_memories_for_topics(uid, topics, [])
+        id_counter = Counter(memory['id'] for memory in memories)
+        memories = sorted(memories, key=lambda x: id_counter[x['id']], reverse=True)
 
-    return Memory.memories_to_string(memories), memories
+    memories = [Memory(**memory) for memory in memories]
+    if len(memories) > 10:
+        memories = memories[:10]
+
+    if memories_id_to_topics:
+        # TODO: restore sorthing here
+        context_data = {}
+        threads = []
+        for memory in memories:
+            topics = memories_id_to_topics.get(memory.id, [])
+            t = threading.Thread(target=get_better_memory_chunk, args=(memory, topics, context_data))
+            threads.append(t)
+        [t.start() for t in threads]
+        [t.join() for t in threads]
+        context_str = '\n'.join(context_data.values()).strip()
+    else:
+        context_str = Memory.memories_to_string(memories)
+
+    return context_str, (memories if context_str else [])
