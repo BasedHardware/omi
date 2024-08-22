565f146f3e7731f8a234a4778bcc932014d8238c
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-08-22 13:18:32 -0700

    removed VAD

---
+++ backend/scripts/rag/mem0_test.py
@@ -0,0 +1,8 @@
+from mem0 import Memory
+
+m = Memory()
+
+
+def test_10_memories():
+    result = m.add("Likes to play cricket on weekends", user_id="alice", metadata={"category": "hobbies"})
+    pass
