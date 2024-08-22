c09884440206ebd56693cca0133c77d88c8a894d
Becca-Saka <thebeccasaka@gmail.com>
2024-08-22 06:03:23 +0100

    calendar

--- app/lib/utils/alerts/app_snackbar.dart
+++ app/lib/utils/alerts/app_snackbar.dart
@@ -2,16 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:friend_private/main.dart';
 
 class AppSnackbar {
-  static void showSnackbar(String message, {Color? color}) {
+  static void showSnackbar(String message, {Color? color, Duration? duration}) {
     ScaffoldMessenger.of(MyApp.navigatorKey.currentState!.context).showSnackBar(
       SnackBar(
         content: Text(message),
-        backgroundColor: color ?? Colors.red,
+        backgroundColor: color,
       ),
     );
   }
 
-  static void showSnackbarError(String message) {
-    showSnackbar(message, color: Colors.red);
+  static void showSnackbarError(String message, {Duration? duration}) {
+    showSnackbar(
+      message,
+      color: Colors.red,
+      duration: duration,
+    );
   }
 }
