da8077b20ba739015652bc04e000bec64b3a6127
kevvz <92408564+kevvz@users.noreply.github.com>
2024-08-22 09:17:29 -0700

    Update gatt_utils.dart

--- app/lib/utils/ble/gatt_utils.dart
+++ app/lib/utils/ble/gatt_utils.dart
@@ -12,9 +12,6 @@ const String audioCodecCharacteristicUuid = '19b10002-e8f2-537e-4f6c-d104768a121
 const String imageDataStreamCharacteristicUuid = '19b10005-e8f2-537e-4f6c-d104768a1214';
 const String imageCaptureControlCharacteristicUuid = '19b10006-e8f2-537e-4f6c-d104768a1214';
 
-const String accelDataStreamServiceUuid  = '32403790-0000-1000-7450-bf445e5829a2';
-const String accelDataStreamCharacteristicUuid  = '32403791-0000-1000-7450-bf445e5829a2';
-
 const String batteryServiceUuid = '0000180f-0000-1000-8000-00805f9b34fb';
 const String batteryLevelCharacteristicUuid = '00002a19-0000-1000-8000-00805f9b34fb';
 
