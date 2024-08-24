610deaebffae8fd920acb81ab768918747484650
Becca-Saka <thebeccasaka@gmail.com>
2024-08-24 23:16:09 +0100

    fix: summary time

--- backend/utils/other/notifications.py
+++ backend/utils/other/notifications.py
@@ -18,7 +18,7 @@ async def start_cron_job():
 
 def should_run_job():
     current_utc = datetime.now(pytz.utc)
-    target_hours = {8, 20, 22}
+    target_hours = {8, 22}
 
     for tz in pytz.all_timezones:
         local_time = current_utc.astimezone(pytz.timezone(tz))
@@ -29,7 +29,7 @@ def should_run_job():
 
 async def send_daily_summary_notification():
     try:
-        daily_summary_target_time = "20:00"
+        daily_summary_target_time = "22:00"
         timezones_in_time = _get_timezones_at_time(daily_summary_target_time)
         user_in_time_zone =  await notification_db.get_users_id_in_timezones(timezones_in_time)
         if not user_in_time_zone:
@@ -79,20 +79,11 @@ async def send_daily_notification():
     try:
         morning_alert_title = "Don\'t forget to wear Friend today"
         morning_alert_body = "Wear your friend and capture your memories today."
-
-        daily_summary_title = "Here is your action plan for tomorrow"
-        daily_summary_body = "Check out your daily summary to see what you should do tomorrow."
         morning_target_time = "08:00"
-        daily_summary_target_time = "22:00"
 
-        morning_task = asyncio.create_task(
-            _send_notification_for_time(morning_target_time, morning_alert_title, morning_alert_body)
-        )
-        evening_task = asyncio.create_task(
-            _send_notification_for_time(daily_summary_target_time, daily_summary_title, daily_summary_body)
-        )
-
-        await asyncio.gather(morning_task, evening_task)
+     
+        await  _send_notification_for_time(morning_target_time, morning_alert_title, morning_alert_body)
+      
 
     except Exception as e:
         print(e)
