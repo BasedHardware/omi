49eb8b896d9c9e5cbbc730b78a68a9c36abae943
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-08-26 11:29:36 -0700

    CRUD user people

--- backend/models/other.py
+++ backend/models/other.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import List
 
 from pydantic import BaseModel
@@ -11,3 +12,15 @@ class SaveFcmTokenRequest(BaseModel):
 class UploadProfile(BaseModel):
     bytes: List[List[int]]
     duration: int
+
+
+class CreatePerson(BaseModel):
+    name: str
+
+
+class Person(BaseModel):
+    id: str
+    name: str
+    created_at: datetime
+    updated_at: datetime
+    deleted: bool = False
