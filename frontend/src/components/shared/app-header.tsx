31d173591d720193b7b36d69d67347ae37bf5f19
Andres Sanabria <sanabria80.andres@gmail.com>
2024-08-27 13:56:08 -0500

    feat: UI events, add BH logo

--- frontend/src/components/shared/app-header.tsx
+++ frontend/src/components/shared/app-header.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { ShareIos } from 'iconoir-react';
+import Image from 'next/image';
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
 export default function AppHeader() {
@@ -19,22 +22,25 @@ export default function AppHeader() {
 
   return (
     <header
-      className={`sticky top-0 flex items-center justify-between p-4 text-white backdrop-blur-md transition-all duration-500 ${
+      className={`sticky top-0 flex items-center justify-between p-4 text-white backdrop-blur-md transition-all duration-500 px-12 ${
         scrollPosition > 100 ? 'bg-black bg-opacity-10' : ''
       }`}
     >
-      <h1 className="text-xl">Based Hardware</h1>
+      <h1 className="text-xl flex gap-2 items-center">
+        <Image src={'/logo.webp'} alt="Based Hardware Logo" width={25} height={30} />
+        Based Hardware</h1>
       <nav>
         <ul className="flex space-x-4">
           <li>
-            <a href="/" className="hover:underline">
-              Home
-            </a>
+            <button className='flex gap-2 items-center border border-solid border-zinc-600 p-1.5 px-3.5 rounded-md hover:bg-zinc-800 transition-colors'>
+              <ShareIos className='text-xs'/>
+              Share
+            </button>
           </li>
           <li>
-            <a href="/memories" className="hover:underline">
-              Memories
-            </a>
+            <Link href={`https://basedhardware.com/`} target='_blank' className='flex gap-2 items-center p-1.5 px-3.5 rounded-md bg-white/90 hover:bg-white text-black transition-colors'>
+              Order now
+            </Link>
           </li>
         </ul>
       </nav>
