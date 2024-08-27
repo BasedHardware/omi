f724e4a5efcbf273fb480711c65832a59e808e21
Andres Sanabria <sanabria80.andres@gmail.com>
2024-08-26 21:02:10 -0500

    feat: initial header, apply font

--- frontend/src/app/memories/[id]/page.tsx
+++ frontend/src/app/memories/[id]/page.tsx
@@ -2,7 +2,7 @@ export default function MemoryPage({ params }) {
   const memoryId = params.id;
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
       Memory page {memoryId}
     </main>
   );
