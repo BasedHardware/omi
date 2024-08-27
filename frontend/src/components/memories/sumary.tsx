09750bbd6b57cd49fc1a80fe471c5aff32fe2ba1
Andres Sanabria <sanabria80.andres@gmail.com>
2024-08-27 05:55:51 -0500

    feat: Initial tab UI, first transcript component

---
+++ frontend/src/components/memories/sumary.tsx
@@ -0,0 +1,22 @@
+import { Memory } from "@/src/types/memory.types";
+import ActionItems from "./action-items";
+
+interface SummaryProps {
+  memory: Memory;
+}
+
+export default function Summary({ memory }: SummaryProps){
+  return(
+    <div className="flex flex-col gap-10">
+    <div className="mt-10">
+      <h3 className="text-2xl font-semibold">
+        Overview
+      </h3>
+      <p className="text-lg mt-2">
+        {memory.structured.overview}
+      </p>
+    </div>
+    <ActionItems items={memory.structured.action_items} />
+  </div>
+  )
+}
