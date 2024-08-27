91f2e0db47f2176f543420452ab326e5df6d2133
Andres Sanabria <sanabria80.andres@gmail.com>
2024-08-27 17:28:46 -0500

    feat: share button functionality, add sitemap and robots

---
+++ frontend/src/components/memories/share-button.tsx
@@ -0,0 +1,20 @@
+import { ShareIos } from "iconoir-react";
+
+export default function ShareButton() {
+  
+  const handleCopy = () => {
+    const url = window.location.href;
+    navigator.clipboard.writeText(url).then(() => {
+      console.log('URL copied to clipboard');
+    }).catch(err => {
+      console.error('Failed to copy URL: ', err);
+    });
+  };
+
+  return (
+    <button onClick={handleCopy} className="flex items-center gap-2 rounded-md border border-solid border-zinc-600 p-1.5 transition-colors hover:bg-zinc-800 md:p-1.5 md:px-3.5">
+      <ShareIos className="text-xs" />
+      <span className="hidden md:inline">Share</span>
+    </button>
+  );
+}
