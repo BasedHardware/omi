3c53c612f1bf6ea0d2693bdfec23f00131f3ab72
Andres Sanabria <sanabria80.andres@gmail.com>
2024-08-26 19:50:50 -0500

    feat: memories url navigation

---
+++ frontend/src/app/memories/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      This page does not exist.
+    </main>
+  );
+}
