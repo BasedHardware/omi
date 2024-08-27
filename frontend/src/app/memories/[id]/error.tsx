4af7a77e0b19b19e4c776ccc6aff0385aad0cdd6
Andres Sanabria <sanabria80.andres@gmail.com>
2024-08-27 14:00:18 -0500

    fix: Error card UI

--- frontend/src/app/memories/[id]/error.tsx
+++ frontend/src/app/memories/[id]/error.tsx
@@ -3,8 +3,11 @@
 export default function Error() {
   console.log('Memory not found');
   return (
-    <div>
-      <h1>Memory not found</h1>
+    <div className="mx-auto my-28 max-w-screen-md rounded-2xl border border-solid border-zinc-800 py-12 text-white px-12">
+      <h1 className="text-xl font-semibolds">Memory not found</h1>
+      <p className="text-lg text-zinc-400 mt-3">
+        The memory you are looking for does not exist. Please check the URL and try again.
+      </p>
     </div>
   );
 }
