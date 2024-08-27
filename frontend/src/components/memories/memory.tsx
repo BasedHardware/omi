66699fa0388a072f53f7a6c885b2cde6273c1596
Andres Sanabria <sanabria80.andres@gmail.com>
2024-08-27 05:16:13 -0500

    feat: show some information

--- frontend/src/components/memories/memory.tsx
+++ frontend/src/components/memories/memory.tsx
@@ -1,9 +1,28 @@
 import { Memory as MemoryType } from "@/src/types/memory.types";
+import moment from "moment";
+import ActionItems from "./action-items";
 
 export default function Memory({ memory }: { memory: MemoryType }){
   return (
     <div className="text-white">
-      {JSON.stringify(memory)}
+      <h2>
+        {memory.structured.title}
+      </h2>
+      <p>
+        {moment(memory.created_at).format("MMMM Do YYYY, h:mm:ss a")}
+      </p>
+      <span>
+        {memory.structured.emoji} {memory.structured.category}
+      </span>
+      <div>
+        <h3>
+          Overview
+        </h3>
+        <p>
+          {memory.structured.overview}
+        </p>
+      </div>
+      <ActionItems items={memory.structured.action_items} />
     </div>
   )
 }
