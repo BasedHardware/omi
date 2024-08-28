66aecf7f1da6988ea50dc84a8f0489663dafecd9
Andres Sanabria <sanabria80.andres@gmail.com>
2024-08-27 19:44:40 -0500

    feat: lint format

--- frontend/src/app/memories/layout.tsx
+++ frontend/src/app/memories/layout.tsx
@@ -1,6 +1,7 @@
 import AppHeader from '@/src/components/shared/app-header';
+import { ReactNode } from 'react';
 
-export default function MemoriesLayout({ children }) {
+export default function MemoriesLayout({ children }: { children: ReactNode }) {
   return (
     <div>
       <AppHeader />
