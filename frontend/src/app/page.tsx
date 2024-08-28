0f38605ef2beecb745e93e135215154b25fef777
Andres Sanabria <sanabria80.andres@gmail.com>
2024-08-28 08:08:03 -0500

    fix: 404 not found messages

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,32 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Home() {
-  return <main className="">Hello world</main>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-12 text-xl text-white md:p-24 md:text-lg">
+      <div>
+        <Image
+          src={'/logo.webp'}
+          alt="Based Hardware Logo"
+          width={100}
+          height={64}
+          className="h-auto w-[50px] mx-auto"
+        />
+        <h2 className='text-xl md:text-2xl mt-2 text-center'>
+          We are working in this feature
+        </h2>
+        <p className='text-center max-w-xl mt-10'>
+        For now, you can order Omi wearable. Remember
+        everything you want to remember with Omi.
+        </p>
+        <Link
+          href={`https://basedhardware.com/`}
+          target="_blank"
+          className="flex items-center gap-2 rounded-md w-fit mx-auto bg-white/90 mt-10 p-1.5 px-3.5 text-black transition-colors hover:bg-white"
+        >
+          Order now
+        </Link>
+      </div>
+    </main>
+  );
 }
