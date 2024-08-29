5345798e779989c908bb55e20b99efbdb9143634
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-08-28 23:20:26 -0700

    minor UI improvement speaker

--- app/lib/backend/schema/person.dart
+++ app/lib/backend/schema/person.dart
@@ -1,9 +1,40 @@
+import 'package:flutter/material.dart';
+
+final List<Color> speakerColors = [
+  Colors.white
+  // Colors.red,
+  // Colors.blue,
+  // Colors.green,
+  // Colors.yellow,
+  // Colors.purple,
+  // Colors.orange,
+  // Colors.pink,
+  // Colors.teal,
+  // Colors.cyan,
+  // Colors.amber,
+];
+
+final List<String> speakerImagePath = [
+  'assets/images/speaker_1_icon.png',
+  // 'assets/images/speaker_1_red.png',
+  // 'assets/images/speaker_1_blue.png',
+  // 'assets/images/speaker_1_green.png',
+  // 'assets/images/speaker_1_yellow.png',
+  // 'assets/images/speaker_1_purple.png',
+  // 'assets/images/speaker_1_orange.png',
+  // 'assets/images/speaker_1_pink.png',
+  // 'assets/images/speaker_1_teal.png',
+  // 'assets/images/speaker_1_cyan.png',
+  // 'assets/images/speaker_1_amber.png',
+];
+
 class Person {
   final String id;
   final String name;
   final DateTime createdAt;
   final DateTime updatedAt;
   final List<String>? speechSamples;
+  final int? colorIdx;
 
   Person({
     required this.id,
@@ -11,6 +42,7 @@ class Person {
     required this.createdAt,
     required this.updatedAt,
     this.speechSamples,
+    this.colorIdx,
   });
 
   factory Person.fromJson(Map<String, dynamic> json) {
@@ -20,6 +52,7 @@ class Person {
       createdAt: DateTime.parse(json['created_at']),
       updatedAt: DateTime.parse(json['updated_at']),
       speechSamples: json['speech_samples'] != null ? List<String>.from(json['speech_samples']) : [],
+      colorIdx: json['color_idx'] ?? json['id'].hashCode % speakerColors.length,
     );
   }
 
@@ -30,6 +63,7 @@ class Person {
       'created_at': createdAt.toIso8601String(),
       'updated_at': updatedAt.toIso8601String(),
       'speech_samples': speechSamples ?? [],
+      'color_idx': colorIdx,
     };
   }
 }
