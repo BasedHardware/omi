c464652e75678637f81b04784d082fac1b71a2cd
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-08-30 14:57:58 -0700

    facts string better used for prompts, differentiating self + generated

--- backend/routers/facts.py
+++ backend/routers/facts.py
@@ -3,6 +3,7 @@ from typing import List
 from fastapi import APIRouter, Depends
 
 import database.facts as facts_db
+from database.auth import get_user_name
 from models.facts import FactDB, Fact
 from utils.other import endpoints as auth
 
@@ -12,6 +13,7 @@ router = APIRouter()
 @router.post('/v1/facts', tags=['facts'], response_model=FactDB)
 def create_fact(fact: Fact, uid: str = Depends(auth.get_current_user_uid)):
     fact_db = FactDB.from_fact(fact, uid, None, None)
+    fact_db.manually_added = True
     facts_db.create_fact(uid, fact_db.dict())
     return fact_db
 
@@ -19,13 +21,15 @@ def create_fact(fact: Fact, uid: str = Depends(auth.get_current_user_uid)):
 @router.get('/v1/facts', tags=['facts'], response_model=List[FactDB])  # filters
 def get_facts(limit: int = 100, offset: int = 0, uid: str = Depends(auth.get_current_user_uid)):
     facts = facts_db.get_facts(uid, limit, offset)
-    filtered = []
-    for fact in facts:
-        if fact['reviewed'] and not fact['user_review']:
-            # skip facts that were reviewed and the user marked as false
-            continue
-        filtered.append(fact)
-    return filtered
+    # user_name = get_user_name(uid, use_default=False)
+    # for fact in facts:
+    #     if fact['manually_added']:
+    #         continue
+    #     if user_name:
+    #         fact['content'] = f'{user_name} {fact["content"]}'
+    #     else:
+    #         fact['content'] = str(fact["content"]).capitalize()
+    return facts
 
 
 @router.delete('/v1/facts/{fact_id}', tags=['facts'])
@@ -42,5 +46,10 @@ def review_fact(fact_id: str, value: bool, uid: str = Depends(auth.get_current_u
 
 @router.patch('/v1/facts/{fact_id}', tags=['facts'])
 def edit_fact(fact_id: str, value: str, uid: str = Depends(auth.get_current_user_uid)):
+    # first_word = value.split(' ')[0]
+    # user_name = get_user_name(uid, use_default=False)
+    # if user_name == first_word:
+    #     value = value[len(first_word):].strip()
+
     facts_db.edit_fact(uid, fact_id, value)
     return {'status': 'ok'}
