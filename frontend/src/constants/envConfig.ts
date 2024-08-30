89612d3a04d8ef20f77730d27e77982d9a85d0ed
Andres Sanabria <sanabria80.andres@gmail.com>
2024-08-30 08:07:03 -0500

    fix: format code, uninstall eslint-react-hooks

--- frontend/src/constants/envConfig.ts
+++ frontend/src/constants/envConfig.ts
@@ -1,5 +1,8 @@
 const envConfig = {
-  API_URL: process.env.NEXT_PUBLIC_NODE_ENV === 'development' ? process.env.API_URL_DEV : process.env.API_URL_PROD,
+  API_URL:
+    process.env.NEXT_PUBLIC_NODE_ENV === 'development'
+      ? process.env.API_URL_DEV
+      : process.env.API_URL_PROD,
   API_URL_DEV: process.env.API_URL_DEV,
   API_URL_PROD: process.env.API_URL_PROD,
   NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV,
@@ -7,7 +10,7 @@ const envConfig = {
   deploymentUrl:
     process.env.NEXT_PUBLIC_NODE_ENV === 'development'
       ? process.env.API_URL_DEV
-      : process.env.API_URL_PROD
+      : process.env.API_URL_PROD,
 };
 
 export default envConfig;
