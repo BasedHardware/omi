d512356c6745df4d0f0d7216f9806ab0cf84bc34
Andres Sanabria <sanabria80.andres@gmail.com>
2024-08-30 21:08:07 -0500

    WIP on main
    fix: clean envConfig file

--- frontend/src/constants/envConfig.ts
+++ frontend/src/constants/envConfig.ts
@@ -1,16 +1,7 @@
 const envConfig = {
-  API_URL:
-    process.env.NEXT_PUBLIC_NODE_ENV === 'development'
-      ? process.env.API_URL_DEV
-      : process.env.API_URL_PROD,
-  API_URL_DEV: process.env.API_URL_DEV,
-  API_URL_PROD: process.env.API_URL_PROD,
+  API_URL: process.env.API_URL,
   NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV,
   IS_DEVELOPMENT: process.env.NEXT_PUBLIC_NODE_ENV === 'development',
-  deploymentUrl:
-    process.env.NEXT_PUBLIC_NODE_ENV === 'development'
-      ? process.env.API_URL_DEV
-      : process.env.API_URL_PROD,
 };
 
 export default envConfig;
