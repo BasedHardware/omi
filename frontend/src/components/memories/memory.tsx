29e44933e30e0b0621fd3380ca7aaa83f84f06af
Andres Sanabria <sanabria80.andres@gmail.com>
2024-08-30 21:14:21 -0500

    fix: change name, remove line-clamp for title memory

--- frontend/src/components/memories/memory.tsx
+++ frontend/src/components/memories/memory.tsx
@@ -13,10 +13,10 @@ interface MemoryProps {
 export default function Memory({ memory, searchParams }: MemoryProps) {
   const currentTab = searchParams.tab ?? 'trs';
   return (
-    <div className="relative mx-3 my-10 max-w-screen-md overflow-hidden rounded-2xl border border-solid border-zinc-800 bg-transparent py-6 text-white shadow-xl shadow-gray-900 backdrop-blur-lg md:mx-auto md:my-28 md:py-12">
+    <div className="relative mx-3 my-10 max-w-screen-md overflow-hidden rounded-2xl border border-solid border-zinc-800 bg-transparent py-6 text-white shadow-md shadow-gray-900 backdrop-blur-lg md:mx-auto md:my-28 md:py-12">
       <div className="relative z-50">
         <div className="px-4 md:px-12">
-          <h2 className="line-clamp-2 text-2xl font-bold md:text-3xl">
+          <h2 className="text-2xl font-bold md:text-3xl">
             {memory.structured.title}
           </h2>
           <p className="my-2 text-sm text-gray-500 md:text-base">
