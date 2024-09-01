223be33c0120b9bce568c07263e81ed4416a0728
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-01 11:48:15 -0500

    feat: format code, disable sticky header for plugins for now

--- frontend/src/components/plugins/indentify-plugin.tsx
+++ frontend/src/components/plugins/indentify-plugin.tsx
@@ -8,17 +8,19 @@ interface IndentifyPluginProps {
 export default async function IndentifyPlugin({ pluginId }: IndentifyPluginProps) {
   const pluginCommunity = await getCommunityPlugin(pluginId);
 
-  if(!pluginCommunity) return null;
+  if (!pluginCommunity) return null;
 
   return (
-    <div className="flex gap-2 items-center sticky top-[4rem] px-4 md:px-12 bg-[#0f0f0f] backdrop-blur-sm bg-opacity-90 z-[50] py-3 shadow-sm border-b border-solid border-zinc-900">
+    <div className="sticky top-[4rem] z-[50] flex items-center gap-2 border-b border-solid border-zinc-900 bg-[#0f0f0f] bg-opacity-90 px-4 py-3 shadow-sm backdrop-blur-sm md:px-12">
       {/* <Image src={`${envConfig.API_URL}${pluginCommunity.image}`} alt={pluginCommunity.name} width={50} height={50}/> */}
-      <div className='w-9 h-9 bg-zinc-700 rounded-full grid place-items-center'>
-        <Puzzle className='text-xs'/>
+      <div className="grid h-9 w-9 place-items-center rounded-full bg-zinc-700">
+        <Puzzle className="text-xs" />
       </div>
       <div>
         <h3 className="text-base font-semibold md:text-base">{pluginCommunity.name}</h3>
-        <p className="text-gray-500 md:-mt-1 md:text-base text-sm line-clamp-1">{pluginCommunity.description}</p>
+        <p className="line-clamp-1 text-sm text-gray-500 md:-mt-1 md:text-base">
+          {pluginCommunity.description}
+        </p>
       </div>
     </div>
   );
