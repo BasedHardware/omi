a7bce087a46953a90e46438da757acc0b3f1ff0e
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-01 10:11:00 -0500

    feat: display text with markdown formatter. first steps to identify plugin based on plugin_id

---
+++ frontend/src/components/plugins/indentify-plugin.tsx
@@ -0,0 +1,15 @@
+import { getCommunityPlugin } from "@/src/actions/plugins/get-community-plugins";
+
+interface IndentifyPluginProps {
+  pluginId: string;
+}
+
+export default async function IndentifyPlugin({ pluginId }: IndentifyPluginProps) {
+  const pluginCommunity = await getCommunityPlugin(pluginId);
+  return (
+    <div className="mt-10">
+      <h3 className="text-xl font-semibold md:text-2xl">{pluginCommunity.name}</h3>
+      <p className="text-white">{pluginCommunity.description}</p>
+    </div>
+  )
+}
