cd744349df1fc407a427c8a849ee9cb250a199d4
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-01 18:05:28 -0500

    Clean env config unused vars

--- frontend/src/constants/envConfig.ts
+++ frontend/src/constants/envConfig.ts
@@ -2,10 +2,6 @@ const envConfig = {
   API_URL: process.env.API_URL,
   NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV,
   IS_DEVELOPMENT: process.env.NEXT_PUBLIC_NODE_ENV === 'development',
-  deploymentUrl:
-    process.env.NEXT_PUBLIC_NODE_ENV === 'development'
-      ? process.env.API_URL_DEV
-      : process.env.API_URL_PROD,
 };
 
 export default envConfig;
