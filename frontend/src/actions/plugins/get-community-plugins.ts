bea5bd36a52e63052b1b4ac3f1659f65b0e5d61e
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-01 10:11:21 -0500

    fix: format code

--- frontend/src/actions/plugins/get-community-plugins.ts
+++ frontend/src/actions/plugins/get-community-plugins.ts
@@ -1,13 +1,16 @@
 'use server';
 
-export default async function getCummunityPlugins(){
-  const response = await fetch('https://raw.githubusercontent.com/BasedHardware/Omi/main/community-plugins.json', {
-    next: { revalidate: 24 * 60 * 60 }
-  });
+export default async function getCummunityPlugins() {
+  const response = await fetch(
+    'https://raw.githubusercontent.com/BasedHardware/Omi/main/community-plugins.json',
+    {
+      next: { revalidate: 24 * 60 * 60 },
+    },
+  );
   return await response.json();
 }
 
-export async function getCommunityPlugin(pluginId: string){
+export async function getCommunityPlugin(pluginId: string) {
   const plugins = await getCummunityPlugins();
   return plugins.find((plugin) => plugin.id === pluginId);
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -15,8 +15,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography')
-  ],
+  plugins: [require('@tailwindcss/typography')],
 };
 export default config;
