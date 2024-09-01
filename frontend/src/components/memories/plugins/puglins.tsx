65a308c718895ff87cfe8c0ad9fa17e7b59e730a
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-01 10:11:00 -0500

    feat: display text with markdown formatter. first steps to identify plugin based on plugin_id

--- frontend/src/components/memories/plugins/puglins.tsx
+++ frontend/src/components/memories/plugins/puglins.tsx
@@ -1,18 +1,26 @@
 import { PluginsResult } from "@/src/types/memory.types";
+import Markdown from 'markdown-to-jsx'
+import { Suspense } from "react";
+import IndentifyPlugin from "../../plugins/indentify-plugin";
 
 interface PuglinsProps{
   puglins: PluginsResult[];
 }
+
 export default function Puglins({ puglins }: PuglinsProps) {
   return (
     <div className="mt-10">
       <h3 className="text-xl font-semibold md:text-2xl">Puglins</h3>
-      {puglins.map((puglin, index) => (
-        <div key={index} className="mt-3">
-          <h4 className="text-lg font-semibold">{puglin.plugin_id}</h4>
-          <p className="mt-2 text-base">{puglin.content}</p>
-        </div>
-      ))}
+      {puglins.map((puglin, index) => {
+        return(
+          <div key={index} className="mt-3">
+            <Suspense>
+              <IndentifyPlugin pluginId={puglin.plugin_id} />
+            </Suspense>
+            <Markdown className="prose prose-ul:list-disc text-white">{puglin.content}</Markdown>
+          </div>
+        )
+      })}
     </div>
   )
 }
