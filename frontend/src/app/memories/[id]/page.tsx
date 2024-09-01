5cc6c90618517f1b0ce293b4e9bd29f2f87c7ecd
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-01 13:04:37 -0500

    feat: sharp for image optimization, comment loading tsx content (test)

--- frontend/src/app/memories/[id]/page.tsx
+++ frontend/src/app/memories/[id]/page.tsx
@@ -1,16 +1,26 @@
 import getSharedMemory from '@/src/actions/memories/get-shared-memory';
 import Memory from '@/src/components/memories/memory';
+import envConfig from '@/src/constants/envConfig';
 import { DEFAULT_TITLE_MEMORY } from '@/src/constants/memory';
 import { ParamsTypes, SearchParamsTypes } from '@/src/types/params.types';
 import { Metadata, ResolvingMetadata } from 'next';
+
+interface MemoryPageProps {
+  params: ParamsTypes;
+  searchParams: SearchParamsTypes;
+}
 
 export async function generateMetadata(
   { params }: { params: ParamsTypes },
   parent: ResolvingMetadata,
 ): Promise<Metadata> {
-  // read route params
+  const memory = (await (
+    await fetch(`${envConfig.API_URL}/v1/memories/${params.id}/shared`, {
+      next: { revalidate: 86400 },
+    })
+  ).json()) as any;
+
   const prevData = (await parent) as Metadata;
-  const memory = await getSharedMemory(params.id);
 
   const title = !memory
     ? 'Memory Not Found'
@@ -33,14 +43,10 @@ export async function generateMetadata(
   };
 }
 
-interface MemoryPageProps {
-  params: ParamsTypes;
-  searchParams: SearchParamsTypes;
-}
-
 export default async function MemoryPage({ params, searchParams }: MemoryPageProps) {
   const memoryId = params.id;
   const memory = await getSharedMemory(memoryId);
   if (!memory) throw new Error();
   return <Memory memory={memory} searchParams={searchParams} />;
 }
+
