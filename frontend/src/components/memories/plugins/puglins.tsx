56394ef0cd189d23296c9235cb0767be0c10679a
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-01 10:47:55 -0500

    feat: community plugins types, improving UI for data

--- frontend/src/components/memories/plugins/puglins.tsx
+++ frontend/src/components/memories/plugins/puglins.tsx
@@ -17,7 +17,7 @@ export default function Puglins({ puglins }: PuglinsProps) {
             <Suspense>
               <IndentifyPlugin pluginId={puglin.plugin_id} />
             </Suspense>
-            <Markdown className="prose text-white prose-ul:list-disc">
+            <Markdown className="prose text-white prose-ul:list-disc prose-p:m-0 prose-p:mt-3 prose-ul:my-0 prose-slate prose-p:text-white prose-li:text-zinc-400 prose-strong:text-white last:prose-p:text-zinc-300 last:prose-p:bg-zinc-900 last:prose-p:p-2 last:prose-p:px-4 last:prose-p:rounded-lg">
               {puglin.content}
             </Markdown>
           </div>
--- frontend/src/components/plugins/indentify-plugin.tsx
+++ frontend/src/components/plugins/indentify-plugin.tsx
@@ -6,10 +6,16 @@ interface IndentifyPluginProps {
 
 export default async function IndentifyPlugin({ pluginId }: IndentifyPluginProps) {
   const pluginCommunity = await getCommunityPlugin(pluginId);
+
+  if(!pluginCommunity) return null;
+
   return (
-    <div className="mt-10">
-      <h3 className="text-xl font-semibold md:text-2xl">{pluginCommunity.name}</h3>
-      <p className="text-white">{pluginCommunity.description}</p>
+    <div className="flex gap-2 items-center">
+      {/* <Image src={`${envConfig.API_URL}${pluginCommunity.image}`} alt={pluginCommunity.name} width={50} height={50}/> */}
+      <div>
+        <h3 className="text-base font-semibold md:text-lg">{pluginCommunity.name}</h3>
+        <p className="text-gray-500 -mt-1 line-clamp-1">{pluginCommunity.description}</p>
+      </div>
     </div>
   );
 }
