a4b79df3a9862bfa937185d3b5e5ca09593766bb
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-01 11:48:15 -0500

    feat: format code, disable sticky header for plugins for now

--- frontend/src/components/memories/plugins/puglins.tsx
+++ frontend/src/components/memories/plugins/puglins.tsx
@@ -10,17 +10,17 @@ interface PuglinsProps {
 
 export default function Puglins({ puglins }: PuglinsProps) {
   return (
-    <div className='h-auto'>
-      <h3 className="text-xl font-semibold px-4 md:px-12 md:text-2xl">Puglins</h3>
-      <div className='flex gap-8 flex-col mt-3'>
+    <div className="h-auto">
+      <h3 className="px-4 text-xl font-semibold md:px-12 md:text-2xl">Puglins</h3>
+      <div className="mt-3 flex flex-col gap-8">
         {puglins.map((puglin, index) => {
           return (
             <div key={index}>
               <Suspense fallback={<IdentifyPluginLoader />}>
                 <IndentifyPlugin pluginId={puglin.plugin_id} />
               </Suspense>
-              <div className='px-4 md:px-12 bg-[#0f0f0f]'>
-                <Markdown className="prose text-white prose-ul:list-disc prose-p:m-0 prose-p:mt-3 prose-ul:my-0 prose-slate prose-p:text-white prose-li:text-zinc-400 prose-strong:text-white last:prose-p:text-zinc-300 last:prose-p:bg-zinc-900 last:prose-p:p-2 last:prose-p:px-4 last:prose-p:rounded-lg last:prose-p:text-sm last:prose-p:mt-8">
+              <div className="bg-[#0f0f0f] px-4 md:px-12">
+                <Markdown className="prose prose-slate text-white prose-p:m-0 prose-p:mt-3 prose-p:text-white last:prose-p:mt-8 last:prose-p:rounded-lg last:prose-p:bg-zinc-900 last:prose-p:p-2 last:prose-p:px-4 last:prose-p:text-sm last:prose-p:text-zinc-300 prose-strong:text-white prose-ul:my-0 prose-ul:list-disc prose-li:text-zinc-400">
                   {puglin.content}
                 </Markdown>
               </div>
--- frontend/src/components/plugins/identify-plugin-loader.tsx
+++ frontend/src/components/plugins/identify-plugin-loader.tsx
@@ -1,15 +1,15 @@
-import { OnePointCircle } from "iconoir-react";
+import { OnePointCircle } from 'iconoir-react';
 
-export default function IdentifyPluginLoader(){
-  return(
-        <div className="flex gap-2 items-center sticky top-[4rem] px-4 md:px-12 bg-[#0f0f0f] backdrop-blur-sm bg-opacity-90 z-[50] py-3 shadow-sm border-b border-solid border-zinc-900">
-      <div className='w-9 h-9 bg-zinc-800 rounded-full grid place-items-center'>
-        <OnePointCircle className='text-xs animate-spin'/>
+export default function IdentifyPluginLoader() {
+  return (
+    <div className="sticky top-[4rem] z-[50] flex items-center gap-2 border-b border-solid border-zinc-900 bg-[#0f0f0f] bg-opacity-90 px-4 py-3 shadow-sm backdrop-blur-sm md:px-12">
+      <div className="grid h-9 w-9 place-items-center rounded-full bg-zinc-800">
+        <OnePointCircle className="animate-spin text-xs" />
       </div>
       <div>
-        <h3 className="text-base font-semibold md:text-base h-[13px] animate-pulse rounded-sm w-[150px] bg-gray-600"></h3>
-        <p className="text-gray-500 md:text-base text-sm h-[10px] mt-2 bg-gray-800 animate-pulse rounded-sm w-[200px] line-clamp-1"></p>
+        <h3 className="h-[13px] w-[150px] animate-pulse rounded-sm bg-gray-600 text-base font-semibold md:text-base"></h3>
+        <p className="mt-2 line-clamp-1 h-[10px] w-[200px] animate-pulse rounded-sm bg-gray-800 text-sm text-gray-500 md:text-base"></p>
       </div>
     </div>
-  )
+  );
 }
