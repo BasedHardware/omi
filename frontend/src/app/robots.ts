a1cf2b3060d5943d5ef04b13598e2b71ad575017
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-02 18:46:46 -0500

    fix: updating url

--- frontend/src/app/robots.ts
+++ frontend/src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: ['/'],
     },
-    sitemap: 'https://omi.me/sitemap.xml',
+    sitemap: 'https://h.omi.me/sitemap.xml',
   };
 }
