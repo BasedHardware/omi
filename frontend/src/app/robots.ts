2553e50cc7d90476fee43301db42f2d981e90e7f
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-02 18:36:25 -0500

    fix: allow indexing robots.txt

--- frontend/src/app/robots.ts
+++ frontend/src/app/robots.ts
@@ -4,7 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      disallow: ['/memories/'],
+      allow: ['/'],
     },
     sitemap: 'https://omi.me/sitemap.xml',
   };
