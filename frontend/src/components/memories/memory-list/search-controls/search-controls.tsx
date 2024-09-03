d2a908b71216e340240fbdbd33ed9310887450e7
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-03 06:55:31 -0500

    format code

--- frontend/src/components/memories/memory-list/search-controls/search-controls.tsx
+++ frontend/src/components/memories/memory-list/search-controls/search-controls.tsx
@@ -1,12 +1,12 @@
-import SearchBar from "./search-bar";
+import SearchBar from './search-bar';
 
 export default function SearchControls() {
-  return(
+  return (
     <div>
-      <h1 className="text-center text-white text-3xl font-bold">Omi Memories</h1>
+      <h1 className="text-center text-3xl font-bold text-white">Omi Memories</h1>
       <div>
         <SearchBar />
       </div>
     </div>
-  )
+  );
 }
