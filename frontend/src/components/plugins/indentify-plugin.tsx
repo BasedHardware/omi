2d97bd8192ec23233f9d1d15ded048ca25fc00f9
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-03 06:55:17 -0500

    feat: search controls components initial

--- frontend/src/components/plugins/indentify-plugin.tsx
+++ frontend/src/components/plugins/indentify-plugin.tsx
@@ -10,7 +10,7 @@ export default async function IndentifyPlugin({ pluginId }: IndentifyPluginProps
   if (!pluginCommunity) throw new Error('Plugin not found');
 
   return (
-    <div className="sticky top-[4rem] z-[50] mb-3 flex items-center gap-2 border-b border-solid border-zinc-900 bg-[#0f0f0f] bg-opacity-90 px-4 py-3 shadow-sm backdrop-blur-sm md:px-12">
+    <div className="sticky top-[4rem] z-[50] mb-3 flex items-center gap-2 border-b border-solid border-zinc-900 bg-bg-color bg-opacity-90 px-4 py-3 shadow-sm backdrop-blur-sm md:px-12">
       <Image
         className="grid h-9 w-9 min-w-[36px] place-items-center rounded-full bg-zinc-700"
         src={`https://raw.githubusercontent.com/BasedHardware/Friend/main/${pluginCommunity.image}`}
