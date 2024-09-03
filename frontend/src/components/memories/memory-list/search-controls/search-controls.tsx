d172ec5f06471169c2dd3211957ae77b000f44b1
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-03 08:07:30 -0500

    feat: dropdown menu for categories

--- frontend/src/components/memories/memory-list/search-controls/search-controls.tsx
+++ frontend/src/components/memories/memory-list/search-controls/search-controls.tsx
@@ -1,11 +1,13 @@
+import CategoriesDropdown from './categories-dropdown';
 import SearchBar from './search-bar';
 
 export default function SearchControls() {
   return (
     <div>
       <h1 className="text-center text-3xl font-bold text-white">Omi Memories</h1>
-      <div>
+      <div className="mb-10 mt-8 flex w-full items-center gap-2">
         <SearchBar />
+        <CategoriesDropdown />
       </div>
     </div>
   );
