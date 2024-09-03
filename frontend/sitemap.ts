b95cb784f5a758cbd5d35b96b0c33cc2940abccd
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-02 19:38:12 -0500

    feat: change sitemap priorities indexing

--- frontend/sitemap.ts
+++ frontend/sitemap.ts
@@ -5,12 +5,12 @@ import getPublicMemoriesPrerender from './src/actions/memories/get-public-memori
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   if (envConfig.IS_DEVELOPMENT) return [];
 
-  const urls = [{ path: 'memories', priority: 0.9 }];
+  const urls = [{ path: 'memories', priority: 0.7 }];
 
   const memories = await getPublicMemoriesPrerender(75000);
   const memoriesUrls = memories.map((memory) => ({
     path: 'memories/' + memory.id,
-    priority: 0.7,
+    priority: 0.9,
   }));
   urls.push(...memoriesUrls);
 
