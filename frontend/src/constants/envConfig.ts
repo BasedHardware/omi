587c95650856e62f11131578fbecce20fee63024
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-03 08:34:49 -0500

    Initial Gleap setup

--- frontend/src/constants/envConfig.ts
+++ frontend/src/constants/envConfig.ts
@@ -3,6 +3,7 @@ const envConfig = {
   NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV,
   IS_DEVELOPMENT: process.env.NEXT_PUBLIC_NODE_ENV === 'development',
   WEB_URL: process.env.WEB_URL ?? 'https://h.omi.me',
+  GLEAP_APP_ID: process.env.GLEAP_APP_ID,
 };
 
 export default envConfig;
