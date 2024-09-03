d7f8e753297c73011df58e0dd4c68e709ca04b92
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-03 06:55:17 -0500

    feat: search controls components initial

---
+++ frontend/src/components/memories/memory-list/search-controls/search-bar.tsx
@@ -0,0 +1,7 @@
+export default function SearchBar() {
+  return(
+    <div className="mt-5 mb-10">
+      <input type="text" placeholder="Search memories" className="bg-transparent rounded-md w-full border border-solid border-zinc-600 py-2 px-3"/>
+    </div>
+  )
+}
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        'bg-color': '#181818'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
