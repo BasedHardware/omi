9718b11845d1e5fc25e93cd5f4ee5f9f53ef5cd2
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-03 14:31:01 -0500

    WIP on main
    fix: Identify plugin client component and log errors

--- frontend/src/components/shared/gleap.tsx
+++ frontend/src/components/shared/gleap.tsx
@@ -3,8 +3,6 @@ import { FC, ReactNode, useEffect } from 'react';
 import Gleap from 'gleap';
 import envConfig from '@/src/constants/envConfig';
 
-
-
 export const GleapInit: FC<{ children?: ReactNode }> = ({ children }) => {
   useEffect(() => {
     Gleap.initialize(envConfig.GLEAP_API_KEY ?? '');
