16985b736df553c7de3eb12c220de7779dc31e36
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-09-03 21:10:55 -0700

    minor gcp deployment fixes

--- backend/modal/main.py
+++ backend/modal/main.py
@@ -12,9 +12,11 @@ app = FastAPI()
 def speaker_identification(
         uid: str, audio_file: UploadFile = File, segments: str = Form(...)
 ) -> List[ResponseItem]:
+    print('speaker_identification')
     return speaker_identification_endpoint(uid, audio_file, segments)
 
 
 @app.post('/v1/vad')
 def vad(audio_file: UploadFile = File):
+    print('vad')
     return vad_endpoint(audio_file)
