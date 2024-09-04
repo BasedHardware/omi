c5f877c7b414ee0af9781a36295dfe662d4ad6bc
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-09-03 20:55:07 -0700

    speech models deployment to GCP

---
+++ backend/modal/main.py
@@ -0,0 +1,20 @@
+from typing import List
+
+from fastapi import FastAPI, UploadFile, File, Form
+
+from speech_profile_modal import ResponseItem, endpoint as speaker_identification_endpoint
+from vad_modal import endpoint as vad_endpoint
+
+app = FastAPI()
+
+
+@app.post('/v1/speaker-identification')
+def speaker_identification(
+        uid: str, audio_file: UploadFile = File, segments: str = Form(...)
+) -> List[ResponseItem]:
+    return speaker_identification_endpoint(uid, audio_file, segments)
+
+
+@app.post('/v1/vad')
+def vad(audio_file: UploadFile = File):
+    return vad_endpoint(audio_file)
