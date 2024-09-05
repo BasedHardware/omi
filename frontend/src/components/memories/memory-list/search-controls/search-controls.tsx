118bc2ad03977e9f2777fc5b9369d647b80e063a
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-05 13:13:53 -0500

    feat: no results boundary and UI improvements

--- frontend/src/components/memories/memory-list/search-controls/search-controls.tsx
+++ frontend/src/components/memories/memory-list/search-controls/search-controls.tsx
@@ -2,25 +2,41 @@
 
 import { liteClient as algoliasearch } from 'algoliasearch/lite';
 import SearchBar from './search-bar';
-import { InstantSearch, Configure} from 'react-instantsearch';
+import { InstantSearch, Configure } from 'react-instantsearch';
 import { Fragment } from 'react';
 import envConfig from '@/src/constants/envConfig';
+import { history } from 'instantsearch.js/es/lib/routers';
 
-const searchClient = algoliasearch(envConfig.ALGOLIA_APP_ID, envConfig.ALGOLIA_SEARCH_API_KEY);
+const searchClient = algoliasearch(
+  envConfig.ALGOLIA_APP_ID,
+  envConfig.ALGOLIA_SEARCH_API_KEY,
+);
 
 interface SearchControlsProps {
   children: React.ReactNode;
 }
 
-export default function SearchControls({ children }: SearchControlsProps) {
+const routing = {
+  router: history({
+    cleanUrlOnDispose: false,
+  }),
+};
 
+export default function SearchControls({ children }: SearchControlsProps) {
   return (
     <Fragment>
-      <h1 className="text-center mt-10 text-3xl font-bold text-white">Memories</h1>
-      <InstantSearch searchClient={searchClient} indexName={envConfig.ALGOLIA_INDEX_NAME} routing={true}>
+      <h1 className="mt-10 text-center text-4xl font-bold text-white">Memories</h1>
+      <InstantSearch
+        future={{
+          preserveSharedStateOnUnmount: true,
+        }}
+        searchClient={searchClient}
+        indexName={envConfig.ALGOLIA_INDEX_NAME}
+        routing={routing}
+      >
         <Configure hitsPerPage={20} />
         <SearchBar />
-        {children}       
+        {children}
       </InstantSearch>
     </Fragment>
   );
