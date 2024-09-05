84abf1a0a590320ceefaab6ddbc6e65c1b3a1c4c
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-04 19:07:41 -0500

    fix: scroll side-drawer

--- frontend/src/components/memories/tendencies/trending-item.tsx
+++ frontend/src/components/memories/tendencies/trending-item.tsx
@@ -4,10 +4,10 @@ interface TrendingItemProps {
 }
 
 export default function TrendingItem({ title, count }: TrendingItemProps) {
-  return(
-    <div className="fle-col flex h-20 w-1/2 flex-col justify-end rounded-lg bg-zinc-800/50 p-3 hover:bg-zinc-800/75 transition-all cursor-pointer">
+  return (
+    <div className="fle-col flex h-20 w-1/2 cursor-pointer flex-col justify-end rounded-lg bg-zinc-800/50 p-3 transition-all hover:bg-zinc-800/75">
       <h3 className="text-sm md:text-base">{title}</h3>
       <p className="text-xs text-neutral-400 md:text-sm">{count} memories</p>
     </div>
-  )
+  );
 }
