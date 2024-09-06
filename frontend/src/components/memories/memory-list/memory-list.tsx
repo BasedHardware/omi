75b6c018a51667ad4c0995fe4060c2f3da29967b
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-06 07:11:53 -0500

    feat: layout improvements, using trending categories in search bar and UI changes

--- frontend/src/components/memories/memory-list/memory-list.tsx
+++ frontend/src/components/memories/memory-list/memory-list.tsx
@@ -3,14 +3,14 @@ import { Hits, Pagination } from 'react-instantsearch';
 import MemoryItem from './memory-item';
 import { NoResultsBoundary } from './no-results-boundary';
 
-export default function MemoryList() {
-  // const { status } = useInstantSearch();
+export default function MemoryList({ searchParams }) {
+  console.log({ searchParams });
   return (
     <div className="mt-12 text-white">
       <div className="flex flex-col gap-10">
         <NoResultsBoundary
           fallback={
-            <div className="px-4 text-center w-full">
+            <div className="w-full px-4 text-center">
               <h3 className="text-lg font-semibold md:text-xl">
                 Sorry, we can't find any matches to your query!
               </h3>
