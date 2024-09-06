803e9b8da7a5da5a47f4014603385f4cbe21153e
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-05 20:42:51 -0500

    feat: search bar UI fixes and highlighted styles

--- frontend/src/components/memories/memory-list/search-controls/search-bar.tsx
+++ frontend/src/components/memories/memory-list/search-controls/search-bar.tsx
@@ -1,13 +1,19 @@
 'use client';
+import { Xmark } from 'iconoir-react';
 import { SearchBox } from 'react-instantsearch';
 
 export default function SearchBar() {
   return (
     <SearchBox
       placeholder="Search memories"
+      className='relative'
       loadingIconComponent={() => <></>}
       submitIconComponent={() => <></>}
-      resetIconComponent={() => <></>}
+      resetIconComponent={() => (
+        <div className='absolute top-[8px] md:top-[15px] right-3 md:right-4 text-white/60 p-1 hover:bg-zinc-800 rounded-full'>
+          <Xmark className='text-sm'/>
+        </div>
+      )}
     />
   );
 }
