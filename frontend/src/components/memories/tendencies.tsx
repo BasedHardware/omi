5d9f349328c4d19c0395a3b77f75baca3c0253ac
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-06 07:11:53 -0500

    feat: layout improvements, using trending categories in search bar and UI changes

--- frontend/src/components/memories/tendencies.tsx
+++ frontend/src/components/memories/tendencies.tsx
@@ -1,16 +1,20 @@
+import { NavArrowRight } from 'iconoir-react';
 import TrendingItem from './tendencies/trending-item';
 
 export default function Tendencies() {
   return (
-    <div className="top-24 mb-5 hidden sticky md:block w-[50rem] max-w-[352px] rounded-md border border-solid border-neutral-800 bg-gradient-to-r from-[#030710] to-[#401c1c8c] text-white shadow-md shadow-[#06060742] transition-colors hover:border-neutral-700">
-      <h2 className="p-3 text-base hover:no-underline md:p-5 md:text-xl">
-        What's trending now!
-      </h2>
-      <div className="p-3 md:p-5">
-        <div className="flex w-full flex-col gap-2">
-          <TrendingItem title="work" count={23} />
-          <TrendingItem title="Business" count={6} />
-          <TrendingItem title="Inspiration" count={6} />
+    <div className="group sticky top-24 mb-5 hidden w-[50rem] max-w-[352px] rounded-md border border-solid border-neutral-900 bg-gradient-to-r from-[#030710] to-[#0000006c] text-white shadow-md shadow-[#06060742] transition-colors hover:border-neutral-800 md:block">
+      <div className="flex items-center justify-between p-3 md:p-5">
+        <h2 className=" text-base hover:underline md:text-xl">What's trending now!</h2>
+        <div className="grid h-8 w-8 place-items-center rounded-full transition-colors group-hover:bg-zinc-900">
+          <NavArrowRight className="text-sm text-neutral-400 group-hover:text-neutral-300" />
+        </div>
+      </div>
+      <div className="py-3 md:py-5">
+        <div className="flex w-full flex-col divide-slate-800">
+          <TrendingItem title="Work" count={23} raking={1} />
+          <TrendingItem title="Business" count={6} raking={2} />
+          <TrendingItem title="Inspiration" count={6} raking={3} />
           <TrendingItem title="Social" count={6} />
         </div>
       </div>
