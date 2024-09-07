d2596e3e14720040659b601d8eef339fb830d262
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-09-07 23:27:26 +0530

    onboarding cleaanup comment

--- app/lib/pages/onboarding/wrapper.dart
+++ app/lib/pages/onboarding/wrapper.dart
@@ -32,6 +32,7 @@ class _OnboardingWrapperState extends State<OnboardingWrapper> with TickerProvid
 
   @override
   void initState() {
+    //TODO: Change from tab controller to default controller and use provider (part of instabug cleanup) @mdmohsin7
     _controller = TabController(length: 7, vsync: this);
     _controller!.addListener(() => setState(() {}));
     WidgetsBinding.instance.addPostFrameCallback((_) async {
