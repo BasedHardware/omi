2575c6a095800a7f505314009e007ff064f03429
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-09-07 17:55:51 -0700

    minor UI cleaning and initial experience improvements

--- app/lib/pages/memories/page.dart
+++ app/lib/pages/memories/page.dart
@@ -44,93 +44,101 @@ class _MemoriesPageState extends State<MemoriesPage> with AutomaticKeepAliveClie
     print('building memories page');
     super.build(context);
     return Consumer<MemoryProvider>(builder: (context, memoryProvider, child) {
+      bool isEmpty = memoryProvider.memories.isEmpty && !memoryProvider.isLoadingMemories;
       return CustomScrollView(
         slivers: [
-          const SliverToBoxAdapter(child: SizedBox(height: 32)),
-          SliverToBoxAdapter(
-            child: Container(
-              width: double.maxFinite,
-              padding: const EdgeInsets.fromLTRB(16, 8, 8, 0),
-              margin: const EdgeInsets.fromLTRB(18, 0, 18, 0),
-              decoration: const BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.all(Radius.circular(16)),
-                border: GradientBoxBorder(
-                  gradient: LinearGradient(colors: [
-                    Color.fromARGB(127, 208, 208, 208),
-                    Color.fromARGB(127, 188, 99, 121),
-                    Color.fromARGB(127, 86, 101, 182),
-                    Color.fromARGB(127, 126, 190, 236)
-                  ]),
-                  width: 1,
-                ),
-                shape: BoxShape.rectangle,
-              ),
-              child: Consumer<HomeProvider>(builder: (context, home, child) {
-                return TextField(
-                  enabled: true,
-                  controller: textController,
-                  onChanged: (s) {
-                    memoryProvider.filterMemories(s);
-                  },
-                  obscureText: false,
-                  autofocus: false,
-                  focusNode: home.memoryFieldFocusNode,
-                  decoration: InputDecoration(
-                    hintText: 'Search for memories...',
-                    hintStyle: const TextStyle(fontSize: 14.0, color: Colors.grey),
-                    enabledBorder: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    suffixIcon: textController.text.isEmpty
-                        ? const SizedBox.shrink()
-                        : IconButton(
-                            icon: const Icon(
-                              Icons.cancel,
-                              color: Color(0xFFF7F4F4),
-                              size: 28.0,
-                            ),
-                            onPressed: () {
-                              textController.clear();
-                              memoryProvider.initFilteredMemories();
-                            },
-                          ),
-                  ),
-                  style: TextStyle(fontSize: 14.0, color: Colors.grey.shade200),
-                );
-              }),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const SizedBox(width: 1),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        memoryProvider.displayDiscardMemories ? 'Hide Discarded' : 'Show Discarded',
-                        style: const TextStyle(color: Colors.white, fontSize: 16),
+          SliverToBoxAdapter(child: SizedBox(height: isEmpty ? 8 : 32)),
+          isEmpty
+              ? const SliverToBoxAdapter(child: SizedBox())
+              : SliverToBoxAdapter(
+                  child: Container(
+                    width: double.maxFinite,
+                    padding: const EdgeInsets.fromLTRB(16, 8, 8, 0),
+                    margin: const EdgeInsets.fromLTRB(18, 0, 18, 0),
+                    decoration: const BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.all(Radius.circular(16)),
+                      border: GradientBoxBorder(
+                        gradient: LinearGradient(colors: [
+                          Color.fromARGB(127, 208, 208, 208),
+                          Color.fromARGB(127, 188, 99, 121),
+                          Color.fromARGB(127, 86, 101, 182),
+                          Color.fromARGB(127, 126, 190, 236)
+                        ]),
+                        width: 1,
                       ),
-                      const SizedBox(width: 8),
-                      IconButton(
-                        onPressed: () {
-                          memoryProvider.toggleDiscardMemories();
+                      shape: BoxShape.rectangle,
+                    ),
+                    child: Consumer<HomeProvider>(builder: (context, home, child) {
+                      return TextField(
+                        enabled: true,
+                        controller: textController,
+                        onChanged: (s) {
+                          memoryProvider.filterMemories(s);
                         },
-                        icon: Icon(
-                          memoryProvider.displayDiscardMemories ? Icons.cancel_outlined : Icons.filter_list,
-                          color: Colors.white,
+                        obscureText: false,
+                        autofocus: false,
+                        focusNode: home.memoryFieldFocusNode,
+                        decoration: InputDecoration(
+                          hintText: 'Search for memories...',
+                          hintStyle: const TextStyle(fontSize: 14.0, color: Colors.grey),
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          suffixIcon: textController.text.isEmpty
+                              ? const SizedBox.shrink()
+                              : IconButton(
+                                  icon: const Icon(
+                                    Icons.cancel,
+                                    color: Color(0xFFF7F4F4),
+                                    size: 28.0,
+                                  ),
+                                  onPressed: () {
+                                    textController.clear();
+                                    memoryProvider.initFilteredMemories();
+                                  },
+                                ),
                         ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ),
+                        style: TextStyle(fontSize: 14.0, color: Colors.grey.shade200),
+                      );
+                    }),
+                  ),
+                ),
+          isEmpty || !memoryProvider.hasNonDiscardedMemories
+              ? const SliverToBoxAdapter(child: SizedBox())
+              : SliverToBoxAdapter(
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 8.0, top: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const SizedBox(width: 1),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: GestureDetector(
+                            onTap: () {
+                              memoryProvider.toggleDiscardMemories();
+                            },
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Text(
+                                  memoryProvider.displayDiscardMemories ? 'Hide Discarded' : 'Show Discarded',
+                                  style: const TextStyle(color: Colors.white, fontSize: 16),
+                                ),
+                                const SizedBox(width: 8),
+                                Icon(
+                                  memoryProvider.displayDiscardMemories ? Icons.cancel_outlined : Icons.filter_list,
+                                  color: Colors.white,
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
           SliverToBoxAdapter(
             child: getMemoryCaptureWidget(),
           ),
