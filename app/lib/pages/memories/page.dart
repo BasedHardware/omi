1378e5336e558ee2640a034e3a77cf80c7266ae3
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-09-12 19:21:37 +0530

    edit segment fix and cleanup

--- app/lib/pages/memories/page.dart
+++ app/lib/pages/memories/page.dart
@@ -43,171 +43,174 @@ class _MemoriesPageState extends State<MemoriesPage> with AutomaticKeepAliveClie
   Widget build(BuildContext context) {
     print('building memories page');
     super.build(context);
-    return Consumer<MemoryProvider>(builder: (context, memoryProvider, child) {
-      bool isEmpty = memoryProvider.memories.isEmpty && !memoryProvider.isLoadingMemories;
-      return CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(child: SizedBox(height: isEmpty ? 8 : 32)),
-          isEmpty
-              ? const SliverToBoxAdapter(child: SizedBox())
-              : SliverToBoxAdapter(
-                  child: Container(
-                    width: double.maxFinite,
-                    padding: const EdgeInsets.fromLTRB(16, 8, 8, 0),
-                    margin: const EdgeInsets.fromLTRB(18, 0, 18, 0),
-                    decoration: const BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.all(Radius.circular(16)),
-                      border: GradientBoxBorder(
-                        gradient: LinearGradient(colors: [
-                          Color.fromARGB(127, 208, 208, 208),
-                          Color.fromARGB(127, 188, 99, 121),
-                          Color.fromARGB(127, 86, 101, 182),
-                          Color.fromARGB(127, 126, 190, 236)
-                        ]),
-                        width: 1,
-                      ),
-                      shape: BoxShape.rectangle,
-                    ),
-                    child: Consumer<HomeProvider>(builder: (context, home, child) {
-                      return TextField(
-                        enabled: true,
-                        controller: textController,
-                        onChanged: (s) {
-                          memoryProvider.filterMemories(s);
-                        },
-                        obscureText: false,
-                        autofocus: false,
-                        focusNode: home.memoryFieldFocusNode,
-                        decoration: InputDecoration(
-                          hintText: 'Search for memories...',
-                          hintStyle: const TextStyle(fontSize: 14.0, color: Colors.grey),
-                          enabledBorder: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                          suffixIcon: textController.text.isEmpty
-                              ? const SizedBox.shrink()
-                              : IconButton(
-                                  icon: const Icon(
-                                    Icons.cancel,
-                                    color: Color(0xFFF7F4F4),
-                                    size: 28.0,
-                                  ),
-                                  onPressed: () {
-                                    textController.clear();
-                                    memoryProvider.initFilteredMemories();
-                                  },
-                                ),
+    return Consumer<MemoryProvider>(
+      builder: (context, memoryProvider, child) {
+        bool isEmpty = memoryProvider.memories.isEmpty && !memoryProvider.isLoadingMemories;
+        return CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(child: SizedBox(height: isEmpty ? 8 : 32)),
+            isEmpty
+                ? const SliverToBoxAdapter(child: SizedBox())
+                : SliverToBoxAdapter(
+                    child: Container(
+                      width: double.maxFinite,
+                      padding: const EdgeInsets.fromLTRB(16, 8, 8, 0),
+                      margin: const EdgeInsets.fromLTRB(18, 0, 18, 0),
+                      decoration: const BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.all(Radius.circular(16)),
+                        border: GradientBoxBorder(
+                          gradient: LinearGradient(colors: [
+                            Color.fromARGB(127, 208, 208, 208),
+                            Color.fromARGB(127, 188, 99, 121),
+                            Color.fromARGB(127, 86, 101, 182),
+                            Color.fromARGB(127, 126, 190, 236)
+                          ]),
+                          width: 1,
                         ),
-                        style: TextStyle(fontSize: 14.0, color: Colors.grey.shade200),
-                      );
-                    }),
+                        shape: BoxShape.rectangle,
+                      ),
+                      child: Consumer<HomeProvider>(builder: (context, home, child) {
+                        return TextField(
+                          enabled: true,
+                          controller: textController,
+                          onChanged: (s) {
+                            memoryProvider.filterMemories(s);
+                          },
+                          obscureText: false,
+                          autofocus: false,
+                          focusNode: home.memoryFieldFocusNode,
+                          decoration: InputDecoration(
+                            hintText: 'Search for memories...',
+                            hintStyle: const TextStyle(fontSize: 14.0, color: Colors.grey),
+                            enabledBorder: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            suffixIcon: textController.text.isEmpty
+                                ? const SizedBox.shrink()
+                                : IconButton(
+                                    icon: const Icon(
+                                      Icons.cancel,
+                                      color: Color(0xFFF7F4F4),
+                                      size: 28.0,
+                                    ),
+                                    onPressed: () {
+                                      textController.clear();
+                                      memoryProvider.initFilteredMemories();
+                                    },
+                                  ),
+                          ),
+                          style: TextStyle(fontSize: 14.0, color: Colors.grey.shade200),
+                        );
+                      }),
+                    ),
+                  ),
+            isEmpty || !memoryProvider.hasNonDiscardedMemories
+                ? const SliverToBoxAdapter(child: SizedBox())
+                : SliverToBoxAdapter(
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 8.0, top: 8),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const SizedBox(width: 1),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: GestureDetector(
+                              onTap: () {
+                                memoryProvider.toggleDiscardMemories();
+                              },
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    memoryProvider.displayDiscardMemories ? 'Hide Discarded' : 'Show Discarded',
+                                    style: const TextStyle(color: Colors.white, fontSize: 16),
+                                  ),
+                                  const SizedBox(width: 8),
+                                  Icon(
+                                    memoryProvider.displayDiscardMemories ? Icons.cancel_outlined : Icons.filter_list,
+                                    color: Colors.white,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+            SliverToBoxAdapter(
+              child: getMemoryCaptureWidget(),
+            ),
+            if (memoryProvider.memoriesWithDates.isEmpty && !memoryProvider.isLoadingMemories)
+              const SliverToBoxAdapter(
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 32.0),
+                    child: EmptyMemoriesWidget(),
                   ),
                 ),
-          isEmpty || !memoryProvider.hasNonDiscardedMemories
-              ? const SliverToBoxAdapter(child: SizedBox())
-              : SliverToBoxAdapter(
+              )
+            else if (memoryProvider.memoriesWithDates.isEmpty && memoryProvider.isLoadingMemories)
+              const SliverToBoxAdapter(
+                child: Center(
                   child: Padding(
-                    padding: const EdgeInsets.only(right: 8.0, top: 8),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const SizedBox(width: 1),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: GestureDetector(
-                            onTap: () {
-                              memoryProvider.toggleDiscardMemories();
-                            },
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Text(
-                                  memoryProvider.displayDiscardMemories ? 'Hide Discarded' : 'Show Discarded',
-                                  style: const TextStyle(color: Colors.white, fontSize: 16),
-                                ),
-                                const SizedBox(width: 8),
-                                Icon(
-                                  memoryProvider.displayDiscardMemories ? Icons.cancel_outlined : Icons.filter_list,
-                                  color: Colors.white,
-                                ),
-                              ],
+                    padding: EdgeInsets.only(top: 32.0),
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                    ),
+                  ),
+                ),
+              )
+            else
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    if (index == memoryProvider.memoriesWithDates.length) {
+                      if (memoryProvider.isLoadingMemories) {
+                        return const Center(
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 32.0),
+                            child: CircularProgressIndicator(
+                              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
                             ),
                           ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-          SliverToBoxAdapter(
-            child: getMemoryCaptureWidget(),
-          ),
-          if (memoryProvider.memoriesWithDates.isEmpty && !memoryProvider.isLoadingMemories)
-            const SliverToBoxAdapter(
-              child: Center(
-                child: Padding(
-                  padding: EdgeInsets.only(top: 32.0),
-                  child: EmptyMemoriesWidget(),
-                ),
-              ),
-            )
-          else if (memoryProvider.memoriesWithDates.isEmpty && memoryProvider.isLoadingMemories)
-            const SliverToBoxAdapter(
-              child: Center(
-                child: Padding(
-                  padding: EdgeInsets.only(top: 32.0),
-                  child: CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                  ),
-                ),
-              ),
-            )
-          else
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) {
-                  if (index == memoryProvider.memoriesWithDates.length) {
-                    if (memoryProvider.isLoadingMemories) {
-                      return const Center(
-                        child: Padding(
-                          padding: EdgeInsets.only(top: 32.0),
-                          child: CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                          ),
-                        ),
+                        );
+                      }
+                      // widget.loadMoreMemories(); // CALL this only when visible
+                      return VisibilityDetector(
+                        key: const Key('memory-loader'),
+                        onVisibilityChanged: (visibilityInfo) {
+                          if (visibilityInfo.visibleFraction > 0 && !memoryProvider.isLoadingMemories) {
+                            memoryProvider.getMoreMemoriesFromServer();
+                          }
+                        },
+                        child: const SizedBox(height: 80, width: double.maxFinite),
                       );
                     }
-                    // widget.loadMoreMemories(); // CALL this only when visible
-                    return VisibilityDetector(
-                      key: const Key('memory-loader'),
-                      onVisibilityChanged: (visibilityInfo) {
-                        if (visibilityInfo.visibleFraction > 0 && !memoryProvider.isLoadingMemories) {
-                          memoryProvider.getMoreMemoriesFromServer();
-                        }
-                      },
-                      child: const SizedBox(height: 80, width: double.maxFinite),
-                    );
-                  }
 
-                  if (memoryProvider.memoriesWithDates[index].runtimeType == DateTime) {
-                    return DateListItem(date: memoryProvider.memoriesWithDates[index] as DateTime, isFirst: index == 0);
-                  }
-                  var memory = memoryProvider.memoriesWithDates[index] as ServerMemory;
-                  return MemoryListItem(
-                    memoryIdx: memoryProvider.memoriesWithDates.indexOf(memory),
-                    memory: memory,
-                    updateMemory: memoryProvider.updateMemory,
-                    deleteMemory: memoryProvider.deleteMemory,
-                  );
-                },
-                childCount: memoryProvider.memoriesWithDates.length + 1,
+                    if (memoryProvider.memoriesWithDates[index].runtimeType == DateTime) {
+                      return DateListItem(
+                          date: memoryProvider.memoriesWithDates[index] as DateTime, isFirst: index == 0);
+                    }
+                    var memory = memoryProvider.memoriesWithDates[index] as ServerMemory;
+                    return MemoryListItem(
+                      memoryIdx: memoryProvider.memoriesWithDates.indexOf(memory),
+                      memory: memory,
+                      updateMemory: memoryProvider.updateMemory,
+                      deleteMemory: memoryProvider.deleteMemory,
+                    );
+                  },
+                  childCount: memoryProvider.memoriesWithDates.length + 1,
+                ),
               ),
+            const SliverToBoxAdapter(
+              child: SizedBox(height: 80),
             ),
-          const SliverToBoxAdapter(
-            child: SizedBox(height: 80),
-          ),
-        ],
-      );
-    });
+          ],
+        );
+      },
+    );
   }
 }
