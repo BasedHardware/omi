c09f1e845ca60b4f7f27d1e0ba375b63631f6001
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-09-13 12:59:08 +0530

    uncomment (will cleanup later)

--- app/lib/pages/capture/location_service.dart
+++ app/lib/pages/capture/location_service.dart
@@ -5,67 +5,67 @@ import 'package:friend_private/backend/schema/geolocation.dart';
 import 'package:friend_private/backend/preferences.dart';
 import 'package:location/location.dart';
 
-// class LocationService {
-//   Location location = Location();
-//   LocationData? locationData;
-//   Future<bool> enableService() async {
-//     bool serviceEnabled = await location.serviceEnabled();
-//     if (serviceEnabled) {
-//       return true;
-//     } else {
-//       return await location.requestService();
-//     }
-//   }
+class LocationService {
+  Location location = Location();
+  LocationData? locationData;
+  Future<bool> enableService() async {
+    bool serviceEnabled = await location.serviceEnabled();
+    if (serviceEnabled) {
+      return true;
+    } else {
+      return await location.requestService();
+    }
+  }
 
-//   Future<bool> displayPermissionsDialog() async {
-//     if (SharedPreferencesUtil().locationPermissionRequested) return false;
-//     SharedPreferencesUtil().locationPermissionRequested = true;
-//     var status = await permissionStatus();
-//     return await isServiceEnabled() == false ||
-//         (status != PermissionStatus.granted && status != PermissionStatus.deniedForever);
-//   }
+  Future<bool> displayPermissionsDialog() async {
+    if (SharedPreferencesUtil().locationPermissionRequested) return false;
+    SharedPreferencesUtil().locationPermissionRequested = true;
+    var status = await permissionStatus();
+    return await isServiceEnabled() == false ||
+        (status != PermissionStatus.granted && status != PermissionStatus.deniedForever);
+  }
 
-//   Future<bool> isServiceEnabled() => location.serviceEnabled();
+  Future<bool> isServiceEnabled() => location.serviceEnabled();
 
-//   Future<PermissionStatus> permissionStatus() => location.hasPermission();
+  Future<PermissionStatus> permissionStatus() => location.hasPermission();
 
-//   Future hasPermission() async => (await location.hasPermission()) == PermissionStatus.granted;
+  Future hasPermission() async => (await location.hasPermission()) == PermissionStatus.granted;
 
-//   Future<void> getDeviceLocation() async {
-//     locationData = await location.getLocation();
-//   }
+  Future<void> getDeviceLocation() async {
+    locationData = await location.getLocation();
+  }
 
-//   Future<Geolocation?> getGeolocationDetails() async {
-//     try {
-//       if (await hasPermission()) {
-//         if (await location.isBackgroundModeEnabled()) {
-//           if (await location.serviceEnabled()) {
-//             await location.requestService();
-//           }
-//           Future<void> timeout = Future.delayed(const Duration(seconds: 1));
-//           await Future.any([getDeviceLocation(), timeout]);
-//           if (locationData == null) {
-//             return null;
-//           } else {
-//             return Geolocation(latitude: locationData!.latitude, longitude: locationData!.longitude);
-//           }
-//         } else {
-//           try {
-//             await getDeviceLocation();
-//             if (locationData != null) {
-//               return Geolocation(latitude: locationData!.latitude, longitude: locationData!.longitude);
-//             }
-//           } catch (e) {
-//             debugPrint("Error getting location data $e");
-//           }
-//           return null;
-//         }
-//       } else {
-//         return null;
-//       }
-//     } catch (e) {
-//       debugPrint("Error getting geolocation details $e");
-//       return null;
-//     }
-//   }
-// }
+  Future<Geolocation?> getGeolocationDetails() async {
+    try {
+      if (await hasPermission()) {
+        if (await location.isBackgroundModeEnabled()) {
+          if (await location.serviceEnabled()) {
+            await location.requestService();
+          }
+          Future<void> timeout = Future.delayed(const Duration(seconds: 1));
+          await Future.any([getDeviceLocation(), timeout]);
+          if (locationData == null) {
+            return null;
+          } else {
+            return Geolocation(latitude: locationData!.latitude, longitude: locationData!.longitude);
+          }
+        } else {
+          try {
+            await getDeviceLocation();
+            if (locationData != null) {
+              return Geolocation(latitude: locationData!.latitude, longitude: locationData!.longitude);
+            }
+          } catch (e) {
+            debugPrint("Error getting location data $e");
+          }
+          return null;
+        }
+      } else {
+        return null;
+      }
+    } catch (e) {
+      debugPrint("Error getting geolocation details $e");
+      return null;
+    }
+  }
+}
