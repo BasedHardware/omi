6c5facc0757c1338771b982dbdbd6d78c31c1abd
Neo <54811660+Neotastisch@users.noreply.github.com>
2024-09-14 17:34:00 +1000

    edited the prompt of the feedback to be less generic

--- plugins/example/basic/memory_created.py
+++ plugins/example/basic/memory_created.py
@@ -14,13 +14,16 @@ def conversation_feedback(memory: Memory):
       The following is the structuring from a transcript of a conversation that just finished.
       First determine if there's crucial feedback to notify a busy entrepreneur about it.
       If not, simply output an empty string, but if it is important, output 20 words (at most) with the most important feedback for the conversation.
-      Be short, concise, and helpful, and specially strict on determining if it's worth notifying or not.
+      Be short, concise, and helpful, and specially strict on determining if it's worth notifying or not. 
+      Also act human-like, friendly and adress the user directly (that includes giving opinion, not writing perfectly, asking questions, cracking a joke, sounding exited, not act generic, etc.)
        
       Transcript:
       ${memory.get_transcript()}
       
       Structured version:
       ${memory.structured.dict()}
+
+      Answer:
     '''
     if num_tokens_from_string(prompt) > 10000:  # unless you are transcribing a podcast, this should never happen
         return {}
