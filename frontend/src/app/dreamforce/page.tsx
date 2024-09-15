78d5f65f6697c94740aff5aceaa05b64c9bc6cb8
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-15 11:51:33 -0500

    trends UI mock

--- frontend/src/app/dreamforce/page.tsx
+++ frontend/src/app/dreamforce/page.tsx
@@ -1,3 +1,47 @@
+import { Trend } from "@/src/types/trends/trends.types";
+
+const trendMock: Trend[] = [
+  {
+    create_at: new Date(),
+    category: "Dreamforce",
+    id: "1",
+    topics: [
+      {
+        id: "1",
+        topic: "Salesforce",
+        memories_count: 100
+      },
+      {
+        id: "2",
+        topic: "Trailhead",
+        memories_count: 50
+      },
+      {
+        id: "3",
+        topic: "Dreamforce",
+        memories_count: 10
+      },
+    ]
+  },
+    {
+    create_at: new Date(),
+    category: "Investment",
+    id: "2",
+    topics: [
+      {
+        id: "1",
+        topic: "Investors",
+        memories_count: 4
+      },
+      {
+        id: "2",
+        topic: "Trailhead",
+        memories_count: 32
+      }
+    ]
+  }
+]
+
 export default function DreamforcePage(){
   return (
     <div className="flex w-full px-4 bg-[url(/noise-texture.svg)] min-h-screen bg-[#09090b]">
@@ -9,6 +53,26 @@ export default function DreamforcePage(){
           </p>
           <div className="absolute top-3 aspect-video h-32 w-full bg-[#dbaafe38] blur-[120px]"></div>
         </div>
+      {/* <Suspense fallback={null}>
+        <GetTrendsMainPage />
+      </Suspense> */}
+      <div className="mt-32 max-w-screen-sm mx-auto flex flex-col gap-10">
+        {trendMock.map((trend, index) => (
+          <div key={index} className="mt-10">
+            <h2 className="text-white text-2xl md:text-4xl text-center font-light">{trend.category}</h2>
+            <div className={`grid grid-cols-2 md:grid-cols-${Math.min(trend.topics.length, 3)} gap-3 md:gap-5 mt-5`}>
+              {trend.topics.map((topic, index) => (
+                <div key={index} className="items-center text-white border border-solid border-zinc-600 p-3 md:p-4 rounded-md bg-white/5 backdrop-blur-[4px]">
+                  <p className="bg-gradient-to-b from-[#6d49a6] to-[#ffffff] bg-clip-text text-base md:text-lg text-transparent">{topic.topic}</p>
+                  <div>
+                    <p className="text-zinc-400 text-sm md:text-base">{topic.memories_count} memories</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
       </div>
     </div>
   )
