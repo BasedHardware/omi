60af88fb12feef7ef9cbc715d27005c4dada7258
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-15 09:41:18 -0500

    feat intial get trends actions, UI page dreamforce trends

---
+++ frontend/src/actions/trends/getTrends.ts
@@ -0,0 +1,10 @@
+'use server';
+
+import envConfig from "@/src/constants/envConfig";
+import { ResponseTrends } from "@/src/types/trends/trends.types";
+
+export default async function getTrends(): Promise<ResponseTrends> {
+  const response = await fetch(`${envConfig.API_URL}/v1/trends`);
+  const data = await response.json();
+  return data;
+}
---
+++ frontend/src/app/dreamforce/page.tsx
@@ -0,0 +1,15 @@
+export default function DreamforcePage(){
+  return (
+    <div className="flex w-full px-4 bg-[url(/noise-texture.svg)] min-h-screen bg-[#09090b]">
+      <div className="mx-auto w-full max-w-screen-xl my-44">
+        <h1 className="text-white text-center font-semibold text-4xl md:text-7xl">What's trending</h1>
+        <div className="text-center text-white mt-10 md:mt-16 max-w-screen-lg mx-auto md:text-base text-sm relative">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur aliquid ullam, illum reprehenderit adipisci quae molestiae ea a aspernatur modi suscipit cupiditate ratione excepturi? Harum dolores voluptatem deserunt sequi veritatis.
+          </p>
+          <div className="absolute top-3 aspect-video h-32 w-full bg-[#dbaafe38] blur-[120px]"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
