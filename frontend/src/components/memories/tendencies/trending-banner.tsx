d48e5901a1be37935c879bd3283e22c15bc3322f
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-15 19:58:48 -0500

    feat: revalidating tendencies, adding in memories page

--- frontend/src/components/memories/tendencies/trending-banner.tsx
+++ frontend/src/components/memories/tendencies/trending-banner.tsx
@@ -1,41 +1,28 @@
 'use client';
-
+import getTrends from '@/src/actions/trends/get-trends';
+import { Trend } from '@/src/types/trends/trends.types';
+import capitalizeFirstLetter from '@/src/utils/capitalize-first-letter';
 import { ArrowRight } from 'iconoir-react';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
-const topTrending = [
-  {
-    title: 'Work',
-    count: 23,
-    ranking: 1,
-  },
-  {
-    title: 'Business',
-    count: 6,
-    ranking: 2,
-  },
-  {
-    title: 'Inspiration',
-    count: 6,
-    ranking: 3,
-  },
-  {
-    title: 'Social',
-    count: 6,
-  },
-];
-
 export default function TrendingBanner() {
-  const [currentTrend, setCurrentTrend] = useState(topTrending[0]);
+  const [trends, setTrends] = useState<Trend[]>([]);
+  const [currentTrend, setCurrentTrend] = useState('');
+
+  useEffect(() => {
+    getTrends().then((res) => {
+      setTrends(res);
+    });
+  }, []);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      const randomIndex = Math.floor(Math.random() * topTrending.length);
-      setCurrentTrend(topTrending[randomIndex]);
-    }, 5000);
+      const randomIndex = Math.floor(Math.random() * trends.length);
+      setCurrentTrend(trends[randomIndex]?.category ?? '');
+    }, 3000);
     return () => clearInterval(interval);
-  }, []);
+  }, [trends]);
 
   return (
     <header className="sticky top-16 z-[40] w-full text-white md:hidden">
@@ -43,12 +30,14 @@ export default function TrendingBanner() {
         <div className="flex items-center justify-between bg-gradient-to-r from-[#030710] to-[#050c1b] px-4 py-3 shadow-md shadow-black/20 md:px-12">
           <h1>What's Trending</h1>
           <div className="flex items-center gap-2">
-            <span
-              key={currentTrend.title}
-              className="animate-slideRightAndFade rounded-sm bg-[#5d5e77cc] px-1.5 py-0.5 text-xs text-white transition-all"
-            >
-              {currentTrend.title}
-            </span>
+            {currentTrend && (
+              <span
+                key={currentTrend}
+                className="animate-slideRightAndFade rounded-sm bg-[#5d5e77cc] px-1.5 py-0.5 text-xs text-white transition-all"
+              >
+                {capitalizeFirstLetter(currentTrend)}
+              </span>
+            )}
             <ArrowRight className="text-[10px]" />
           </div>
         </div>
