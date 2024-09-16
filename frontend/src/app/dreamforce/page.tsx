a35a71c838a4f2c287bd0c43ea0ba4b6ee662bb9
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-16 14:21:37 -0500

    WIP on features/share-memories-public-optionality
    Fix: implement error boundary, UI fix

--- frontend/src/app/dreamforce/page.tsx
+++ frontend/src/app/dreamforce/page.tsx
@@ -1,50 +1,9 @@
 import DreamforceHeader from '@/src/components/dreamforce/dreamforce-header';
+import TrendsError from '@/src/components/dreamforce/trends-error';
 import GetTrendsMainPage from '@/src/components/trends/get-trends-main-page';
 import TrendsTitle from '@/src/components/trends/trends-title';
-import { Trend } from '@/src/types/trends/trends.types';
+import { ErrorBoundary } from 'next/dist/client/components/error-boundary';
 import { Fragment, Suspense } from 'react';
-
-const trendMock: Trend[] = [
-  {
-    create_at: new Date(),
-    category: 'Dreamforce',
-    id: '1',
-    topics: [
-      {
-        id: '1',
-        topic: 'Salesforce',
-        memories_count: 100,
-      },
-      {
-        id: '2',
-        topic: 'Trailhead',
-        memories_count: 50,
-      },
-      {
-        id: '3',
-        topic: 'Dreamforce',
-        memories_count: 10,
-      },
-    ],
-  },
-  {
-    create_at: new Date(),
-    category: 'Investment',
-    id: '2',
-    topics: [
-      {
-        id: '1',
-        topic: 'Investors',
-        memories_count: 4,
-      },
-      {
-        id: '2',
-        topic: 'Trailhead',
-        memories_count: 32,
-      },
-    ],
-  },
-];
 
 export default function DreamforcePage() {
   return (
@@ -53,9 +12,11 @@ export default function DreamforcePage() {
       <div className="flex min-h-screen w-full bg-gradient-to-t from-[#d2e3ff] via-white via-55% to-white px-4">
         <div className="mx-auto my-44 w-full max-w-screen-xl">
           <TrendsTitle>
-            <Suspense fallback={null}>
-              <GetTrendsMainPage />
-            </Suspense>
+            <ErrorBoundary errorComponent={TrendsError}>
+              <Suspense fallback={null}>
+                <GetTrendsMainPage />
+              </Suspense>
+            </ErrorBoundary>
           </TrendsTitle>
         </div>
       </div>
