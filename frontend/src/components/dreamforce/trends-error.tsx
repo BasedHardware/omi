29cd8899930b4f3fca4aace501ce0cd6b3b1dd2b
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-16 14:21:37 -0500

    WIP on features/share-memories-public-optionality
    Fix: implement error boundary, UI fix

---
+++ frontend/src/components/dreamforce/trends-error.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function TrendsError() {
+   return (
+    <div>
+      <p className="text-center text-black">
+        There was an error loading the trends. Please try again later.
+      </p>
+    </div>
+   )
+}
--- frontend/src/components/trends/get-trends-main-page.tsx
+++ frontend/src/components/trends/get-trends-main-page.tsx
@@ -2,48 +2,6 @@ import TrendItem from './trend-item';
 import Animation from './animation';
 import getTrends from '@/src/actions/trends/get-trends';
 
-const trendMock = [
-  {
-    create_at: new Date(),
-    category: 'Dreamforce',
-    id: '1',
-    topics: [
-      {
-        id: '1',
-        topic: 'Salesforce',
-        memories_count: 100,
-      },
-      {
-        id: '2',
-        topic: 'Trailhead',
-        memories_count: 50,
-      },
-      {
-        id: '3',
-        topic: 'Dreamforce',
-        memories_count: 10,
-      },
-    ],
-  },
-  {
-    create_at: new Date(),
-    category: 'Investment',
-    id: '2',
-    topics: [
-      {
-        id: '1',
-        topic: 'Investors',
-        memories_count: 4,
-      },
-      {
-        id: '2',
-        topic: 'Trailhead',
-        memories_count: 32,
-      },
-    ],
-  },
-];
-
 export default async function GetTrendsMainPage() {
   const trends = await getTrends();
   return (
