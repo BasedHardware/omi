40e04901aee266a9e97d4b797f4ee1a286c1f5dd
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-16 09:04:05 -0500

    feat: dreamforce header

--- frontend/src/app/dreamforce/page.tsx
+++ frontend/src/app/dreamforce/page.tsx
@@ -1,7 +1,8 @@
+import DreamforceHeader from '@/src/components/dreamforce/dreamforce-header';
 import GetTrendsMainPage from '@/src/components/trends/get-trends-main-page';
 import TrendsTitle from '@/src/components/trends/trends-title';
 import { Trend } from '@/src/types/trends/trends.types';
-import { Suspense } from 'react';
+import { Fragment, Suspense } from 'react';
 
 const trendMock: Trend[] = [
   {
@@ -47,14 +48,17 @@ const trendMock: Trend[] = [
 
 export default function DreamforcePage() {
   return (
-    <div className="flex min-h-screen w-full bg-gradient-to-t from-[#d2e3ff] via-white via-55% to-white px-4">
-      <div className="mx-auto my-44 w-full max-w-screen-xl">
-        <TrendsTitle>
-          <Suspense fallback={null}>
-            <GetTrendsMainPage />
-          </Suspense>
-        </TrendsTitle>
+    <Fragment>
+        <DreamforceHeader />
+      <div className="flex min-h-screen w-full bg-gradient-to-t from-[#d2e3ff] via-white via-55% to-white px-4">
+        <div className="mx-auto my-44 w-full max-w-screen-xl">
+          <TrendsTitle>
+            <Suspense fallback={null}>
+              <GetTrendsMainPage />
+            </Suspense>
+          </TrendsTitle>
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 }
