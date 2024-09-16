0466cc01068f1f3ddec33f481e32d8d454933b43
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-15 20:27:25 -0500

    showing top 3 badge

--- frontend/src/components/trends/trend-topic-item.tsx
+++ frontend/src/components/trends/trend-topic-item.tsx
@@ -6,22 +6,36 @@ interface TrendTopicItemProps {
   topic: Topic;
   trend: Trend;
   variants: any;
+  index: number;
 }
 
-export default function TrendTopicItem({ topic, trend, variants }: TrendTopicItemProps) {
+export default function TrendTopicItem({ topic, trend, variants, index }: TrendTopicItemProps) {
+  const numberOfTopics = trend.topics.length;
   return (
     <motion.div
       variants={variants}
-      className={`items-center rounded-md border border-solid border-zinc-600 bg-white/5 p-3 text-white backdrop-blur-[4px] md:p-4 ${
-        trend.topics.length > 1 ? '' : 'col-span-3'
+      className={`items-center relative rounded-md border border-solid border-zinc-600 bg-white/5 p-3 text-white backdrop-blur-[4px] md:p-4 ${
+        numberOfTopics === 1  ? 'col-span-3' : ''
       }`}
     >
-      <p className="bg-gradient-to-b from-[#6d49a6] to-[#ffffff] bg-clip-text text-base text-transparent md:text-lg">
+        {index <= 2 && (
+          <span className={`text-black text-xs shadow-md rounded-md px-1.5 pt-0.5 absolute -top-1.5 -right-1.5 font-bold ${
+            index === 0
+              ? 'bg-yellow-400 text-black/60 shadow-yellow-600'
+              : index === 1
+              ? 'bg-gray-400 text-black/60'
+              : 'bg-gray-500 text-black'
+          }`}>
+            {index + 1}
+            <sup className="text-[10px]">st</sup>
+          </span>
+        )}
+      <p className="bg-gradient-to-b from-[#6d49a6] line-clamp-1 to-[#ffffff] bg-clip-text text-base text-transparent md:text-lg">
         {capitalizeFirstLetter(topic.topic)}
       </p>
       <div>
         <p className="text-sm text-zinc-400 md:text-base">
-          {topic.memories_count} memories
+          {topic.memories_count} {topic.memories_count > 1 ? 'memories' : 'memory'}
         </p>
       </div>
     </motion.div>
