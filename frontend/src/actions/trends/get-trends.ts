ed9b798c346f1420b43544eaa9a42e0d6d535b78
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-16 14:30:45 -0500

    feat: put logs, use error boundary

--- frontend/src/actions/trends/get-trends.ts
+++ frontend/src/actions/trends/get-trends.ts
@@ -4,9 +4,19 @@ import envConfig from '@/src/constants/envConfig';
 import { ResponseTrends } from '@/src/types/trends/trends.types';
 
 export default async function getTrends(): Promise<ResponseTrends> {
-  const response = await fetch(`${envConfig.API_URL}/v1/trends`, {
-    next: { revalidate: 60 },
-  });
-  const data = await response.json();
-  return data;
+  console.log('URL:', `${envConfig.API_URL}/v1/trends`);
+  try {
+      const response = await fetch(`${envConfig.API_URL}/v1/trends`, {
+        next: { revalidate: 60 },
+      });
+
+      if(!response.ok){
+        throw new Error('Failed to fetch trends');
+      }
+
+      const data = await response.json();
+      return data;
+  } catch (error) {
+    throw new Error(error);
+  }
 }
--- frontend/src/components/dreamforce/trends-error.tsx
+++ frontend/src/components/dreamforce/trends-error.tsx
@@ -3,7 +3,7 @@
 export default function TrendsError() {
    return (
     <div>
-      <p className="text-center text-black">
+      <p className="text-center mt-10 text-red-500">
         There was an error loading the trends. Please try again later.
       </p>
     </div>
