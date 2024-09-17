53d06bcfd7bab8537a7475e4d8346ff975f50a56
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-16 19:50:41 -0500

    remove logs

--- frontend/src/actions/trends/get-trends.ts
+++ frontend/src/actions/trends/get-trends.ts
@@ -2,9 +2,8 @@
 import envConfig from '@/src/constants/envConfig';
 
 export default async function getTrends() {
-  console.log('URL:', `${envConfig.API_URL}/v1/trends`);
   try {
-      const response = await fetch(`${envConfig.API_URL}/v1/trends`, {
+      const response = await fetch(`${JSON.stringify(envConfig.API_URL)}/v1/trends`, {
         next: { revalidate: 60 },
       });
 
