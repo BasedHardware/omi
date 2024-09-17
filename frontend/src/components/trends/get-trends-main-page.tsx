cf629af6e3d5090687042f7dda7baa460f8006b5
Andres Sanabria <sanabria80.andres@gmail.com>
2024-09-16 20:00:23 -0500

    add logs

--- frontend/src/components/trends/get-trends-main-page.tsx
+++ frontend/src/components/trends/get-trends-main-page.tsx
@@ -1,8 +1,10 @@
 import TrendItem from './trend-item';
 import Animation from './animation';
 import getTrends from '@/src/actions/trends/get-trends';
+import envConfig from '@/src/constants/envConfig';
 
 export default async function GetTrendsMainPage() {
+  console.log('URL FROM PAGE:', `${envConfig.API_URL}/v1/trends`);
   const trends = await getTrends();
   if(!trends) return null;
 
