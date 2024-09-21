362435d3646417f862a0d2a289f9fd87ab265b72
thịnh <ngocthinhdp@gmail.com>
2024-09-21 11:21:52 +0700

    Optmize process updates patch by basic fields using only

--- backend/utils/processing_memories.py
+++ backend/utils/processing_memories.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime, timezone
 
-from models.processing_memory import ProcessingMemory, UpdateProcessingMemory
+from models.processing_memory import ProcessingMemory, UpdateProcessingMemory, BasicProcessingMemory
 from models.memory import CreateMemory, PostProcessingModel, PostProcessingStatus, MemoryPostProcessing, TranscriptSegment
 from utils.memories.process_memory import process_memory
 from utils.memories.location import get_google_maps_location
@@ -55,13 +55,13 @@ async def create_memory_by_processing_memory(uid: str, processing_memory_id: str
 
     return (memory, messages, processing_memory)
 
-def update_basic_processing_memory(uid: str, update_processing_memory: UpdateProcessingMemory,) -> ProcessingMemory:
+def update_basic_processing_memory(uid: str, update_processing_memory: UpdateProcessingMemory,) -> BasicProcessingMemory:
     # Fetch new
-    processing_memories = processing_memories_db.get_processing_memories_by_id(uid, [update_processing_memory.id])
+    processing_memories = processing_memories_db.get_basic_processing_memories_by_id(uid, [update_processing_memory.id])
     if len(processing_memories) == 0:
         print("processing memory is not found")
         return
-    processing_memory = ProcessingMemory(**processing_memories[0])
+    processing_memory = BasicProcessingMemory(**processing_memories[0])
 
     # geolocation
     if update_processing_memory.geolocation:
