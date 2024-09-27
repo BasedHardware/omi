4ee1c6bf8aa1ac85728f8bd01361b7edf86d82a6
thịnh <ngocthinhdp@gmail.com>
2024-09-27 15:27:59 +0700

    Keep capturing memory when disconnects happened

--- app/lib/backend/http/api/processing_memories.dart
+++ app/lib/backend/http/api/processing_memories.dart
@@ -46,3 +46,30 @@ Future<UpdateProcessingMemoryResponse?> updateProcessingMemoryServer({
   }
   return null;
 }
+
+Future<ProcessingMemoryResponse?> fetchProcessingMemoryServer({
+  required String id,
+}) async {
+  var response = await makeApiCall(
+    url: '${Env.apiBaseUrl}v1/processing-memories/$id',
+    headers: {},
+    method: 'GET',
+    body: "",
+  );
+  if (response == null) return null;
+  debugPrint('updateProcessingMemoryServer: ${response.body}');
+  if (response.statusCode == 200) {
+    return ProcessingMemoryResponse.fromJson(jsonDecode(response.body));
+  } else {
+    // TODO: Server returns 304 doesn't recover
+    CrashReporting.reportHandledCrash(
+      Exception('Failed to create memory'),
+      StackTrace.current,
+      level: NonFatalExceptionLevel.info,
+      userAttributes: {
+        'response': response.body,
+      },
+    );
+  }
+  return null;
+}
