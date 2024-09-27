ae24b7e308de64751d08d0c1569739bd14ddb9cf
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-09-27 22:23:11 +0530

    intercom during onboarding and misc

--- app/lib/pages/onboarding/welcome/page.dart
+++ app/lib/pages/onboarding/welcome/page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:friend_private/providers/onboarding_provider.dart';
+import 'package:friend_private/utils/analytics/intercom.dart';
 import 'package:friend_private/widgets/dialog.dart';
 import 'package:gradient_borders/box_borders/gradient_box_border.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -37,71 +38,88 @@ class _WelcomePageState extends State<WelcomePage> with SingleTickerProviderStat
     return Column(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
-        Consumer<OnboardingProvider>(builder: (context, provider, child) {
-          return Padding(
-            padding: EdgeInsets.only(left: screenSize.width * 0.1, right: screenSize.width * 0.1),
-            child: Container(
-              decoration: BoxDecoration(
-                border: const GradientBoxBorder(
-                  gradient: LinearGradient(colors: [
-                    Color.fromARGB(127, 208, 208, 208),
-                    Color.fromARGB(127, 188, 99, 121),
-                    Color.fromARGB(127, 86, 101, 182),
-                    Color.fromARGB(127, 126, 190, 236)
-                  ]),
-                  width: 2,
-                ),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: ElevatedButton(
-                onPressed: () async {
-                  await provider.askForBluetoothPermissions();
-                  if (provider.hasBluetoothPermission) {
-                    widget.goNext();
-                  } else {
-                    showDialog(
-                      context: context,
-                      builder: (c) => getDialog(
-                        context,
-                        () {
-                          Navigator.of(context).pop();
-                          openAppSettings();
-                        },
-                        () {},
-                        'Permissions Required',
-                        'This app needs Bluetooth and Location permissions to function properly. Please enable them in the settings.',
-                        okButtonText: 'Open Settings',
-                        singleButton: true,
-                      ),
-                      barrierDismissible: false,
-                    );
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.transparent,
-                  shadowColor: const Color.fromARGB(255, 17, 17, 17),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
+        Consumer<OnboardingProvider>(
+          builder: (context, provider, child) {
+            return Padding(
+              padding: EdgeInsets.only(left: screenSize.width * 0.1, right: screenSize.width * 0.1),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: const GradientBoxBorder(
+                    gradient: LinearGradient(colors: [
+                      Color.fromARGB(127, 208, 208, 208),
+                      Color.fromARGB(127, 188, 99, 121),
+                      Color.fromARGB(127, 86, 101, 182),
+                      Color.fromARGB(127, 126, 190, 236)
+                    ]),
+                    width: 2,
                   ),
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                child: Container(
-                  width: double.infinity, // Button takes full width of the padding
-                  height: 45, // Fixed height for the button
-                  alignment: Alignment.center,
-                  child: const Text(
-                    'Connect My Friend',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 18,
-                      color: Color.fromARGB(255, 255, 255, 255),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    await provider.askForBluetoothPermissions();
+                    if (provider.hasBluetoothPermission) {
+                      widget.goNext();
+                    } else {
+                      showDialog(
+                        context: context,
+                        builder: (c) => getDialog(
+                          context,
+                          () {
+                            Navigator.of(context).pop();
+                            openAppSettings();
+                          },
+                          () {},
+                          'Permissions Required',
+                          'This app needs Bluetooth and Location permissions to function properly. Please enable them in the settings.',
+                          okButtonText: 'Open Settings',
+                          singleButton: true,
+                        ),
+                        barrierDismissible: false,
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    shadowColor: const Color.fromARGB(255, 17, 17, 17),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: Container(
+                    width: double.infinity, // Button takes full width of the padding
+                    height: 45, // Fixed height for the button
+                    alignment: Alignment.center,
+                    child: const Text(
+                      'Connect My Friend',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 18,
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      ),
                     ),
                   ),
                 ),
               ),
+            );
+          },
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+        InkWell(
+          child: Text(
+            'Need Help?',
+            style: TextStyle(
+              color: Colors.grey.shade300,
+              decoration: TextDecoration.underline,
             ),
-          );
-        }),
-        const SizedBox(height: 16)
+          ),
+          onTap: () {
+            IntercomManager.instance.intercom.displayMessenger();
+          },
+        ),
+        const SizedBox(height: 10)
       ],
     );
   }
