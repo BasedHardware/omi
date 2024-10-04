03c7f1fe3deaec8077899df334163af8bd8198f6
Ash Bhat <me@ashbhat.com>
2024-10-04 13:26:51 -0700

    updated package file

--- Package.swift
+++ Package.swift
@@ -1,19 +1,37 @@
-// swift-tools-version:5.3
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Omi",
+    name: "omi-lib",
+    platforms: [
+        .iOS(.v17)  // Set minimum version to iOS 17
+    ],
     products: [
         .library(
-            name: "OmiSDK",
-            targets: ["OmiSDK"]
+            name: "omi-lib",
+            targets: ["omi-lib"]
         ),
+    ],
+    dependencies: [
+        // Add your dependency here
+        .package(url: "https://github.com/nelcea/swift-opus.git", from: "1.0.0"),
+        .package(url: "https://github.com/exPHAT/SwiftWhisper.git", branch: "fast"),
+        .package(url: "https://github.com/AudioKit/AudioKit.git", from: "5.6.4"),
     ],
     targets: [
         .target(
-            name: "OmiSDK",
-            path: "sdks/swift"
-        )
+            name: "omi-lib",
+            dependencies: [
+                .product(name: "Opus", package: "swift-opus"),
+                .product(name: "SwiftWhisper", package: "SwiftWhisper"),
+                .product(name: "AudioKit", package: "AudioKit"),
+            ],
+            path: "sdks/swift",  // Correct the path to your source files
+            resources: [
+                .process("ggml-tiny.en.bin")  // Make sure this resource is in the correct directory
+            ]
+        ),
     ]
 )
