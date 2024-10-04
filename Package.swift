da58b7dd844bdfaac384acd93ddb55f4809d53cd
Ash Bhat <me@ashbhat.com>
2024-10-04 13:21:37 -0700

    re added package

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "Omi",
+    products: [
+        .library(
+            name: "OmiSDK",
+            targets: ["OmiSDK"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "OmiSDK",
+            path: "sdks/swift"
+        )
+    ]
+)
