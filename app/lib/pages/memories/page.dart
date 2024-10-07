be6adcd4b11d52323a0b7b9fffba2b690467c0de
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-10-07 23:32:56 +0530

    sync banner and sync tracking in provider

--- app/lib/pages/memories/page.dart
+++ app/lib/pages/memories/page.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:friend_private/backend/schema/memory.dart';
 import 'package:friend_private/pages/capture/widgets/widgets.dart';
+import 'package:friend_private/pages/memories/sync_page.dart';
 import 'package:friend_private/pages/memories/widgets/date_list_item.dart';
 import 'package:friend_private/pages/memories/widgets/processing_capture.dart';
 import 'package:friend_private/providers/memory_provider.dart';
+import 'package:friend_private/utils/other/temp.dart';
 import 'package:provider/provider.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
 import 'widgets/empty_memories.dart';
 import 'widgets/memory_list_item.dart';
+
+String secondsToHumanReadable(int seconds) {
+  if (seconds < 60) {
+    return '$seconds secs';
+  } else if (seconds < 3600) {
+    var minutes = (seconds / 60).floor();
+    var remainingSeconds = seconds % 60;
+    if (remainingSeconds == 0) {
+      return '$minutes mins';
+    } else {
+      return '$minutes mins $remainingSeconds secs';
+    }
+  } else if (seconds < 86400) {
+    var hours = (seconds / 3600).floor();
+    var remainingMinutes = (seconds % 3600 / 60).floor();
+    if (remainingMinutes == 0) {
+      return '$hours hours';
+    } else {
+      return '$hours hours $remainingMinutes mins';
+    }
+  } else {
+    var days = (seconds / 86400).floor();
+    var remainingHours = (seconds % 86400 / 3600).floor();
+    if (remainingHours == 0) {
+      return '$days days';
+    } else {
+      return '$days days $remainingHours hours';
+    }
+  }
+}
 
 class MemoriesPage extends StatefulWidget {
   const MemoriesPage({super.key});
@@ -46,25 +78,32 @@ class _MemoriesPageState extends State<MemoriesPage> with AutomaticKeepAliveClie
         },
         child: CustomScrollView(
           slivers: [
-            // TODO: FIXME
             SliverToBoxAdapter(
-                child: memoryProvider.missingWals.isNotEmpty
-                    ? Padding(
-                        padding: const EdgeInsets.all(32.0),
-                        child: Center(
-                          child: GestureDetector(
-                            onTap: () {
-                              memoryProvider.syncWals();
-                            },
-                            child: memoryProvider.walsSyncedProgress == 0.0
-                                ? Text(
-                                    "You have ${memoryProvider.missingWals.map((val) => val.seconds).reduce((a, b) => a + b)}s stereo localy, sync now?")
-                                : Text("${(memoryProvider.walsSyncedProgress * 100).toInt()}%"),
+              child: memoryProvider.missingWals.isNotEmpty
+                  ? GestureDetector(
+                      onTap: () {
+                        routeToPage(context, const SyncPage());
+                      },
+                      child: Container(
+                        width: double.maxFinite,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade900,
+                          borderRadius: const BorderRadius.all(Radius.circular(12)),
+                        ),
+                        margin: const EdgeInsets.fromLTRB(16, 18, 16, 0),
+                        padding: const EdgeInsets.all(16),
+                        child: ListTile(
+                          leading: const Icon(Icons.record_voice_over_rounded),
+                          title: Text(
+                            'You have ${secondsToHumanReadable(memoryProvider.missingWals.map((val) => val.seconds).reduce((a, b) => a + b))} of conversation locally, sync now?',
+                            style: const TextStyle(color: Colors.white, fontSize: 16),
                           ),
                         ),
-                      )
-                    : SizedBox.shrink()),
-            const SliverToBoxAdapter(child: SizedBox(height: 32)),
+                      ),
+                    )
+                  : const SizedBox.shrink(),
+            ),
+            const SliverToBoxAdapter(child: SizedBox(height: 26)),
             const SliverToBoxAdapter(child: SpeechProfileCardWidget()),
             const SliverToBoxAdapter(child: UpdateFirmwareCardWidget()),
             const SliverToBoxAdapter(child: MemoryCaptureWidget()),
@@ -95,6 +134,7 @@ class _MemoriesPageState extends State<MemoriesPage> with AutomaticKeepAliveClie
                   childCount: memoryProvider.groupedMemories.length + 1,
                   (context, index) {
                     if (index == memoryProvider.groupedMemories.length) {
+                      print('loading more memories');
                       if (memoryProvider.isLoadingMemories) {
                         return const Center(
                           child: Padding(
