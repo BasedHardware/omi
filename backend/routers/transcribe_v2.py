bee8ada6d6930fcfd908ec9ffd3485288d269b33
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-10-10 12:26:04 -0700

    transcribe v2 VAD for only for 1.0.2

--- backend/routers/transcribe_v2.py
+++ backend/routers/transcribe_v2.py
@@ -329,16 +329,16 @@ async def _websocket_util(
 
                 if codec == 'opus' and sample_rate == 16000:
                     data = decoder.decode(bytes(data), frame_size=160)
-                # if uid not in ['QnwpAf9QjLg5taNlT5OZ4kVnOLH3', '5IpB4K1ovyeNaJOT0wLJKuKTDgw2']:
-                #     if include_speech_profile:
-                #         vad_sample_size = 320 if sample_rate == 16000 else 160
-                #         vad_sample = data[:vad_sample_size]
-                #         if len(vad_sample) < vad_sample_size:
-                #             # print('VAD sample is less than 320 bytes', len(vad_sample))
-                #             vad_sample = vad_sample + bytes([0x00] * (vad_sample_size - len(vad_sample)))
-                #         has_speech = w_vad.is_speech(vad_sample, sample_rate)
-                #         if not has_speech:
-                #             continue
+
+                if include_speech_profile and sample_rate == 8000: # don't do for opus 1.0.4 for now
+                    vad_sample_size = 320 if sample_rate == 16000 else 160
+                    vad_sample = data[:vad_sample_size]
+                    if len(vad_sample) < vad_sample_size:
+                        # print('VAD sample is less than 320 bytes', len(vad_sample))
+                        vad_sample = vad_sample + bytes([0x00] * (vad_sample_size - len(vad_sample)))
+                    has_speech = w_vad.is_speech(vad_sample, sample_rate)
+                    if not has_speech:
+                        continue
 
                 # TODO: is the VAD slowing down the STT service? specially soniox?
                 # - but from write data, it feels faster, but the processing is having issues
