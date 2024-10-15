b38189a36d2593242445fb5db6ee5da315eee061
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-10-15 20:51:10 +0530

    change plugin page to use selectors for fewer rebuilds

--- app/lib/pages/plugins/page.dart
+++ app/lib/pages/plugins/page.dart
@@ -14,7 +14,7 @@ class PluginsPage extends StatefulWidget {
   State<PluginsPage> createState() => _PluginsPageState();
 }
 
-class _PluginsPageState extends State<PluginsPage> {
+class _PluginsPageState extends State<PluginsPage> with AutomaticKeepAliveClientMixin {
   @override
   void initState() {
     WidgetsBinding.instance.addPostFrameCallback((_) {
@@ -25,151 +25,164 @@ class _PluginsPageState extends State<PluginsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<PluginProvider>(builder: (context, provider, child) {
-      List<Plugin> memoryPromptPlugins = provider.plugins.where((p) => p.worksWithMemories()).toList();
-      List<Plugin> memoryIntegrationPlugins = provider.plugins.where((p) => p.worksExternally()).toList();
-      List<Plugin> chatPromptPlugins = provider.plugins.where((p) => p.worksWithChat()).toList();
+    super.build(context);
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      appBar: widget.filterChatOnly
+          ? AppBar(
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              automaticallyImplyLeading: true,
+              title: const Text('Apps'),
+              centerTitle: true,
+              elevation: 0,
+            )
+          : null,
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: DefaultTabController(
+            length: 2,
+            initialIndex: widget.filterChatOnly ? 1 : 0,
+            child: Column(
+              children: [
+                TabBar(
+                  indicatorSize: TabBarIndicatorSize.label,
+                  isScrollable: false,
+                  padding: EdgeInsets.zero,
+                  indicatorPadding: EdgeInsets.zero,
+                  labelStyle: Theme.of(context).textTheme.titleLarge!.copyWith(fontSize: 18),
+                  indicatorColor: Colors.transparent,
+                  tabs: const [Tab(text: 'Memories'), Tab(text: 'Chat')],
+                ),
+                Expanded(
+                  child: TabBarView(children: [
+                    CustomScrollView(
+                      slivers: [
+                        const EmptyPluginsWidget(),
+                        const SectionTitleWidget(title: 'External Apps', emoji: '🚀'),
+                        Selector<PluginProvider, List<Plugin>>(
+                            selector: (context, provider) =>
+                                provider.plugins.where((p) => p.worksExternally()).toList(),
+                            builder: (context, memoryIntegrationPlugins, child) {
+                              return SliverList(
+                                delegate: SliverChildBuilderDelegate(
+                                  (context, index) {
+                                    return PluginListItem(
+                                      plugin: memoryIntegrationPlugins[index],
+                                      index: index,
+                                    );
+                                  },
+                                  childCount: memoryIntegrationPlugins.length,
+                                ),
+                              );
+                            }),
+                        context.read<PluginProvider>().plugins.isNotEmpty
+                            ? SliverToBoxAdapter(child: Divider(color: Colors.grey.shade800, thickness: 1))
+                            : const SliverToBoxAdapter(child: SizedBox.shrink()),
+                        const SectionTitleWidget(title: 'Prompts', emoji: '📝'),
+                        Selector<PluginProvider, List<Plugin>>(
+                            selector: (context, provider) =>
+                                provider.plugins.where((p) => p.worksWithMemories()).toList(),
+                            builder: (context, memoryPromptPlugins, child) {
+                              return SliverList(
+                                delegate: SliverChildBuilderDelegate(
+                                  (context, index) {
+                                    return PluginListItem(
+                                      plugin: memoryPromptPlugins[index],
+                                      index: index,
+                                    );
+                                  },
+                                  childCount: memoryPromptPlugins.length,
+                                ),
+                              );
+                            }),
+                      ],
+                    ),
+                    CustomScrollView(
+                      slivers: [
+                        const EmptyPluginsWidget(),
+                        const SectionTitleWidget(title: 'Personalities', emoji: '🤖'),
+                        Selector<PluginProvider, List<Plugin>>(
+                            selector: (context, provider) => provider.plugins.where((p) => p.worksWithChat()).toList(),
+                            builder: (context, chatPromptPlugins, child) {
+                              return SliverList(
+                                delegate: SliverChildBuilderDelegate(
+                                  (context, index) {
+                                    return PluginListItem(
+                                      plugin: chatPromptPlugins[index],
+                                      index: index,
+                                    );
+                                  },
+                                  childCount: chatPromptPlugins.length,
+                                ),
+                              );
+                            }),
+                      ],
+                    ),
+                  ]),
+                )
+              ],
+            )),
+      ),
+    );
+  }
 
-      return Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.primary,
-          automaticallyImplyLeading: true,
-          title: const Text('Plugins'),
-          centerTitle: true,
-          elevation: 0,
-        ),
-        body: GestureDetector(
-          onTap: () => FocusScope.of(context).unfocus(),
-          child: DefaultTabController(
-              length: 2,
-              initialIndex: widget.filterChatOnly ? 1 : 0,
-              child: Column(
-                children: [
-                  TabBar(
-                    indicatorSize: TabBarIndicatorSize.label,
-                    isScrollable: false,
-                    padding: EdgeInsets.zero,
-                    indicatorPadding: EdgeInsets.zero,
-                    labelStyle: Theme.of(context).textTheme.titleLarge!.copyWith(fontSize: 18),
-                    indicatorColor: Colors.transparent,
-                    tabs: const [Tab(text: 'Memories'), Tab(text: 'Chat')],
+  @override
+  bool get wantKeepAlive => true;
+}
+
+class SectionTitleWidget extends StatelessWidget {
+  final String title;
+  final String emoji;
+  const SectionTitleWidget({super.key, required this.title, required this.emoji});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<PluginProvider>(builder: (context, provider, child) {
+      return provider.plugins.isNotEmpty
+          ? SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 32),
+                child: GestureDetector(
+                  child: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(title, style: const TextStyle(color: Colors.white, fontSize: 18)),
+                        const SizedBox(width: 12),
+                        Text(emoji, style: const TextStyle(fontSize: 18)),
+                      ],
+                    ),
                   ),
-                  Expanded(
-                    child: TabBarView(children: [
-                      CustomScrollView(
-                        slivers: [
-                          _emptyPluginsWidget(provider),
-                          _getSectionTitle(context, provider, 'External Apps', '🚀'),
-                          SliverList(
-                            delegate: SliverChildBuilderDelegate(
-                              (context, index) {
-                                return PluginListItem(
-                                  plugin: memoryIntegrationPlugins[index],
-                                  index: index,
-                                  provider: provider,
-                                );
-                              },
-                              childCount: memoryIntegrationPlugins.length,
-                            ),
-                          ),
-                          provider.plugins.isNotEmpty
-                              ? SliverToBoxAdapter(child: Divider(color: Colors.grey.shade800, thickness: 1))
-                              : const SliverToBoxAdapter(child: SizedBox.shrink()),
-                          _getSectionTitle(context, provider, 'Prompts', '📝'),
-                          SliverList(
-                            delegate: SliverChildBuilderDelegate(
-                              (context, index) {
-                                return PluginListItem(
-                                  plugin: memoryPromptPlugins[index],
-                                  index: index,
-                                  provider: provider,
-                                );
-                              },
-                              childCount: memoryPromptPlugins.length,
-                            ),
-                          ),
-                        ],
-                      ),
-                      CustomScrollView(
-                        slivers: [
-                          _emptyPluginsWidget(provider),
-                          _getSectionTitle(context, provider, 'Personalities', '🤖'),
-                          SliverList(
-                            delegate: SliverChildBuilderDelegate(
-                              (context, index) {
-                                return PluginListItem(
-                                  plugin: chatPromptPlugins[index],
-                                  index: index,
-                                  provider: provider,
-                                );
-                              },
-                              childCount: chatPromptPlugins.length,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ]),
-                  )
-                ],
-              )),
-        ),
-      );
+                ),
+              ),
+            )
+          : const SliverToBoxAdapter(child: SizedBox.shrink());
     });
   }
+}
 
-  _getSectionTitle(BuildContext context, PluginProvider provider, String title, String emoji) {
-    return provider.plugins.isNotEmpty
-        ? SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 32),
-              child: GestureDetector(
-                // onTap: () {
-                //   showDialog(
-                //     context: context,
-                //     builder: getDialog(
-                //       context,
-                //       () => Navigator.pop(context),
-                //       () => Navigator.pop(context),
-                //       'asd',
-                //       'asd',
-                //       singleButton: true,
-                //       okButtonText: 'Ok',
-                //     ),
-                //   );
-                // },
+class EmptyPluginsWidget extends StatelessWidget {
+  const EmptyPluginsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<PluginProvider>(builder: (context, provider, child) {
+      return provider.plugins.isEmpty
+          ? SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 64, left: 14, right: 14),
                 child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(title, style: const TextStyle(color: Colors.white, fontSize: 18)),
-                      const SizedBox(width: 12),
-                      Text(emoji, style: const TextStyle(fontSize: 18)),
-                    ],
+                  child: Text(
+                    context.read<ConnectivityProvider>().isConnected
+                        ? 'No apps found'
+                        : 'Unable to fetch apps :(\n\nPlease check your internet connection and try again.',
+                    style: const TextStyle(color: Colors.white, fontSize: 16),
+                    textAlign: TextAlign.center,
                   ),
                 ),
               ),
-            ),
-          )
-        : const SliverToBoxAdapter(child: SizedBox.shrink());
-  }
-
-  _emptyPluginsWidget(provider) {
-    return provider.plugins.isEmpty
-        ? SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 64, left: 14, right: 14),
-              child: Center(
-                child: Text(
-                  context.read<ConnectivityProvider>().isConnected
-                      ? 'No plugins found'
-                      : 'Unable to fetch plugins :(\n\nPlease check your internet connection and try again.',
-                  style: const TextStyle(color: Colors.white, fontSize: 16),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-          )
-        : const SliverToBoxAdapter(child: SizedBox.shrink());
+            )
+          : const SliverToBoxAdapter(child: SizedBox.shrink());
+    });
   }
 }
