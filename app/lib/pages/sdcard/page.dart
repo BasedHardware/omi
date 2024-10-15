0dc091005ebe62c60834af796b5398c7ffc60f2a
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-10-15 22:19:53 +0530

    improve sdcard page ui

--- app/lib/pages/sdcard/page.dart
+++ app/lib/pages/sdcard/page.dart
@@ -3,6 +3,8 @@ import 'package:friend_private/providers/capture_provider.dart';
 import 'package:friend_private/providers/device_provider.dart';
 import 'package:provider/provider.dart';
 
+import 'sdcard_transfer_progress.dart';
+
 class SdCardCapturePage extends StatefulWidget {
   const SdCardCapturePage({super.key});
 
@@ -14,66 +16,74 @@ class _SdCardCapturePageState extends State<SdCardCapturePage> {
   @override
   Widget build(BuildContext context) {
     return Consumer2<CaptureProvider, DeviceProvider>(builder: (context, provider, deviceProvider, child) {
-      String sdCardSecondsRemaining = (provider.sdCardSecondsTotal - provider.sdCardSecondsReceived).toStringAsFixed(2);
-      String percentageRemaining = provider.totalStorageFileBytes == 0
-          ? '0.0'
-          : (provider.totalBytesReceived / provider.totalStorageFileBytes * 100).toStringAsFixed(2);
-
-      String displayText = 'about $sdCardSecondsRemaining seconds remaining\n$percentageRemaining% there';
-      if (provider.sdCardDownloadDone) {
-        displayText = 'Done! Check back later for your memories.';
-      }
-
-      return Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        appBar: AppBar(
+      return PopScope(
+        canPop: !provider.sdCardIsDownloading,
+        onPopInvoked: (didPop) {
+          // show dialog when downloading
+        },
+        child: Scaffold(
           backgroundColor: Theme.of(context).colorScheme.primary,
-          automaticallyImplyLeading: true,
-          title: const Text('SD Card'),
-          centerTitle: true,
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back_ios_new),
-            onPressed: () {
-              Navigator.pop(context);
-            },
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            automaticallyImplyLeading: true,
+            title: const Text('SD Card'),
+            centerTitle: true,
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back_ios_new),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            elevation: 0,
           ),
-          elevation: 0,
-        ),
-        body: Center(
-          child: Column(
+          body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
             children: [
-              LinearProgressIndicator(
-                value: provider.sdCardSecondsReceived / (provider.sdCardSecondsTotal == 0 ? 1 : provider.sdCardSecondsTotal),
-                backgroundColor: Colors.grey,
-                color: Colors.green,
-                minHeight: 10,
-              ),
-              const SizedBox(height: 20),
-              Center(
-                child: Text(displayText),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
+              SizedBox(
+                height: MediaQuery.sizeOf(context).height * 0.44,
+                child: SdCardTransferProgress(
+                  displayPercentage: provider.totalStorageFileBytes == 0
+                      ? '0.0'
+                      : (provider.totalBytesReceived / provider.totalStorageFileBytes * 100).toStringAsFixed(2),
+                  progress: provider.sdCardSecondsReceived /
+                      (provider.sdCardSecondsTotal == 0 ? 1 : provider.sdCardSecondsTotal),
                 ),
-                onPressed: () {
-                  setState(() {
-                    displayText = 'about $sdCardSecondsRemaining seconds remaining, about $percentageRemaining% there';
-                  });
-                  if (!provider.sdCardIsDownloading) {
-                    provider.sendStorage(deviceProvider.connectedDevice!.id);
-                    provider.setSdCardIsDownloading(true);
-                  }
-                },
-                child: const Text('Click to starting Importing Memories'),
               ),
+              provider.sdCardIsDownloading
+                  ? const SizedBox.shrink()
+                  : ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                      ),
+                      onPressed: () {
+                        // setState(() {
+                        //   displayText =
+                        //       'about $sdCardSecondsRemaining seconds remaining, about $percentageRemaining% there';
+                        // });
+                        if (!provider.sdCardIsDownloading) {
+                          provider.sendStorage(deviceProvider.connectedDevice!.id);
+                          provider.setSdCardIsDownloading(true);
+                        }
+                      },
+                      child: const Text('Start Importing Memories'),
+                    ),
+              provider.sdCardDownloadDone
+                  ? const Text('Done! Check back later for your memories.')
+                  : Container(
+                      width: double.maxFinite,
+                      padding: const EdgeInsets.all(16.0),
+                      margin: const EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade900,
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      child: const Text(
+                        'This download may take a while. Exiting this while the download is in progress will halt all progress and some memories may be lost.\n\nPlease ensure that you have good internet connection.',
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
               const SizedBox(height: 20),
-              const Text(
-                  'This download may take a while. Exiting this while the download is in progress will halt all progress and some memories may be lost.'),
-              const SizedBox(height: 20),
-              const Text('Please ensure that you have good internet connection.'),
             ],
           ),
         ),
