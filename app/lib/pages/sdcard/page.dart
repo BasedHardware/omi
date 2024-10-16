1bb317ce6458d25bf473c2c9b68e2c2d375655ba
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-10-16 20:37:55 +0530

    improve sdcard sync overall ui and text copy

--- app/lib/pages/sdcard/page.dart
+++ app/lib/pages/sdcard/page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:friend_private/providers/capture_provider.dart';
 import 'package:friend_private/providers/device_provider.dart';
+import 'package:gradient_borders/box_borders/gradient_box_border.dart';
 import 'package:provider/provider.dart';
 
+import 'about_sdcard_sync.dart';
 import 'sdcard_transfer_progress.dart';
 
 class SdCardCapturePage extends StatefulWidget {
@@ -17,16 +19,17 @@ class _SdCardCapturePageState extends State<SdCardCapturePage> {
   Widget build(BuildContext context) {
     return Consumer2<CaptureProvider, DeviceProvider>(builder: (context, provider, deviceProvider, child) {
       return PopScope(
-        canPop: !provider.sdCardIsDownloading,
+        canPop: true,
         onPopInvoked: (didPop) {
           // show dialog when downloading
+          // I don't think this is necessary, the only situation that will cause this to fail is if the user exits the app or restarts it
         },
         child: Scaffold(
           backgroundColor: Theme.of(context).colorScheme.primary,
           appBar: AppBar(
             backgroundColor: Theme.of(context).colorScheme.primary,
             automaticallyImplyLeading: true,
-            title: const Text('SD Card'),
+            title: const Text('SD Card Sync'),
             centerTitle: true,
             leading: IconButton(
               icon: const Icon(Icons.arrow_back_ios_new),
@@ -35,55 +38,113 @@ class _SdCardCapturePageState extends State<SdCardCapturePage> {
               },
             ),
             elevation: 0,
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.help_outline),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const AboutSdCardSync(),
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
           body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             mainAxisSize: MainAxisSize.min,
             children: [
-              SizedBox(
-                height: MediaQuery.sizeOf(context).height * 0.44,
+              Container(
+                height: MediaQuery.sizeOf(context).height * 0.365,
                 child: SdCardTransferProgress(
                   displayPercentage: provider.totalStorageFileBytes == 0
                       ? '0.0'
                       : (provider.totalBytesReceived / provider.totalStorageFileBytes * 100).toStringAsFixed(2),
                   progress: provider.sdCardSecondsReceived /
                       (provider.sdCardSecondsTotal == 0 ? 1 : provider.sdCardSecondsTotal),
+                  secondsRemaining: (provider.sdCardSecondsTotal - provider.sdCardSecondsReceived).toStringAsFixed(2),
                 ),
               ),
-              provider.sdCardIsDownloading
+              provider.sdCardIsDownloading || provider.sdCardDownloadDone
                   ? const SizedBox.shrink()
-                  : ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                      ),
-                      onPressed: () {
-                        // setState(() {
-                        //   displayText =
-                        //       'about $sdCardSecondsRemaining seconds remaining, about $percentageRemaining% there';
-                        // });
-                        if (!provider.sdCardIsDownloading) {
-                          provider.sendStorage(deviceProvider.connectedDevice!.id);
-                          provider.setSdCardIsDownloading(true);
-                        }
-                      },
-                      child: const Text('Start Importing Memories'),
-                    ),
-              provider.sdCardDownloadDone
-                  ? const Text('Done! Check back later for your memories.')
                   : Container(
-                      width: double.maxFinite,
-                      padding: const EdgeInsets.all(16.0),
-                      margin: const EdgeInsets.all(16.0),
                       decoration: BoxDecoration(
-                        color: Colors.grey.shade900,
-                        borderRadius: BorderRadius.circular(16.0),
+                        border: const GradientBoxBorder(
+                          gradient: LinearGradient(colors: [
+                            Color.fromARGB(127, 208, 208, 208),
+                            Color.fromARGB(127, 188, 99, 121),
+                            Color.fromARGB(127, 86, 101, 182),
+                            Color.fromARGB(127, 126, 190, 236)
+                          ]),
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
                       ),
-                      child: const Text(
-                        'This download may take a while. Exiting this while the download is in progress will halt all progress and some memories may be lost.\n\nPlease ensure that you have good internet connection.',
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (!provider.sdCardIsDownloading) {
+                            provider.sendStorage(deviceProvider.connectedDevice!.id);
+                            provider.setSdCardIsDownloading(true);
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                          shadowColor: const Color.fromARGB(255, 17, 17, 17),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        child: Container(
+                          width: 200,
+                          height: 45,
+                          alignment: Alignment.center,
+                          child: const Text(
+                            'Import Audio Files',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                              color: Color.fromARGB(255, 255, 255, 255),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                margin: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade900,
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                child: provider.sdCardDownloadDone
+                    ? const Text(
+                        'Audio files have been imported successfully!\n\nMemories will appear once they have been created on the server. You can now continue with recording your conversations.',
+                        textAlign: TextAlign.center)
+                    : const Text(
+                        'Importing audio files might take some time. Please keep the app open to avoid any interruptions and ensure you have a stable internet connection',
                         textAlign: TextAlign.center,
                       ),
-                    ),
-              const SizedBox(height: 20),
+              ),
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                margin: const EdgeInsets.only(left: 16.0, right: 16.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade900,
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                child: const ListTile(
+                  leading: Icon(
+                    Icons.warning,
+                    color: Colors.yellow,
+                  ),
+                  title: Text('No conversation will be recorded until the import is complete'),
+                ),
+              ),
             ],
           ),
         ),
