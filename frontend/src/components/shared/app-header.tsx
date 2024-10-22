4ecf63aac7f3feeabafc60a5924eccacd2da36f4
Nikia Shevchenko <kodjima33@gmail.com>
2024-10-22 12:35:32 -0700

    hackathon announcement

--- frontend/src/components/shared/app-header.tsx
+++ frontend/src/components/shared/app-header.tsx
@@ -25,47 +25,59 @@ export default function AppHeader() {
   }, []);
 
   return !dreamforcePage ? (
-    <header
-      className={`fixed top-0 z-30 flex w-full items-center justify-between bg-black/40 p-4 px-4 text-white transition-all duration-500 md:bg-transparent md:px-12 ${
-        scrollPosition > 100 ? 'backdrop-blur-md md:!bg-black md:!bg-opacity-10' : ''
-      }`}
-    >
-      <h1 className="flex items-center gap-2 text-xl">
-        <Image
-          src={'/omi-white.webp'}
-          alt="Based Hardware Logo"
-          width={146}
-          height={64}
-          className="h-auto w-[50px]"
-        />
-      </h1>
-      <nav>
-        <ul className="flex gap-3 text-sm md:gap-4 md:text-base">
-          {params.id && (
+    <>
+      <header
+        className={`fixed top-0 z-30 flex w-full items-center justify-between bg-black/40 p-4 px-4 text-white transition-all duration-500 md:bg-transparent md:px-12 ${
+          scrollPosition > 100 ? 'backdrop-blur-md md:!bg-black md:!bg-opacity-10' : ''
+        }`}
+      >
+        <h1 className="flex items-center gap-2 text-xl">
+          <Image
+            src={'/omi-white.webp'}
+            alt="Based Hardware Logo"
+            width={146}
+            height={64}
+            className="h-auto w-[50px]"
+          />
+        </h1>
+        <nav>
+          <ul className="flex gap-3 text-sm md:gap-4 md:text-base">
+            {params.id && (
+              <li>
+                <ShareButton />
+              </li>
+            )}
             <li>
-              <ShareButton />
+              <Link
+                href={`https://basedhardware.com/`}
+                target="_blank"
+                className="flex items-center gap-2 rounded-md bg-white/90 p-1.5 px-3.5 text-black transition-colors hover:bg-white"
+              >
+                Order now
+              </Link>
             </li>
-          )}
-          <li>
-            <Link
-              href={`https://basedhardware.com/`}
-              target="_blank"
-              className="flex items-center gap-2 rounded-md bg-white/90 p-1.5 px-3.5 text-black transition-colors hover:bg-white"
-            >
-              Order now
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={`https://docs.omi.me/developer/plugins/Introduction`}
-              target="_blank"
-              className="flex items-center gap-2 rounded-md bg-white/90 p-1.5 px-3.5 text-black transition-colors hover:bg-white"
-            >
-              Create App
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+            <li>
+              <Link
+                href={`https://docs.omi.me/developer/plugins/Introduction`}
+                target="_blank"
+                className="flex items-center gap-2 rounded-md bg-white/90 p-1.5 px-3.5 text-black transition-colors hover:bg-white"
+              >
+                Build Plugin
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className="fixed top-16 z-30 w-full bg-blue-500 p-2 text-center text-white">
+        <span>Submit your own App by November 30th and get a free trip to OMI HQ in San Francisco</span>
+        <Link
+          href="https://discord.gg/based-hardware-1192313062041067520"
+          target="_blank"
+          className="ml-4 inline-block rounded-md bg-white px-3 py-1 text-blue-500 hover:bg-blue-100"
+        >
+          Join Hackathon
+        </Link>
+      </div>
+    </>
   ) : null;
 }
