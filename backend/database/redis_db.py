f88489ac0995b14d805c60a2cdafda234e2eef6b
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-10-23 17:27:03 +0530

    return empty url from get_user_webhook_db if not toggled

--- backend/database/redis_db.py
+++ backend/database/redis_db.py
@@ -249,7 +249,11 @@ def user_webhook_status_db(uid: str, wtype: str):
 
 
 def get_user_webhook_db(uid: str, wtype: str) -> str:
-    url = r.get(f'users:{uid}:developer:webhook:{wtype}')
-    if not url:
+    toggled = user_webhook_status_db(uid, wtype)
+    if not toggled:
         return ''
+    else:
+        url = r.get(f'users:{uid}:developer:webhook:{wtype}')
+        if not url:
+            return ''
     return url.decode()
