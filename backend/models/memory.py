bc7ad87afa63440b753a58e4b888d28164e5baa8
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-10-24 15:40:29 +0530

    soft delete action item backend

--- backend/models/memory.py
+++ backend/models/memory.py
@@ -52,6 +52,7 @@ class PluginResult(BaseModel):
 class ActionItem(BaseModel):
     description: str = Field(description="The action item to be completed")
     completed: bool = False  # IGNORE ME from the model parser
+    deleted: bool = False
 
 
 class Event(BaseModel):
@@ -251,4 +252,9 @@ class SetMemoryEventsStateRequest(BaseModel):
 class SetMemoryActionItemsStateRequest(BaseModel):
     items_idx: List[int]
     values: List[bool]
+
+
+class DeleteActionItemRequest(BaseModel):
+    description: str
+    completed: bool
 
