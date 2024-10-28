fe9a0e26cee35048ba0675cf345c11a645439760
thịnh <ngocthinhdp@gmail.com>
2024-10-28 12:41:05 +0700

    Add pusher

---
+++ backend/pusher/main.py
@@ -0,0 +1,47 @@
+import json
+import os
+
+import firebase_admin
+from fastapi import FastAPI
+
+from modal import Image, App, asgi_app, Secret
+from routers import pusher
+
+if os.environ.get('SERVICE_ACCOUNT_JSON'):
+    service_account_info = json.loads(os.environ["SERVICE_ACCOUNT_JSON"])
+    credentials = firebase_admin.credentials.Certificate(service_account_info)
+    firebase_admin.initialize_app(credentials)
+else:
+    firebase_admin.initialize_app()
+
+app = FastAPI()
+app.include_router(pusher.router)
+
+modal_app = App(
+    name='pusher',
+    secrets=[Secret.from_name("gcp-credentials"), Secret.from_name('envs')],
+)
+image = (
+    Image.debian_slim()
+    .apt_install('ffmpeg', 'git', 'unzip')
+    .pip_install_from_requirements('requirements.txt')
+)
+
+
+@modal_app.function(
+    image=image,
+    keep_warm=2,
+    memory=(512, 1024),
+    cpu=2,
+    allow_concurrent_inputs=10,
+    timeout=60 * 10,
+)
+@asgi_app()
+def api():
+    return app
+
+
+paths = ['_temp', '_samples', '_segments', '_speech_profiles']
+for path in paths:
+    if not os.path.exists(path):
+        os.makedirs(path)
