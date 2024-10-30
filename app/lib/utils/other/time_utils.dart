2b2cae56f5358cad0de886003112cc8ffc88d9ef
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-10-30 18:42:01 +0530

    util improvements

--- app/lib/utils/other/time_utils.dart
+++ app/lib/utils/other/time_utils.dart
@@ -5,6 +5,9 @@ String secondsToHumanReadable(int seconds) {
     var minutes = (seconds / 60).floor();
     var remainingSeconds = seconds % 60;
     if (remainingSeconds == 0) {
+      if (minutes == 1) {
+        return '$minutes min';
+      }
       return '$minutes mins';
     } else {
       return '$minutes mins $remainingSeconds secs';
@@ -13,6 +16,9 @@ String secondsToHumanReadable(int seconds) {
     var hours = (seconds / 3600).floor();
     var remainingMinutes = (seconds % 3600 / 60).floor();
     if (remainingMinutes == 0) {
+      if (hours == 1) {
+        return '$hours hour';
+      }
       return '$hours hours';
     } else {
       return '$hours hours $remainingMinutes mins';
@@ -21,9 +27,20 @@ String secondsToHumanReadable(int seconds) {
     var days = (seconds / 86400).floor();
     var remainingHours = (seconds % 86400 / 3600).floor();
     if (remainingHours == 0) {
+      if (days == 1) {
+        return '$days day';
+      }
       return '$days days';
     } else {
       return '$days days $remainingHours hours';
     }
   }
+}
+
+// convert seconds to hh:mm:ss format
+String secondsToHMS(int seconds) {
+  var hours = (seconds / 3600).floor();
+  var minutes = (seconds % 3600 / 60).floor();
+  var remainingSeconds = seconds % 60;
+  return '$hours:$minutes:$remainingSeconds';
 }
