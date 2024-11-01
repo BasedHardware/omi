88ffdfcd0a18f8f38e0e104997b364093ea9bb0d
Vincent Koc <vincentkoc@ieee.org>
2024-11-02 09:42:06 +1100

    Update mock_helper.dart

--- app/test/helpers/mock_helper.dart
+++ app/test/helpers/mock_helper.dart
@@ -1,15 +1,15 @@
 import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:firebase_auth_platform_interface/firebase_auth_platform_interface.dart';
 
 void setupFirebaseMocks() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  // Setup mock platform interface for Firebase
-  final platform = TestFirebasePlatform();
-  FirebasePlatform.instance = platform;
+  // Setup mock platform interface for Firebase Core
+  FirebasePlatform.instance = MockFirebasePlatform();
 
-  // Mock method channels
+  // Mock Firebase Core method channel
   const MethodChannel channel = MethodChannel('plugins.flutter.io/firebase_core');
   TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
       .setMockMethodCallHandler(channel, (call) async {
@@ -35,22 +35,30 @@ void setupFirebaseMocks() {
         return null;
     }
   });
+
+  // Mock Firebase Auth Pigeon API
+  const pigeonChannel = BinaryMessenger.setMockMessageHandler(
+    'dev.flutter.pigeon.FirebaseAuthHostApi.registerIdTokenListener',
+    (ByteData? message) async => null,
+  );
+
+  // Mock other required Pigeon channels
+  BinaryMessenger.setMockMessageHandler(
+    'dev.flutter.pigeon.FirebaseAuthHostApi.registerAuthStateListener',
+    (ByteData? message) async => null,
+  );
 }
 
-class TestFirebasePlatform extends FirebasePlatform {
+class MockFirebasePlatform extends FirebasePlatform {
   @override
   FirebaseAppPlatform app([String name = defaultFirebaseAppName]) {
-    return TestFirebaseAppPlatform(
-      TestFirebasePlatform._testAppName,
-      TestFirebasePlatform._testOptions,
-    );
+    return TestFirebaseAppPlatform(name, _testOptions);
   }
 
   @override
   List<FirebaseAppPlatform> get apps => [];
 
-  static const String _testAppName = '[DEFAULT]';
-  static final FirebaseOptions _testOptions = const FirebaseOptions(
+  static const FirebaseOptions _testOptions = FirebaseOptions(
     apiKey: 'test-api-key',
     appId: 'test-app-id',
     messagingSenderId: 'test-sender-id',
