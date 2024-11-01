1fad845ab3cdb75664665439e7582a2ed1d40fd8
Vincent Koc <vincentkoc@ieee.org>
2024-11-01 22:17:35 +1100

    Update mock_helper.dart

--- app/test/helpers/mock_helper.dart
+++ app/test/helpers/mock_helper.dart
@@ -2,74 +2,63 @@ import 'package:firebase_core_platform_interface/firebase_core_platform_interfac
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-class MockFirebasePlatform extends FirebasePlatform {
+void setupFirebaseMocks() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  // Setup mock platform interface for Firebase
+  final platform = TestFirebasePlatform();
+  FirebasePlatform.instance = platform;
+
+  // Mock method channels
+  const MethodChannel channel = MethodChannel('plugins.flutter.io/firebase_core');
+  TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+      .setMockMethodCallHandler(channel, (call) async {
+    switch (call.method) {
+      case 'Firebase#initializeCore':
+        return [
+          {
+            'name': '[DEFAULT]',
+            'options': {
+              'apiKey': 'test-api-key',
+              'appId': 'test-app-id',
+              'messagingSenderId': 'test-sender-id',
+              'projectId': 'test-project-id',
+            },
+          }
+        ];
+      case 'Firebase#initializeApp':
+        return {
+          'name': call.arguments['appName'],
+          'options': call.arguments['options'],
+        };
+      default:
+        return null;
+    }
+  });
+}
+
+class TestFirebasePlatform extends FirebasePlatform {
   @override
   FirebaseAppPlatform app([String name = defaultFirebaseAppName]) {
-    return MockFirebaseApp(
-      options: const FirebaseOptions(
-        apiKey: 'test-api-key',
-        appId: 'test-app-id',
-        messagingSenderId: 'test-sender-id',
-        projectId: 'test-project-id',
-      ),
+    return TestFirebaseAppPlatform(
+      TestFirebasePlatform._testAppName,
+      TestFirebasePlatform._testOptions,
     );
   }
 
   @override
   List<FirebaseAppPlatform> get apps => [];
+
+  static const String _testAppName = '[DEFAULT]';
+  static final FirebaseOptions _testOptions = const FirebaseOptions(
+    apiKey: 'test-api-key',
+    appId: 'test-app-id',
+    messagingSenderId: 'test-sender-id',
+    projectId: 'test-project-id',
+  );
 }
 
-class MockFirebaseApp extends FirebaseAppPlatform {
-  MockFirebaseApp({required FirebaseOptions options})
-      : super(defaultFirebaseAppName, options);
-
-  @override
-  Future<void> delete() async {}
-
-  @override
-  bool get isAutomaticDataCollectionEnabled => false;
-}
-
-void setupFirebaseMocks() {
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  // Setup mock platform
-  FirebasePlatform.instance = MockFirebasePlatform();
-
-  // Mock method channels
-  const MethodChannel('plugins.flutter.io/firebase_core')
-    .setMockMethodCallHandler((call) async {
-      switch (call.method) {
-        case 'Firebase#initializeCore':
-          return [
-            {
-              'name': defaultFirebaseAppName,
-              'options': {
-                'apiKey': 'test-api-key',
-                'appId': 'test-app-id',
-                'messagingSenderId': 'test-sender-id',
-                'projectId': 'test-project-id',
-              },
-            }
-          ];
-        case 'Firebase#initializeApp':
-          return {
-            'name': call.arguments['appName'],
-            'options': call.arguments['options'],
-          };
-        default:
-          return null;
-      }
-    });
-
-  // Mock auth channel
-  const MethodChannel('plugins.flutter.io/firebase_auth')
-    .setMockMethodCallHandler((call) async {
-      switch (call.method) {
-        case 'Auth#authStateChanges':
-          return null;
-        default:
-          return null;
-      }
-    });
+class TestFirebaseAppPlatform extends FirebaseAppPlatform {
+  TestFirebaseAppPlatform(String name, FirebaseOptions options)
+      : super(name, options);
 }
