95a20c672168c365d877209b19f0829b5d049c66
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-11-02 22:11:47 +0530

    upload plugin logo and new routes and db funcs

--- backend/database/plugins.py
+++ backend/database/plugins.py
@@ -1,6 +1,10 @@
 from datetime import datetime, timezone
+from typing import List
+
+import requests
 
 from models.plugin import UsageHistoryType
+from utils.other.storage import storage_client
 from ._client import db
 
 
@@ -29,3 +33,62 @@ def record_plugin_usage(
 def get_plugin_usage_history(plugin_id: str):
     usage = db.collection('plugins').document(plugin_id).collection('usage_history').stream()
     return [doc.to_dict() for doc in usage]
+
+
+def add_public_plugin(plugin_data: dict):
+    plugin_ref = db.collection('plugins_data')
+    plugin_ref.add(plugin_data, plugin_data['id'])
+
+
+def add_private_plugin(plugin_data: dict, uid: str):
+    plugin_ref = db.collection('users').document(uid).collection('plugins')
+    plugin_ref.add(plugin_data, plugin_data['id'])
+
+
+def change_plugin_approval_status(plugin_id: str, approved: bool):
+    plugin_ref = db.collection('plugins_data').document(plugin_id)
+    plugin_ref.update({'approved': approved})
+
+
+def change_plugin_visibility(plugin_id: str, private: bool, was_public: bool, uid: str):
+    if was_public:
+        plugin_ref = db.collection('plugins_data').document(plugin_id)
+        plugin = plugin_ref.get().to_dict()
+        plugin_ref.delete()
+        plugin_ref = db.collection('users').document(uid).collection('plugins').document(plugin_id)
+        plugin_ref.set(plugin)
+    else:
+        plugin_ref = db.collection('users').document(uid).collection('plugins').document(plugin_id)
+        plugin = plugin_ref.get().to_dict()
+        plugin_ref.delete()
+        plugin_ref = db.collection('plugins_data').document(plugin_id)
+        plugin_ref.set(plugin)
+
+
+def get_plugins_db(uid: str) -> List:
+    public_plugins = db.collection('plugins_data').stream()
+    private_plugins = db.collection('users').document(uid).collection('plugins').stream()
+    data = [doc.to_dict() for doc in public_plugins] + [doc.to_dict() for doc in private_plugins]
+    return data
+
+
+# TODO: only temporary, to move from the json file to firestore. Remove after the migration
+def add_plugin_script(plugin_data: dict):
+    img = requests.get("https://raw.githubusercontent.com/BasedHardware/Omi/main/" + plugin_data['image'], stream=True)
+    bucket = storage_client.bucket('omi_plugins')
+    path = plugin_data['image'].split('/plugins/logos/')[1]
+    blob = bucket.blob(path)
+    blob.upload_from_file(img.raw)
+    plugin_data['image'] = f'https://storage.googleapis.com/omi_plugins/{path}'
+    plugin_data['private'] = False
+    plugin_data['approved'] = True
+
+    if "external_integration" in plugin_data['capabilities']:
+        plugin_data['external_integration'][
+            'setup_instructions_file_path'] = "https://raw.githubusercontent.com/BasedHardware/Omi/main/" + \
+                                              plugin_data['external_integration']['setup_instructions_file_path']
+    plugin_ref = db.collection('plugins_data').document(plugin_data['id'])
+    if plugin_ref.get().exists:
+        plugin_ref.update(plugin_data)
+    else:
+        plugin_ref.set(plugin_data)
