5a1d1c9e423fbc13648c0fa5429994306fd4c99b
Vincent Koc <vincentkoc@ieee.org>
2024-11-03 23:31:40 +1100

    Update button.c

--- Friend/firmware/firmware_v1.0/src/button.c
+++ Friend/firmware/firmware_v1.0/src/button.c
@@ -444,3 +444,7 @@ FSM_STATE_T get_current_button_state()
 {
     return current_button_state;
 }
+
+extern void start_voice_interaction(void);
+extern void stop_voice_interaction(void);
+extern bool voice_interaction_active;
