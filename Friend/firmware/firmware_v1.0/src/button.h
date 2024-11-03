5c8d9a2de146aeba9307d54fcc92d170bfb13a3d
Vincent Koc <vincentkoc@ieee.org>
2024-11-03 23:48:25 +1100

    Update button.h

--- Friend/firmware/firmware_v1.0/src/button.h
+++ Friend/firmware/firmware_v1.0/src/button.h
@@ -1,8 +1,16 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
+// Button states
+#define DEFAULT_STATE 0
+#define SINGLE_TAP 1    // Quick press and release
+#define DOUBLE_TAP 2    // Two quick presses - Currently used for voice interaction
+#define LONG_TAP 3      // Long press - Currently used for sleep/wake
+#define BUTTON_PRESS 4  // Button down event
+#define BUTTON_RELEASE 5 // Button up event
+
 typedef enum {
-    IDLE, 
+    IDLE,
     ONE_PRESS,
     TWO_PRESS,
     GRACE
@@ -11,11 +19,6 @@ typedef enum {
 int button_init();
 void activate_button_work();
 void register_button_service();
-
 FSM_STATE_T get_current_button_state();
-// Voice interaction declarations
-extern void start_voice_interaction(void);
-extern void stop_voice_interaction(void);
-extern bool voice_interaction_active;
 
-#endif
+#endif#endif
