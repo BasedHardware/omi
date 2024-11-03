5858e75de61deeee4b66915092f6cd56408debff
Vincent Koc <vincentkoc@ieee.org>
2024-11-04 00:33:36 +1100

    Update transport.h

--- Friend/firmware/firmware_v1.0/src/transport.h
+++ Friend/firmware/firmware_v1.0/src/transport.h
@@ -1,33 +1,17 @@
 #ifndef TRANSPORT_H
 #define TRANSPORT_H
 
-#include <zephyr/types.h> // For uint8_t, size_t
-#include <zephyr/drivers/sensor.h>
-typedef struct sensors {
+#include <zephyr/types.h>
 
-	struct sensor_value a_x;
-	struct sensor_value a_y;
-	struct sensor_value a_z;
-    struct sensor_value g_x;
-    struct sensor_value g_y;
-    struct sensor_value g_z;
-
-};
-/**
- * @brief Initialize the BLE transport logic
- *
- * Initializes the BLE Logic
- *
- * @return 0 if successful, negative errno code if error
- */
-int transport_start();
-struct bt_conn *get_current_connection();
-int broadcast_audio_packets(uint8_t *buffer, size_t size);
-
-// Voice interaction function prototypes and variable
+// Voice interaction functions
 void start_voice_interaction(void);
 void stop_voice_interaction(void);
 extern bool voice_interaction_active;
 
-int bt_on();
+// Other functions
+int transport_start(void);
+struct bt_conn *get_current_connection(void);
+int broadcast_audio_packets(uint8_t *buffer, size_t size);
+int bt_on(void);
+
 #endif
