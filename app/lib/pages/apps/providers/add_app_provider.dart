3ebf1e80de8aef5415c8c7159f61785166f611f9
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-11-03 22:46:21 +0530

    submitting app logic

--- app/lib/pages/apps/providers/add_app_provider.dart
+++ app/lib/pages/apps/providers/add_app_provider.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:friend_private/backend/http/api/apps.dart';
 import 'package:friend_private/backend/preferences.dart';
 import 'package:friend_private/utils/alerts/app_snackbar.dart';
 import 'package:image_picker/image_picker.dart';
@@ -31,10 +33,17 @@ class AddAppProvider extends ChangeNotifier {
   File? imageFile;
   List<String> capabilities = [];
 
-  void init() {
-    getCategories();
+  bool isLoading = false;
+
+  Future init() async {
+    await getCategories();
     creatorNameController.text = SharedPreferencesUtil().givenName;
     creatorEmailController.text = SharedPreferencesUtil().email;
+  }
+
+  void setIsLoading(bool loading) {
+    isLoading = loading;
+    notifyListeners();
   }
 
   void clear() {
@@ -46,38 +55,124 @@ class AddAppProvider extends ChangeNotifier {
     memoryPromptController.clear();
   }
 
-  void getCategories() {
-    // TODO: Fetch categories from the backend
-    categories = [
-      'Conversation Analysis',
-      'Personality Emulation',
-      'Health and Wellness',
-      'Education and Learning',
-      'Emotional and Mental Support',
-    ];
+  Future<void> getCategories() async {
+    categories = await getAppCategories();
     notifyListeners();
   }
 
-  void submitApp() {
+  bool validateForm() {
     if (formKey.currentState!.validate()) {
       if (!termsAgreed) {
         AppSnackbar.showSnackbarError('Please agree to the terms and conditions to proceed');
-        return;
+        return false;
       }
       if (!capabilitySelected()) {
         AppSnackbar.showSnackbarError('Please select at least one capability for your app');
-        return;
+        return false;
       }
+      if (imageFile == null) {
+        AppSnackbar.showSnackbarError('Please select a logo for your app');
+        return false;
+      }
+      if (isCapabilitySelected('external_integration')) {
+        if (triggerEvent == null) {
+          AppSnackbar.showSnackbarError('Please select a trigger event for your app');
+          return false;
+        }
+        if (webhookUrlController.text.isEmpty) {
+          AppSnackbar.showSnackbarError('Please enter a webhook URL for your app');
+          return false;
+        }
+        if (setupCompletedController.text.isEmpty) {
+          AppSnackbar.showSnackbarError('Please enter a setup completed URL for your app');
+          return false;
+        }
+        if (instructionsController.text.isEmpty) {
+          AppSnackbar.showSnackbarError('Please enter a setup instructions file path for your app');
+          return false;
+        }
+      }
+      if (isCapabilitySelected('chat')) {
+        if (chatPromptController.text.isEmpty) {
+          AppSnackbar.showSnackbarError('Please enter a chat prompt for your app');
+          return false;
+        }
+      }
+      if (isCapabilitySelected('memories')) {
+        if (memoryPromptController.text.isEmpty) {
+          AppSnackbar.showSnackbarError('Please enter a memory prompt for your app');
+          return false;
+        }
+      }
+      if (appCategory == null) {
+        AppSnackbar.showSnackbarError('Please select a category for your app');
+        return false;
+      }
+      if (privacyLevel == null) {
+        AppSnackbar.showSnackbarError('Please select a privacy level for your app');
+        return false;
+      }
+      return true;
     } else {
       AppSnackbar.showSnackbarError('Please fill in all the required fields correctly');
+      return false;
     }
+  }
+
+  Future<void> submitApp() async {
+    setIsLoading(true);
+    Map<String, dynamic> data = {
+      'name': appNameController.text,
+      'description': appDescriptionController.text,
+      'author': creatorNameController.text,
+      'email': creatorEmailController.text,
+      'capabilities': capabilities,
+      'deleted': false,
+      'uid': SharedPreferencesUtil().uid,
+      'category': appCategory,
+    };
+    if (isCapabilitySelected('external_integration')) {
+      data['external_integration'] = {
+        'triggers_on': triggerEvent,
+        'webhook_url': webhookUrlController.text,
+        'setup_completed_url': setupCompletedController.text,
+        'setup_instructions_file_path': instructionsController.text,
+      };
+    }
+    if (isCapabilitySelected('chat')) {
+      data['chat_prompt'] = chatPromptController.text;
+    }
+    if (isCapabilitySelected('memories')) {
+      data['memory_prompt'] = memoryPromptController.text;
+    }
+    if (appCategory != null) {}
+    if (privacyLevel == 'public') {
+      data['private'] = false;
+    } else {
+      data['private'] = true;
+    }
+    var res = await submitAppServer(imageFile!, data);
+    if (res) {
+      AppSnackbar.showSnackbarSuccess('App submitted successfully 🚀');
+      clear();
+    } else {
+      AppSnackbar.showSnackbarError('Failed to submit app. Please try again later');
+    }
+    setIsLoading(false);
   }
 
   Future pickImage() async {
     ImagePicker imagePicker = ImagePicker();
-    var file = await imagePicker.pickImage(source: ImageSource.gallery);
-    if (file != null) {
-      imageFile = File(file.path);
+    try {
+      var file = await imagePicker.pickImage(source: ImageSource.gallery);
+      if (file != null) {
+        imageFile = File(file.path);
+      }
+      notifyListeners();
+    } on PlatformException catch (e) {
+      if (e.code == 'photo_access_denied') {
+        AppSnackbar.showSnackbarError('Photos permission denied. Please allow access to photos to select an image');
+      }
     }
     notifyListeners();
   }
