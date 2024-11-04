1f3411409b214d5640e94c6a75004424c5b405f8
Vincent Koc <vincentkoc@ieee.org>
2024-11-05 09:48:06 +1100

    Update transport.h

--- Friend/firmware/firmware_v1.0/src/transport.h
+++ Friend/firmware/firmware_v1.0/src/transport.h
@@ -1,14 +1,19 @@
 #ifndef TRANSPORT_H
 #define TRANSPORT_H
 
-#include <zephyr/types.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
 
 // Voice interaction functions
+extern bool voice_interaction_active;
 void start_voice_interaction(void);
 void stop_voice_interaction(void);
-extern bool voice_interaction_active;
 
-// Other functions
+// Declare handle_voice_data as non-static
+int handle_voice_data(uint8_t *data, size_t len);
+
+// Existing declarations...
 int transport_start(void);
 struct bt_conn *get_current_connection(void);
 int broadcast_audio_packets(uint8_t *buffer, size_t size);
