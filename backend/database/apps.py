cd0e5b69a867a67c78298140e8e21fb108e39331
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-11-12 20:39:52 +0530

    use new doc structure

--- backend/database/apps.py
+++ backend/database/apps.py
@@ -1,13 +1,6 @@
 import os
-import random
-from datetime import datetime, timezone
 from typing import List
 
-import requests
-
-from models.app import App
-from models.plugin import UsageHistoryType
-from utils.other.storage import storage_client
 from ._client import db
 
 # *****************************
@@ -17,19 +10,18 @@ from ._client import db
 omi_plugins_bucket = os.getenv('BUCKET_PLUGINS_LOGOS')
 
 
-def get_app_by_id_db(app_id: str, uid: str):
-    if 'private' in app_id:
-        app_ref = db.collection('users').document(uid).collection('plugins').document(app_id)
-    else:
-        app_ref = db.collection('plugins_data').document(app_id)
+def get_app_by_id_db(app_id: str):
+    app_ref = db.collection('plugins_data').document(app_id)
     doc = app_ref.get()
     if doc.exists:
+        if doc.to_dict().get('deleted', True):
+            return None
         return doc.to_dict()
     return None
 
 
 def get_private_apps_db(uid: str) -> List:
-    private_plugins = db.collection('users').document(uid).collection('plugins').stream()
+    private_plugins = db.collection('plugins_data').where('uid', '==', uid).where('private', '==', True).stream()
     data = [doc.to_dict() for doc in private_plugins]
     return data
 
@@ -44,43 +36,62 @@ def get_public_apps_db(uid: str) -> List:
     data = [doc.to_dict() for doc in public_plugins]
 
     # Include the doc if it is not approved but uid matches
-    unapproved = db.collection('plugins_data').where('approved', '==', False).where('uid', '==', uid).stream()
+    unapproved = db.collection('plugins_data').where('approved', '==', False).where('uid', '==', uid).where('private', '==', False).stream()
     data.extend([doc.to_dict() for doc in unapproved])
 
     return data
 
 
 def get_public_approved_apps_db() -> List:
-    public_plugins = db.collection('plugins_data').where('approved', '==', True).stream()
+    public_plugins = db.collection('plugins_data').where('approved', '==', True).where('deleted', '==', False).stream()
     return [doc.to_dict() for doc in public_plugins]
 
 
 def get_public_unapproved_apps_db(uid: str) -> List:
-    public_plugins = db.collection('plugins_data').where('approved', '==', False).where('uid', '==', uid).stream()
+    public_plugins = db.collection('plugins_data').where('approved', '==', False).where('uid', '==', uid).where('deleted','==', False).stream()
     return [doc.to_dict() for doc in public_plugins]
 
 
-def public_app_id_exists_db(app_id: str) -> bool:
-    app_ref = db.collection('plugins_data').document(app_id)
-    return app_ref.get().exists
+# def public_app_id_exists_db(app_id: str) -> bool:
+#     app_ref = db.collection('plugins_data').document(app_id)
+#     return app_ref.get().exists
+#
+#
+# def private_app_id_exists_db(app_id: str, uid: str) -> bool:
+#     app_ref = db.collection('users').document(uid).collection('plugins').document(app_id)
+#     return app_ref.get().exists
 
 
-def private_app_id_exists_db(app_id: str, uid: str) -> bool:
-    app_ref = db.collection('users').document(uid).collection('plugins').document(app_id)
-    return app_ref.get().exists
+# def get_incremented_public_app_id(app_id: str):
+#     res = db.collection('plugins_data').count().get()
+#     apps_count = res[0][0].value
+#     return f'{app_id}-{apps_count}'
+#
+#
+# def get_incremented_private_app_id(app_id: str, uid: str):
+#     res = db.collection('users').document(uid).collection('plugins').count().get()
+#     apps_count = res[0][0].value
+#     app_id = app_id.split('-private')[0]
+#     return f'{app_id}-{apps_count}-private'
 
 
-def get_incremented_public_app_id(app_id: str):
-    res = db.collection('plugins_data').count().get()
-    apps_count = res[0][0].value
-    return f'{app_id}-{apps_count}'
+def add_app_to_db(app_data: dict):
+    plugin_ref = db.collection('plugins_data')
+    plugin_ref.add(app_data, app_data['id'])
 
 
-def get_incremented_private_app_id(app_id: str, uid: str):
-    res = db.collection('users').document(uid).collection('plugins').count().get()
-    apps_count = res[0][0].value
-    app_id = app_id.split('-private')[0]
-    return f'{app_id}-{apps_count}-private'
+def update_app_in_db(app_data: dict):
+    plugin_ref = db.collection('plugins_data').document(app_data['id'])
+    plugin_ref.update(app_data)
+
+
+def delete_app_from_db(app_id: str):
+    plugin_ref = db.collection('plugins_data').document(app_id)
+    plugin_ref.update({'deleted': True})
+
+def update_app_visibility_in_db(app_id: str, private: bool):
+    plugin_ref = db.collection('plugins_data').document(app_id)
+    plugin_ref.update({'private': private})
 
 
 def add_public_app(plugin_data: dict):
@@ -118,28 +129,28 @@ def change_app_approval_status(plugin_id: str, approved: bool):
     plugin_ref.update({'approved': approved, 'status': 'approved' if approved else 'rejected'})
 
 
-def change_app_visibility_db(app_id: str, private: bool, was_public: bool, uid: str):
-    if was_public and private:  # public -> private
-        plugin_ref = db.collection('plugins_data').document(app_id)
-        plugin = plugin_ref.get().to_dict()
-        plugin_ref.delete()
-        new_plugin_id = f'{app_id}-private'
-        if private_app_id_exists_db(new_plugin_id, uid):
-            new_plugin_id = get_incremented_private_app_id(app_id, uid)
-        plugin['id'] = new_plugin_id
-        plugin['private'] = private
-        plugin_ref = db.collection('users').document(uid).collection('plugins').document(new_plugin_id)
-        plugin_ref.set(plugin)
-    elif not was_public and not private:  # private -> public
-        plugin_ref = db.collection('users').document(uid).collection('plugins').document(app_id)
-        plugin = plugin_ref.get().to_dict()
-        plugin_ref.delete()
-        new_plugin_id = app_id.split('-private')[0]
-        if public_app_id_exists_db(new_plugin_id):
-            new_plugin_id = get_incremented_public_app_id(app_id)
-        plugin['id'] = new_plugin_id
-        plugin['private'] = private
-        if public_app_id_exists_db(new_plugin_id):
-            new_plugin_id = new_plugin_id + '-' + ''.join([str(random.randint(0, 9)) for _ in range(5)])
-        plugin_ref = db.collection('plugins_data').document(new_plugin_id)
-        plugin_ref.set(plugin)
+# def change_app_visibility_db(app_id: str, private: bool, was_public: bool, uid: str):
+#     if was_public and private:  # public -> private
+#         plugin_ref = db.collection('plugins_data').document(app_id)
+#         plugin = plugin_ref.get().to_dict()
+#         plugin_ref.delete()
+#         new_plugin_id = f'{app_id}-private'
+#         if private_app_id_exists_db(new_plugin_id, uid):
+#             new_plugin_id = get_incremented_private_app_id(app_id, uid)
+#         plugin['id'] = new_plugin_id
+#         plugin['private'] = private
+#         plugin_ref = db.collection('users').document(uid).collection('plugins').document(new_plugin_id)
+#         plugin_ref.set(plugin)
+#     elif not was_public and not private:  # private -> public
+#         plugin_ref = db.collection('users').document(uid).collection('plugins').document(app_id)
+#         plugin = plugin_ref.get().to_dict()
+#         plugin_ref.delete()
+#         new_plugin_id = app_id.split('-private')[0]
+#         if public_app_id_exists_db(new_plugin_id):
+#             new_plugin_id = get_incremented_public_app_id(app_id)
+#         plugin['id'] = new_plugin_id
+#         plugin['private'] = private
+#         if public_app_id_exists_db(new_plugin_id):
+#             new_plugin_id = new_plugin_id + '-' + ''.join([str(random.randint(0, 9)) for _ in range(5)])
+#         plugin_ref = db.collection('plugins_data').document(new_plugin_id)
+#         plugin_ref.set(plugin)
