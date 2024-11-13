4780059d789e424c8d5d54c03b32017b67f25e2c
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-11-13 11:50:50 -0800

    /v1/users/analytics/chat_message

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -216,3 +216,12 @@ def get_memory_summary_rating(
     if not rating:
         return {'has_rating': False}
     return {'has_rating': rating.get('value', -1) != -1, 'rating': rating.get('value', -1)}
+
+
+@router.get('/v1/users/analytics/chat_message', tags=['v1'])
+def get_chat_message_analytics(
+        message_id: str,
+        value: int,
+        uid: str = Depends(auth.get_current_user_uid),
+):
+    return set_chat_message_rating_score(uid, message_id, value)
