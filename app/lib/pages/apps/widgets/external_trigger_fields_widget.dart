bfccda6c5309edb49ba215c6aaac07f0a0011e97
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-11-15 12:53:02 +0530

    add option auth field

--- app/lib/pages/apps/widgets/external_trigger_fields_widget.dart
+++ app/lib/pages/apps/widgets/external_trigger_fields_widget.dart
@@ -69,6 +69,50 @@ class ExternalTriggerFieldsWidget extends StatelessWidget {
             controller: provider.webhookUrlController,
             validator: (value) {
               if (value == null || !isValidUrl(value)) {
+                return 'Please enter a valid Auth URL';
+              }
+              return null;
+            },
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              focusedBorder: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                borderSide: BorderSide(
+                  color: Colors.white,
+                ),
+              ),
+              label: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.link,
+                      color: WidgetStateColor.resolveWith(
+                          (states) => states.contains(WidgetState.focused) ? Colors.white : Colors.grey)),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  const Text(
+                    'Auth URL (if required)',
+                  ),
+                ],
+              ),
+              hintText: 'https://your-auth-url.com/',
+              labelStyle: const TextStyle(
+                color: Colors.grey,
+              ),
+              floatingLabelStyle: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+            controller: provider.authUrlController,
+            validator: (value) {
+              if (value == null || !isValidUrl(value)) {
                 return 'Please enter a valid webhook URL';
               }
               return null;
