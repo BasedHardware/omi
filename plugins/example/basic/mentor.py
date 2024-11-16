6d97be72e3a588f24bf6a4014c814e3ce82e97da
thịnh <ngocthinhdp@gmail.com>
2024-11-16 11:56:36 +0700

    Simplify context, add example to Mentor01 app

--- plugins/example/basic/mentor.py
+++ plugins/example/basic/mentor.py
@@ -43,6 +43,7 @@ def mentoring(data: RealtimePluginRequest):
 
     user_name = "{{user_name}}"
     user_facts = "{{user_facts}}"
+    user_context = "{{user_context}}"
 
     prompt = f"""
     You are an experienced mentor, that helps people achieve their goals during the meeting.
@@ -67,15 +68,27 @@ def mentoring(data: RealtimePluginRequest):
     Output your response in plain text, without markdown.
 
     If you cannot find the topic or problem of the meeting, respond 'Nah 🤷 ~'.
+
+    Conversation:
     ```
     ${transcript}
     ```
+
+    Context:
+    ```
+    {user_context}
+    ```
     """.replace('    ', '').strip()
 
-    # 3. Respond with the format {notification: {prompt, params}}
-    return {'session_id': data.session_id,
-            'notification': {'prompt': prompt,
-                       'params': ['user_name', 'user_facts']}}
+    # 3. Respond with the format {notification: {prompt, params, context}}
+    #   - context: {question, filters: {people, topics, entities, dates}} | None
+    return {
+        'session_id': data.session_id,
+        'notification': {
+            'prompt': prompt,
+            'params': ['user_name', 'user_facts', 'user_context'],
+        }
+    }
 
 @ router.get('/setup/mentor', tags=['mentor'])
 def is_setup_completed(uid: str):
