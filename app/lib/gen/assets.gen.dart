3cfb5d818100a54972894e0261f6e22fb25c708b
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-11-20 15:46:22 +0530

    setup asset_gen and add new icons

---
+++ app/lib/gen/assets.gen.dart
@@ -0,0 +1,308 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsDeviceAssetsGen {
+  const $AssetsDeviceAssetsGen();
+
+  /// File path: assets/device_assets/frame_lib.lua
+  String get frameLib => 'assets/device_assets/frame_lib.lua';
+
+  /// List of all assets
+  List<String> get values => [frameLib];
+}
+
+class $AssetsFontsGen {
+  const $AssetsFontsGen();
+
+  /// File path: assets/fonts/SFPRODISPLAYBLACKITALIC.OTF
+  String get sfprodisplayblackitalic =>
+      'assets/fonts/SFPRODISPLAYBLACKITALIC.OTF';
+
+  /// File path: assets/fonts/SFPRODISPLAYBOLD.OTF
+  String get sfprodisplaybold => 'assets/fonts/SFPRODISPLAYBOLD.OTF';
+
+  /// File path: assets/fonts/SFPRODISPLAYHEAVYITALIC.OTF
+  String get sfprodisplayheavyitalic =>
+      'assets/fonts/SFPRODISPLAYHEAVYITALIC.OTF';
+
+  /// File path: assets/fonts/SFPRODISPLAYLIGHTITALIC.OTF
+  String get sfprodisplaylightitalic =>
+      'assets/fonts/SFPRODISPLAYLIGHTITALIC.OTF';
+
+  /// File path: assets/fonts/SFPRODISPLAYMEDIUM.OTF
+  String get sfprodisplaymedium => 'assets/fonts/SFPRODISPLAYMEDIUM.OTF';
+
+  /// File path: assets/fonts/SFPRODISPLAYREGULAR.OTF
+  String get sfprodisplayregular => 'assets/fonts/SFPRODISPLAYREGULAR.OTF';
+
+  /// File path: assets/fonts/SFPRODISPLAYSEMIBOLDITALIC.OTF
+  String get sfprodisplaysemibolditalic =>
+      'assets/fonts/SFPRODISPLAYSEMIBOLDITALIC.OTF';
+
+  /// File path: assets/fonts/SFPRODISPLAYTHINITALIC.OTF
+  String get sfprodisplaythinitalic =>
+      'assets/fonts/SFPRODISPLAYTHINITALIC.OTF';
+
+  /// List of all assets
+  List<String> get values => [
+        sfprodisplayblackitalic,
+        sfprodisplaybold,
+        sfprodisplayheavyitalic,
+        sfprodisplaylightitalic,
+        sfprodisplaymedium,
+        sfprodisplayregular,
+        sfprodisplaysemibolditalic,
+        sfprodisplaythinitalic
+      ];
+}
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/ic_apps_selected.svg
+  String get icAppsSelected => 'assets/icons/ic_apps_selected.svg';
+
+  /// File path: assets/icons/ic_apps_unselected.svg
+  String get icAppsUnselected => 'assets/icons/ic_apps_unselected.svg';
+
+  /// File path: assets/icons/ic_chat_selected.svg
+  String get icChatSelected => 'assets/icons/ic_chat_selected.svg';
+
+  /// File path: assets/icons/ic_chat_unselected.svg
+  String get icChatUnselected => 'assets/icons/ic_chat_unselected.svg';
+
+  /// File path: assets/icons/ic_home_selected.svg
+  String get icHomeSelected => 'assets/icons/ic_home_selected.svg';
+
+  /// File path: assets/icons/ic_home_unselected.svg
+  String get icHomeUnselected => 'assets/icons/ic_home_unselected.svg';
+
+  /// File path: assets/icons/ic_settings_selected.svg
+  String get icSettingsSelected => 'assets/icons/ic_settings_selected.svg';
+
+  /// File path: assets/icons/ic_settings_unselected.svg
+  String get icSettingsUnselected => 'assets/icons/ic_settings_unselected.svg';
+
+  /// List of all assets
+  List<String> get values => [
+        icAppsSelected,
+        icAppsUnselected,
+        icChatSelected,
+        icChatUnselected,
+        icHomeSelected,
+        icHomeUnselected,
+        icSettingsSelected,
+        icSettingsUnselected
+      ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/app_launcher_icon.png
+  AssetGenImage get appLauncherIcon =>
+      const AssetGenImage('assets/images/app_launcher_icon.png');
+
+  /// File path: assets/images/backbutton.svg
+  String get backbutton => 'assets/images/backbutton.svg';
+
+  /// File path: assets/images/background.png
+  AssetGenImage get background =>
+      const AssetGenImage('assets/images/background.png');
+
+  /// File path: assets/images/blob.png
+  AssetGenImage get blob => const AssetGenImage('assets/images/blob.png');
+
+  /// File path: assets/images/emotional_feedback_1.png
+  AssetGenImage get emotionalFeedback1 =>
+      const AssetGenImage('assets/images/emotional_feedback_1.png');
+
+  /// File path: assets/images/favicon.png
+  AssetGenImage get favicon => const AssetGenImage('assets/images/favicon.png');
+
+  /// File path: assets/images/herologo.png
+  AssetGenImage get herologo =>
+      const AssetGenImage('assets/images/herologo.png');
+
+  /// File path: assets/images/instruction_1.png
+  AssetGenImage get instruction1 =>
+      const AssetGenImage('assets/images/instruction_1.png');
+
+  /// File path: assets/images/instruction_2.png
+  AssetGenImage get instruction2 =>
+      const AssetGenImage('assets/images/instruction_2.png');
+
+  /// File path: assets/images/instruction_3.png
+  AssetGenImage get instruction3 =>
+      const AssetGenImage('assets/images/instruction_3.png');
+
+  /// File path: assets/images/logo_transparent.png
+  AssetGenImage get logoTransparent =>
+      const AssetGenImage('assets/images/logo_transparent.png');
+
+  /// File path: assets/images/recording_green_circle_icon.png
+  AssetGenImage get recordingGreenCircleIcon =>
+      const AssetGenImage('assets/images/recording_green_circle_icon.png');
+
+  /// File path: assets/images/splash.png
+  AssetGenImage get splash => const AssetGenImage('assets/images/splash.png');
+
+  /// File path: assets/images/splash_icon.png
+  AssetGenImage get splashIcon =>
+      const AssetGenImage('assets/images/splash_icon.png');
+
+  /// File path: assets/images/stars.png
+  AssetGenImage get stars => const AssetGenImage('assets/images/stars.png');
+
+  /// List of all assets
+  List<dynamic> get values => [
+        appLauncherIcon,
+        backbutton,
+        background,
+        blob,
+        emotionalFeedback1,
+        favicon,
+        herologo,
+        instruction1,
+        instruction2,
+        instruction3,
+        logoTransparent,
+        recordingGreenCircleIcon,
+        splash,
+        splashIcon,
+        stars
+      ];
+}
+
+class $AssetsLottieAnimationsGen {
+  const $AssetsLottieAnimationsGen();
+
+  /// File path: assets/lottie_animations/no_internet.json
+  String get noInternet => 'assets/lottie_animations/no_internet.json';
+
+  /// File path: assets/lottie_animations/server_error.json
+  String get serverError => 'assets/lottie_animations/server_error.json';
+
+  /// File path: assets/lottie_animations/wave.json
+  String get wave => 'assets/lottie_animations/wave.json';
+
+  /// List of all assets
+  List<String> get values => [noInternet, serverError, wave];
+}
+
+class $AssetsPdfsGen {
+  const $AssetsPdfsGen();
+
+  /// File path: assets/pdfs/favicon.png
+  AssetGenImage get favicon => const AssetGenImage('assets/pdfs/favicon.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [favicon];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsDeviceAssetsGen deviceAssets = $AssetsDeviceAssetsGen();
+  static const $AssetsFontsGen fonts = $AssetsFontsGen();
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+  static const $AssetsLottieAnimationsGen lottieAnimations =
+      $AssetsLottieAnimationsGen();
+  static const $AssetsPdfsGen pdfs = $AssetsPdfsGen();
+  static const String sileroVad = 'assets/silero_vad.onnx';
+  static const String sileroVadV5 = 'assets/silero_vad.v5.onnx';
+  static const String shorebird = 'shorebird.yaml';
+
+  /// List of all assets
+  static List<String> get values => [sileroVad, sileroVadV5, shorebird];
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
