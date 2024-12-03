025b40ffe719f56fb649b65472bb56a549d03dae
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-12-03 11:39:14 +0530

    typo

--- backend/routers/apps.py
+++ backend/routers/apps.py
@@ -7,8 +7,8 @@ from ulid import ULID
 from fastapi import APIRouter, Depends, Form, UploadFile, File, HTTPException, Header
 
 from database.apps import change_app_approval_status, get_unapproved_public_apps_db, \
-    add_app_to_db, update_app_in_db, delete_app_from_db, update_app_visibility_in_db, delete_personas_db, \
-    get_personas_by_username_db, get_persona_by_id_db
+    add_app_to_db, update_app_in_db, delete_app_from_db, update_app_visibility_in_db, \
+    get_personas_by_username_db, get_persona_by_id_db, delete_persona_db
 from database.notifications import get_token_only
 from database.redis_db import delete_generic_cache, get_specific_user_review, increase_app_installs_count, \
     decrease_app_installs_count, enable_app, disable_app, delete_app_cache_by_id
@@ -384,7 +384,7 @@ def delete_persona(persona_id: str, secret_key: str = Header(...)):
     personas = get_persona_by_id_db(persona_id)
     if not personas:
         raise HTTPException(status_code=404, detail='Persona not found')
-    delete_personas_db(persona_id)
+    delete_persona_db(persona_id)
     return {'status': 'ok'}
 
 
