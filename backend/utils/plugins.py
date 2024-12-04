9deb303916129d983fd1ef739ee14c8ed279b437
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-12-04 13:59:34 +0530

    replace plugin with app

--- backend/utils/plugins.py
+++ backend/utils/plugins.py
@@ -354,8 +354,8 @@ def _trigger_realtime_integrations(uid: str, token: str, segments: List[dict], m
                       response.text[:100])
                 return
 
-            if plugin.uid is None or plugin.uid != uid:
-                record_plugin_usage(uid, plugin.id, UsageHistoryType.transcript_processed_external_integration, memory_id=memory_id)
+            if app.uid is None or app.uid != uid:
+                record_app_usage(uid, app.id, UsageHistoryType.transcript_processed_external_integration, memory_id=memory_id)
 
             response_data = response.json()
             if not response_data:
