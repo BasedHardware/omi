1a40c5ed77145dbd8c0cd828a057ada47e7ec3a0
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-12-05 21:33:02 +0530

    add example values forExtractedInformation

--- backend/utils/llm.py
+++ backend/utils/llm.py
@@ -662,18 +662,22 @@ def followup_question_prompt(segments: List[TranscriptSegment]):
 class ExtractedInformation(BaseModel):
     people: List[str] = Field(
         default=[],
+        examples=['John Doe', 'Jane Doe'],
         description='Identify all the people names who were mentioned during the conversation.'
     )
     topics: List[str] = Field(
         default=[],
+        examples=['Artificial Intelligence', 'Machine Learning'],
         description='List all the main topics and subtopics that were discussed.',
     )
     entities: List[str] = Field(
         default=[],
+        examples=['OpenAI', 'GPT-4'],
         description='List any products, technologies, places, or other entities that are relevant to the conversation.'
     )
     dates: List[str] = Field(
         default=[],
+        examples=['2024-01-01', '2024-01-02'],
         description=f'Extract any dates mentioned in the conversation. Use the format YYYY-MM-DD.'
     )
 
