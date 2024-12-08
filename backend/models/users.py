166b93b408449983d280c99f3a27052037320723
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-12-08 22:31:26 +0530

    update creator details model

--- backend/models/users.py
+++ backend/models/users.py
@@ -3,6 +3,7 @@ from enum import Enum
 from pydantic import BaseModel
 from typing import Optional
 
+
 class WebhookType(str, Enum):
     audio_bytes = 'audio_bytes'
     audio_bytes_websocket = 'audio_bytes_websocket'
@@ -11,8 +12,12 @@ class WebhookType(str, Enum):
     day_summary = 'day_summary'
 
 
+class PayPalDetails(BaseModel):
+    paypal_email: str
+    paypal_me_link: Optional[str] = None
+
+
 class CreatorProfileRequest(BaseModel):
     creator_name: str
     creator_email: str
-    paypal_email: str
-    paypal_me_link: Optional[str]
+    paypal_details: PayPalDetails
