6b96298c80d766a54eb8c93c7f47d0aed4cd099c
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-12-15 16:38:54 +0530

    rename to next.config.js

---
+++ personas-open-source/next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'standalone',
+  // Add other config options here
+};
+
+module.exports = nextConfig;
