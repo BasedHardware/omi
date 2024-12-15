cdeba6d13ef7f702069ebd3ff1fe6b617f988c01
Thai Nguyen <thainguyensunya@gmail.com>
2024-12-15 20:19:57 +0700

    Fix VAD issue

--- backend/modal/vad_modal.py
+++ backend/modal/vad_modal.py
@@ -3,7 +3,6 @@ import uuid
 
 import torch
 from fastapi import UploadFile
-from modal import App, web_endpoint, Secret, Image
 from pyannote.audio import Pipeline
 
 # Instantiate pretrained voice activity detection pipeline
@@ -13,26 +12,18 @@ vad = Pipeline.from_pretrained(
     use_auth_token=os.getenv('HUGGINGFACE_TOKEN')
 ).to(device)
 
-app = App(name='vad')
-image = (
-    Image.debian_slim()
-    .pip_install("pyannote.audio")
-    .pip_install("torch")
-    .pip_install("torchaudio")
-)
+# app = App(name='vad')
+# image = (
+#     Image.debian_slim()
+#     .pip_install("pyannote.audio")
+#     .pip_install("torch")
+#     .pip_install("torchaudio")
+# )
 
 os.makedirs('_temp', exist_ok=True)
 
 
-# @app.function(
-#     image=image,
-#     keep_warm=1,
-#     memory=(1024, 2048),
-#     cpu=4,
-#     secrets=[Secret.from_name('huggingface-token')],
-# )
-# @web_endpoint(method='POST')
-def endpoint(file: UploadFile):
+def vad_endpoint(file: UploadFile):
     upload_id = str(uuid.uuid4())
     file_path = f"_temp/{upload_id}_{file.filename}"
     with open(file_path, 'wb') as f:
