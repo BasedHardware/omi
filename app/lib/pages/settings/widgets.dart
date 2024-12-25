21e11b02e8c2d9fa924d727f3b8187312e311016
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-12-25 19:44:52 +0530

    remove unused widgets

--- app/lib/pages/settings/widgets.dart
+++ app/lib/pages/settings/widgets.dart
@@ -1,52 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:friend_private/utils/analytics/mixpanel.dart';
 
-getItemAddonWrapper(List<Widget> widgets) {
-  return Card(
-    shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8))),
-    child: Column(
-      children: widgets,
-    ),
-  );
-}
-
-getItemAddOn(String title, VoidCallback onTap, {required IconData icon, bool visibility = true}) {
-  return Visibility(
-    visible: visibility,
-    child: GestureDetector(
-      onTap: onTap,
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 12, 8, 0),
-        child: Container(
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 29, 29, 29), // Replace with your desired color
-            borderRadius: BorderRadius.circular(10.0), // Adjust for desired rounded corners
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      title,
-                      style: const TextStyle(color: Color.fromARGB(255, 150, 150, 150), fontSize: 16),
-                    ),
-                    const SizedBox(width: 16),
-                    Icon(icon, color: Colors.white, size: 16),
-                  ],
-                ),
-                const Icon(Icons.arrow_forward_ios, color: Colors.white, size: 16),
-              ],
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
-}
-
 class CustomListTile extends StatelessWidget {
   final String title;
   final VoidCallback onTap;
