7555a466d670e9cffa42e5a72f4b9efa4cb6c945
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-12-29 14:16:35 +0530

    minor fix

--- app/lib/pages/home/page.dart
+++ app/lib/pages/home/page.dart
@@ -439,18 +439,23 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver, Ticker
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 const BatteryInfoWidget(),
-                context.read<ConversationProvider>().missingWalsInSeconds >= 120 &&
-                        context.read<HomeProvider>().selectedIndex == 0
-                    ? GestureDetector(
-                        onTap: () {
-                          routeToPage(context, const SyncPage());
-                        },
-                        child: Container(
-                          padding: const EdgeInsets.only(left: 12),
-                          child: const Icon(Icons.download, color: Colors.white, size: 24),
-                        ),
-                      )
-                    : const SizedBox.shrink(),
+                Consumer<HomeProvider>(builder: (context, provider, child) {
+                  if (provider.selectedIndex == 0) {
+                    return (context.read<ConversationProvider>().missingWalsInSeconds >= 120
+                        ? GestureDetector(
+                            onTap: () {
+                              routeToPage(context, const SyncPage());
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.only(left: 12),
+                              child: const Icon(Icons.download, color: Colors.white, size: 24),
+                            ),
+                          )
+                        : const SizedBox.shrink());
+                  } else {
+                    return const SizedBox.shrink();
+                  }
+                }),
                 Consumer<HomeProvider>(
                   builder: (context, provider, child) {
                     if (provider.selectedIndex == 1) {
@@ -458,10 +463,7 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver, Ticker
                         controller: _controller,
                       );
                     } else if (provider.selectedIndex == 2) {
-                      return Padding(
-                        padding: EdgeInsets.only(left: MediaQuery.sizeOf(context).width * 0.06),
-                        child: const Text('Apps', style: TextStyle(color: Colors.white, fontSize: 18)),
-                      );
+                      return const Text('Apps', style: TextStyle(color: Colors.white, fontSize: 18));
                     } else {
                       return Flexible(
                         child: Row(
