dbbef173b4bf9328d8dba9a899cac7f2d11b98e8
Fred Turkington <z3ugma@users.noreply.github.com>
2025-01-03 12:07:40 -0600

    Remove unused firmware v1.5 folder

--- Friend/firmware/firmware_v1.5/src/battery.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define CHARGE_NOT_CHARGING 0
-#define CHARGE_CHARGING 1
-
-bool is_battery_charging(void);
-int get_battery_voltage(void);
-int get_battery_percentage(void);
-int battery_start(void);
