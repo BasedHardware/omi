3bb03f80a7f11496f11ced7db26520115a0be7d5
thịnh <ngocthinhdp@gmail.com>
2025-01-03 15:41:42 +0700

    Update splash icon on anroid, brand name

--- app/lib/providers/device_provider.dart
+++ app/lib/providers/device_provider.dart
@@ -222,12 +222,12 @@ class DeviceProvider extends ChangeNotifier implements IDeviceServiceSubsciption
 
     print('after resetState inside initiateConnectionListener');
 
-    InstabugLog.logInfo('Friend Device Disconnected');
+    InstabugLog.logInfo('Omi Device Disconnected');
     _disconnectNotificationTimer?.cancel();
     _disconnectNotificationTimer = Timer(const Duration(seconds: 30), () {
       NotificationService.instance.createNotification(
-        title: 'Your Friend Device Disconnected',
-        body: 'Please reconnect to continue using your Friend.',
+        title: 'Your Omi Device Disconnected',
+        body: 'Please reconnect to continue using your Omi.',
       );
     });
     MixpanelManager().deviceDisconnected();
