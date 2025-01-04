2bf7daaa87be2442dc27e2d184637563293e8a28
thịnh <ngocthinhdp@gmail.com>
2025-01-04 12:02:41 +0700

    Improve clear chat / apps enable on chat. Fix clearing chat on Omi

--- app/lib/pages/home/widgets/chat_apps_dropdown_widget.dart
+++ app/lib/pages/home/widgets/chat_apps_dropdown_widget.dart
@@ -7,6 +7,7 @@ import 'package:friend_private/providers/app_provider.dart';
 import 'package:friend_private/providers/home_provider.dart';
 import 'package:friend_private/providers/message_provider.dart';
 import 'package:friend_private/utils/analytics/mixpanel.dart';
+import 'package:friend_private/widgets/dialog.dart';
 import 'package:provider/provider.dart';
 
 class ChatAppsDropdownWidget extends StatelessWidget {
@@ -29,30 +30,75 @@ class ChatAppsDropdownWidget extends StatelessWidget {
         return child!;
       },
       child: Consumer<AppProvider>(builder: (context, provider, child) {
+        var selectedApp = provider.apps.firstWhereOrNull((app) => app.id == provider.selectedChatAppId);
         return Padding(
           padding: const EdgeInsets.only(left: 0),
           child: Container(
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: PopupMenuButton<String>(
-              icon: Container(
-                child: Text("yoh"),
+              iconSize: 132,
+              icon: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  selectedApp != null ? _getAppAvatar(selectedApp) : _getOmiAvatar(),
+                  const SizedBox(width: 8),
+                  Container(
+                    constraints: const BoxConstraints(
+                      maxWidth: 68, // 100-32
+                    ),
+                    child: Text(
+                      selectedApp != null ? selectedApp.name : "Omi",
+                      style: const TextStyle(color: Colors.white, fontSize: 16),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  const SizedBox(
+                    width: 24,
+                    child: Icon(Icons.keyboard_arrow_down, color: Colors.white60, size: 16),
+                  ),
+                ],
               ),
               constraints: const BoxConstraints(
                 minWidth: 250.0,
                 maxWidth: 250.0,
+                maxHeight: 350.0,
               ),
-              offset: Offset((MediaQuery.sizeOf(context).width - 250) / 2, 48),
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(16))),
-              onSelected: (s) async {
-                if ((s == 'no_selected' && provider.apps.where((p) => p.enabled).isEmpty) || s == 'enable') {
-                  // routeToPage(context, const AppsPage(filterChatOnly: true));
+              offset: Offset((MediaQuery.sizeOf(context).width - 250) / 2 / MediaQuery.devicePixelRatioOf(context), 96),
+              shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(16))),
+              onSelected: (String? val) async {
+                if (val == null || val == provider.selectedChatAppId) {
+                  return;
+                }
+
+                // clear chat
+                if (val == 'clear_chat') {
+                  showDialog(
+                    context: context,
+                    builder: (ctx) {
+                      return getDialog(context, () {
+                        Navigator.of(context).pop();
+                      }, () {
+                        context.read<MessageProvider>().clearChat();
+                        Navigator.of(context).pop();
+                      }, "Clear Chat?", "Are you sure you want to clear the chat? This action cannot be undone.");
+                    },
+                  );
+                  return;
+                }
+
+                // enable apps
+                if (val == 'enable') {
                   MixpanelManager().pageOpened('Chat Apps');
                   context.read<HomeProvider>().setIndex(2);
                   controller?.animateToPage(2, duration: const Duration(milliseconds: 200), curve: Curves.easeInOut);
                   return;
                 }
-                if (s == null || s == provider.selectedChatAppId) return;
-                provider.setSelectedChatAppId(s);
+
+                // select app by id
+                provider.setSelectedChatAppId(val);
                 await context.read<MessageProvider>().refreshMessages(dropdownSelected: true);
                 var app = provider.getSelectedApp();
                 if (context.read<MessageProvider>().messages.isEmpty) {
@@ -70,42 +116,67 @@ class ChatAppsDropdownWidget extends StatelessWidget {
     );
   }
 
+  _getAppAvatar(App app) {
+    return CachedNetworkImage(
+      imageUrl: app.getImageUrl(),
+      imageBuilder: (context, imageProvider) {
+        return CircleAvatar(
+          backgroundColor: Colors.white,
+          radius: 12,
+          backgroundImage: imageProvider,
+        );
+      },
+      errorWidget: (context, url, error) {
+        return const CircleAvatar(
+          backgroundColor: Colors.white,
+          radius: 12,
+          child: Icon(Icons.error_outline_rounded),
+        );
+      },
+      progressIndicatorBuilder: (context, url, progress) => CircleAvatar(
+        backgroundColor: Colors.white,
+        radius: 12,
+        child: CircularProgressIndicator(
+          value: progress.progress,
+          valueColor: const AlwaysStoppedAnimation<Color>(Colors.white),
+        ),
+      ),
+    );
+  }
+
+  _getOmiAvatar() {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(Assets.images.background.path),
+          fit: BoxFit.cover,
+        ),
+        borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+      ),
+      height: 24,
+      width: 24,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Image.asset(
+            Assets.images.herologo.path,
+            height: 16,
+            width: 16,
+          ),
+        ],
+      ),
+    );
+  }
+
   _getAppsDropdownItems(BuildContext context, AppProvider provider) {
     var selectedApp = provider.apps.firstWhereOrNull((app) => app.id == provider.selectedChatAppId);
-    var selectedAppName = selectedApp != null ? selectedApp.name : "Omi";
     var items = [
-          PopupMenuItem<String>(
-            value: '#',
-            height: 40,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 32),
-                  child: Text(
-                    selectedAppName.length > 18
-                        ? 'Chat with ' + '${selectedAppName.substring(0, 18)}...'
-                        : 'Chat with ' + selectedAppName,
-                    style: const TextStyle(color: Colors.white60, fontSize: 12),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          PopupMenuItem<String>(
-            height: 1,
-            child: Divider(
-              height: 1,
-            ),
-          ),
-          PopupMenuItem<String>(
+          const PopupMenuItem<String>(
             height: 40,
             value: 'clear_chat',
             child: Padding(
-              padding: const EdgeInsets.only(left: 32),
-              child: const Row(
+              padding: EdgeInsets.only(left: 32),
+              child: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -120,7 +191,7 @@ class ChatAppsDropdownWidget extends StatelessWidget {
           )
         ] +
         [
-          PopupMenuItem<String>(
+          const PopupMenuItem<String>(
             height: 1,
             child: Divider(
               height: 1,
@@ -148,7 +219,7 @@ class ChatAppsDropdownWidget extends StatelessWidget {
                         Text('Enable Apps', style: TextStyle(color: Colors.white, fontSize: 16)),
                         SizedBox(
                           width: 24,
-                          child: Icon(Icons.apps, color: Colors.white, size: 16),
+                          child: Icon(Icons.apps, color: Colors.white60, size: 16),
                         ),
                       ],
                     ),
@@ -171,34 +242,27 @@ class ChatAppsDropdownWidget extends StatelessWidget {
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(Assets.images.background.path),
-                      fit: BoxFit.cover,
-                    ),
-                    borderRadius: const BorderRadius.all(Radius.circular(16.0)),
-                  ),
-                  height: 24,
-                  width: 24,
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Image.asset(
-                        Assets.images.herologo.path,
-                        height: 16,
-                        width: 16,
-                      ),
-                    ],
-                  ),
-                ),
+                _getOmiAvatar(),
                 const SizedBox(width: 10),
-                Text(
-                  'Omi',
-                  style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 16),
+                Expanded(
+                  child: Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Omi",
+                          style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 16),
+                        ),
+                        selectedApp == null
+                            ? const SizedBox(
+                                width: 24,
+                                child: Icon(Icons.check, color: Colors.white60, size: 16),
+                              )
+                            : const SizedBox.shrink(),
+                      ],
+                    ),
+                  ),
                 ),
-                // const SizedBox(width: 40),
-                // Icon(Icons.arrow_drop_down, color: Colors.white, size: 24)
               ],
             ),
           )
@@ -211,35 +275,26 @@ class ChatAppsDropdownWidget extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.end,
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                CachedNetworkImage(
-                  imageUrl: app.getImageUrl(),
-                  imageBuilder: (context, imageProvider) {
-                    return CircleAvatar(
-                      backgroundColor: Colors.white,
-                      radius: 12,
-                      backgroundImage: imageProvider,
-                    );
-                  },
-                  errorWidget: (context, url, error) {
-                    return const CircleAvatar(
-                      backgroundColor: Colors.white,
-                      radius: 12,
-                      child: Icon(Icons.error_outline_rounded),
-                    );
-                  },
-                  progressIndicatorBuilder: (context, url, progress) => CircleAvatar(
-                    backgroundColor: Colors.white,
-                    radius: 12,
-                    child: CircularProgressIndicator(
-                      value: progress.progress,
-                      valueColor: const AlwaysStoppedAnimation<Color>(Colors.white),
+                _getAppAvatar(app),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          app.name.length > 18 ? '${app.name.substring(0, 15)}...' : app.name,
+                          style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 16),
+                        ),
+                        selectedApp?.id == app.id
+                            ? const SizedBox(
+                                width: 24,
+                                child: Icon(Icons.check, color: Colors.white60, size: 16),
+                              )
+                            : const SizedBox.shrink(),
+                      ],
                     ),
                   ),
-                ),
-                const SizedBox(width: 8),
-                Text(
-                  app.name.length > 18 ? '${app.name.substring(0, 18)}...' : app.name + ' ' * (18 - app.name.length),
-                  style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 16),
                 ),
               ],
             ),
