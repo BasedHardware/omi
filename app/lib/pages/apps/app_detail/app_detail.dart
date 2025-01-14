9400164fc9474f33a59ad21e83e1e2e1a2364b37
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2025-01-14 11:27:19 +0530

    move analytics widget

--- app/lib/pages/apps/app_detail/app_detail.dart
+++ app/lib/pages/apps/app_detail/app_detail.dart
@@ -8,6 +8,7 @@ import 'package:friend_private/backend/preferences.dart';
 import 'package:friend_private/gen/assets.gen.dart';
 import 'package:friend_private/pages/apps/app_detail/reviews_list_page.dart';
 import 'package:friend_private/pages/apps/app_detail/widgets/add_review_widget.dart';
+import 'package:friend_private/pages/apps/app_detail/widgets/app_analytics_widget.dart';
 import 'package:friend_private/pages/apps/markdown_viewer.dart';
 import 'package:friend_private/pages/apps/providers/add_app_provider.dart';
 import 'package:friend_private/providers/app_provider.dart';
@@ -727,64 +728,7 @@ class _AppDetailPageState extends State<AppDetailPage> {
                   : const SizedBox.shrink(),
               // isIntegration ? const SizedBox(height: 16) : const SizedBox.shrink(),
               // widget.plugin.worksExternally() ? const SizedBox(height: 16) : const SizedBox.shrink(),
-              app.private
-                  ? const SizedBox.shrink()
-                  : Container(
-                      width: double.infinity,
-                      padding: const EdgeInsets.all(16.0),
-                      margin: const EdgeInsets.only(left: 8.0, right: 8.0, top: 12, bottom: 6),
-                      decoration: BoxDecoration(
-                        color: Colors.grey.shade900,
-                        borderRadius: BorderRadius.circular(16.0),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text('App Analytics', style: TextStyle(color: Colors.white, fontSize: 16)),
-                          const SizedBox(height: 16),
-                          Row(
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Skeleton.shade(child: SvgPicture.asset(Assets.images.icChart, width: 20)),
-                                      const SizedBox(width: 8),
-                                      Text(
-                                        usageCount.toString(),
-                                        style: const TextStyle(color: Colors.white, fontSize: 30),
-                                      ),
-                                    ],
-                                  ),
-                                  const SizedBox(height: 6),
-                                  Text('Times Used', style: TextStyle(color: Colors.grey.shade300, fontSize: 14)),
-                                ],
-                              ),
-                              const Spacer(flex: 2),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Skeleton.shade(child: SvgPicture.asset(Assets.images.icDollar, width: 20)),
-                                      const SizedBox(width: 8),
-                                      Text(
-                                        "\$$moneyMade",
-                                        style: const TextStyle(color: Colors.white, fontSize: 28),
-                                      ),
-                                    ],
-                                  ),
-                                  const SizedBox(height: 6),
-                                  Text('Money Earned', style: TextStyle(color: Colors.grey.shade300, fontSize: 14)),
-                                ],
-                              ),
-                              const Spacer(flex: 2),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
+              app.private ? const SizedBox.shrink() : AppAnalyticsWidget(usageCount: usageCount, moneyMade: moneyMade),
               const SizedBox(height: 60),
             ],
           ),
