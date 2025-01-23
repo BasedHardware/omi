7551c216165069abad8242506467c2fd97b75f56
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2025-01-23 20:21:19 +0530

    file handler widget for user message

--- app/lib/pages/chat/widgets/user_message.dart
+++ app/lib/pages/chat/widgets/user_message.dart
@@ -1,6 +1,6 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:friend_private/backend/schema/message.dart';
+import 'package:friend_private/pages/chat/widgets/files_handler_widget.dart';
 import 'package:friend_private/widgets/extensions/string.dart';
 import 'package:friend_private/utils/other/temp.dart';
 
@@ -26,56 +26,7 @@ class HumanMessage extends StatelessWidget {
               ),
             ),
           ),
-          message.files.isNotEmpty
-              ? Padding(
-                  padding: const EdgeInsets.only(bottom: 4.0),
-                  child: message.files.first.mimeTypeToFileType() != 'image'
-                      ? CachedNetworkImage(
-                          imageUrl: message.files.first.thumbnail ?? '',
-                          imageBuilder: (context, imageProvider) => Container(
-                            decoration: BoxDecoration(
-                              color: Theme.of(context).primaryColor,
-                              borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-                            ),
-                            width: MediaQuery.sizeOf(context).width * 0.4,
-                            height: MediaQuery.sizeOf(context).width * 0.3,
-                          ),
-                          placeholder: (context, url) => const SizedBox(
-                            width: 48,
-                            height: 48,
-                            child: CircularProgressIndicator(
-                              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                            ),
-                          ),
-                          errorWidget: (context, url, error) => const Icon(Icons.error, color: Colors.white),
-                        )
-                      : Container(
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).primaryColor,
-                            borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-                          ),
-                          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
-                          width: MediaQuery.sizeOf(context).width * 0.5,
-                          child: Row(
-                            children: [
-                              const Icon(Icons.insert_drive_file, color: Colors.white),
-                              const SizedBox(width: 8),
-                              Column(
-                                children: [
-                                  Text(
-                                    message.files.first.name,
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                )
-              : Container(),
+          FilesHandlerWidget(message: message),
           Wrap(
             alignment: WrapAlignment.end,
             children: [
