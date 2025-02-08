f0dcfa2eef48458c9b468a9ca90910e084066ad4
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2025-02-08 18:42:10 +0530

    paypal flow imporvements

--- app/lib/pages/payments/payment_method_provider.dart
+++ app/lib/pages/payments/payment_method_provider.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:friend_private/backend/http/api/payments.dart';
 import 'package:friend_private/utils/alerts/app_snackbar.dart';
+
+import 'models/payment_method_config.dart';
 
 enum PaymentMethodType {
   stripe,
@@ -27,35 +30,55 @@ PaymentConnectionState getPaymentConnectionState(String state) {
 class PaymentMethodProvider extends ChangeNotifier {
   PaymentMethodType? _activeMethod;
   bool _isStripePolling = false;
+  bool _isLoading = false;
   PaymentConnectionState _stripeConnectionState = PaymentConnectionState.notConnected;
   PaymentConnectionState _payPalConnectionState = PaymentConnectionState.notConnected;
+
+  PayPalDetails? paypalDetails;
 
   PaymentMethodType? get activeMethod => _activeMethod;
   bool get isStripeConnected => _stripeConnectionState == PaymentConnectionState.connected;
   bool get isStripePolling => _isStripePolling;
   bool get isPayPalConnected => _payPalConnectionState == PaymentConnectionState.connected;
+  bool get isLoading => _isLoading;
 
-  void setActiveMethod(PaymentMethodType? method) {
-    // if (method == null) {
-    //   AppSnackbar.showSnackbarError('You must have at least one payment method active and connected.');
-    //   return;
-    // }
-    if (_activeMethod != method) {
+  void setActiveMethod(PaymentMethodType method) async {
+    _isLoading = true;
+    var res = await setDefaultPaymentMethod(method.name);
+    _isLoading = false;
+    if (res) {
       _activeMethod = method;
       notifyListeners();
+    } else {
+      AppSnackbar.showSnackbarError('Failed to set default payment method. Please try again later.');
     }
   }
 
   Future getPaymentMethodsStatus() async {
+    _isLoading = true;
+    notifyListeners();
     var res = await fetchPaymentMethodsStatus();
+    _isLoading = false;
     if (res != null) {
       _payPalConnectionState = getPaymentConnectionState(res['paypal']);
       _stripeConnectionState = getPaymentConnectionState(res['stripe']);
+      if (_payPalConnectionState == PaymentConnectionState.connected) {
+        getPayPalDetails();
+      }
       if (res['default'] != null) {
         _activeMethod = res['default'] == 'stripe' ? PaymentMethodType.stripe : PaymentMethodType.paypal;
       } else {
         _activeMethod = null;
       }
+    }
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  Future getPayPalDetails() async {
+    var res = await fetchPayPalDetails();
+    if (res != null) {
+      paypalDetails = res;
       notifyListeners();
     }
   }
@@ -78,9 +101,11 @@ class PaymentMethodProvider extends ChangeNotifier {
   void startStripePolling() {
     _isStripePolling = true;
     checkStripeConnectionStatus();
-    if (!isStripeConnected) {
+    if (_isStripePolling && _stripeConnectionState != PaymentConnectionState.connected) {
       Future.delayed(const Duration(seconds: 5), () {
-        startStripePolling();
+        if (_isStripePolling) {
+          startStripePolling();
+        }
       });
     } else {
       _isStripePolling = false;
