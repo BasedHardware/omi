9ab7438776fb94b00690f13b3222a3b777151a79
smian1 <smian1@gmail.com>
2025-02-25 18:11:40 -0800

    Add Twitter image domain to Next.js image configuration.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -23,6 +23,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'storage.googleapis.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+      },
     ],
   },
   async headers() {
