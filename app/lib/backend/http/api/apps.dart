6d931a0d5d6dcb0dd2dae17b5162d6b66998e734
tiagoefreitas <tiagofreitas87@gmail.com>
2025-03-10 11:59:38 +0000

    revert

--- app/lib/backend/http/api/apps.dart
+++ app/lib/backend/http/api/apps.dart
@@ -7,8 +7,9 @@ import 'package:friend_private/backend/http/shared.dart';
 import 'package:friend_private/backend/preferences.dart';
 import 'package:friend_private/backend/schema/app.dart';
 import 'package:friend_private/env/env.dart';
-import 'package:http/http.dart' as http;
 import 'package:instabug_flutter/instabug_flutter.dart';
+
+import 'package:http/http.dart' as http;
 import 'package:path/path.dart';
 
 Future<List<App>> retrieveApps() async {
