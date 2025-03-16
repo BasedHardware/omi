1a487295a2c213b53df9e54fc64dc9c945641ea1
thịnh <ngocthinhdp@gmail.com>
2025-03-16 17:42:03 +0700

    Modeling the twitter* model; Creating facts right on verifing owner-ship; Migrating facts; Chat with Omi instead of your clone

--- app/lib/pages/chat/clone_chat_page.dart
+++ app/lib/pages/chat/clone_chat_page.dart
@@ -35,7 +35,7 @@ class CloneChatPageState extends State<CloneChatPage> {
         var appProvider = Provider.of<AppProvider>(context, listen: false);
         SharedPreferencesUtil().appsList = [selectedApp];
         appProvider.setApps();
-        appProvider.setSelectedChatAppId(selectedApp.id);
+        appProvider.setSelectedChatAppId(null);
         if (!selectedApp.enabled) {
           await appProvider.toggleApp(selectedApp.id, true, null);
         }
