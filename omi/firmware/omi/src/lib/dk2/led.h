cc2c27ca2935c8ed8d146c5cd900a6915460d9ac
Thinh <ngocthinhdp@gmail.com>
2025-04-23 17:55:51 +0700

    Add support led, battery, charger to the omi cv1 firmrware (#2254)

--- omi/firmware/omi/src/lib/dk2/led.h
+++ omi/firmware/omi/src/lib/dk2/led.h
@@ -4,9 +4,9 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
-static const struct gpio_dt_spec led_red = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
-static const struct gpio_dt_spec led_green = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
-static const struct gpio_dt_spec led_blue = GPIO_DT_SPEC_GET(DT_ALIAS(led2), gpios);
+extern const struct gpio_dt_spec led_red;
+extern const struct gpio_dt_spec led_green;
+extern const struct gpio_dt_spec led_blue;
 
 /**
  * @brief Initialize the LEDs
