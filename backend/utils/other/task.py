5bf69a4b72e36126f15ed54b824ee89fa8e6bbdc
nquanq <74983280+nquang29@users.noreply.github.com>
2025-04-28 20:18:51 +0700

    fix: task exception was never retrieved in transcribe (#2184)

---
+++ backend/utils/other/task.py
@@ -0,0 +1,8 @@
+import asyncio
+
+def safe_create_task(t):
+    task = asyncio.create_task(t)
+    task.add_done_callback(
+        lambda l: print("Unhandled exception in background task:", l.exception()) if l.exception() else None
+    )
+    return task
