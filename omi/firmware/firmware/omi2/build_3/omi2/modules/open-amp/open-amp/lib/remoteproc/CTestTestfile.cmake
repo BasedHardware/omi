03399dd7d2428b7d788de0d45dacd9986b509a35
Thinh <ngocthinhdp@gmail.com>
2025-04-28 15:37:41 +0700

    Delete omi/firmware/firmware/omi2 directory (#2271)

--- omi/firmware/firmware/omi2/build_3/omi2/modules/open-amp/open-amp/lib/remoteproc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/nordic/ncs/v2.9.0/modules/lib/open-amp/open-amp/lib/remoteproc
-# Build directory: /Users/nik/projects/omi/omi/firmware/firmware/omi2/build_3/omi2/modules/open-amp/open-amp/lib/remoteproc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- omi/firmware/firmware/omi2/build_3/omi2/modules/open-amp/open-amp/lib/virtio/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/nordic/ncs/v2.9.0/modules/lib/open-amp/open-amp/lib/virtio
-# Build directory: /Users/nik/projects/omi/omi/firmware/firmware/omi2/build_3/omi2/modules/open-amp/open-amp/lib/virtio
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
