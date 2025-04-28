66e121f3825bf356700e702161a9b94c8af8164e
Thinh <ngocthinhdp@gmail.com>
2025-04-28 15:37:24 +0700

    Delete omi/firmware/firmware/omi2 directory

--- omi/firmware/firmware/omi2/build_2/ipc_radio/modules/libmetal/libmetal/lib/system/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/nordic/ncs/v2.9.0/modules/hal/libmetal/libmetal/lib/system
-# Build directory: /Users/nik/projects/omi/omi/firmware/firmware/omi2/build_2/ipc_radio/modules/libmetal/libmetal/lib/system
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("zephyr")
