38464b6852b3fa06eba89cbd71dbd26b7689a8a7
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2025-05-03 13:58:08 -0700

    mcp starting point

---
+++ mcp/src/mcp_server_omi/__init__.py
@@ -0,0 +1,26 @@
+import click
+from pathlib import Path
+import logging
+import sys
+from .server import serve
+
+
+@click.command()
+@click.option("--uid", "-u", type=str, help="User ID")
+@click.option("-v", "--verbose", count=True)
+def main(uid: str | None, verbose: bool) -> None:
+    """MCP Omi Server - Omi functionality for MCP"""
+    import asyncio
+
+    logging_level = logging.WARN
+    if verbose == 1:
+        logging_level = logging.INFO
+    elif verbose >= 2:
+        logging_level = logging.DEBUG
+
+    logging.basicConfig(level=logging_level, stream=sys.stderr)
+    asyncio.run(serve(uid))
+
+
+if __name__ == "__main__":
+    main()
