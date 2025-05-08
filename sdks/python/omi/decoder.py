951251b2ca93fad8aef923fa3bd695032dd52659
Ankush Malaker <43288948+AnkushMalaker@users.noreply.github.com>
2025-05-08 15:59:42 +0530

    update the decoder to account for the stupid behaviour

--- sdks/python/omi/decoder.py
+++ sdks/python/omi/decoder.py
@@ -1,15 +1,19 @@
 from opuslib import Decoder
 
+
 class OmiOpusDecoder:
     def __init__(self):
         self.decoder = Decoder(16000, 1)  # 16kHz mono
 
-    def decode_packet(self, data):
+    def decode_packet(self, data: bytes, strip_header: bool = True):
         if len(data) <= 3:
             return b''
 
         # Remove 3-byte header
-        clean_data = bytes(data[3:])
+        if strip_header:
+            clean_data = bytes(data[3:])
+        else:
+            clean_data = data
 
         # Decode Opus to PCM 16-bit
         try:
