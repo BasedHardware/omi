b9ddbb7f75db1b515b2f3267acaaa5e2ac69daec
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2025-06-05 15:06:08 -0700

    google auth on windows and fixes and cleanup

--- app/lib/utils/platform/platform_service.dart
+++ app/lib/utils/platform/platform_service.dart
@@ -3,11 +3,13 @@ import 'dart:io';
 /// A utility class to handle platform-specific service availability
 class PlatformService {
   static bool get isMacOS => Platform.isMacOS;
-  static bool get isAnalyticsSupported => !Platform.isMacOS;
-  static bool get isNotificationSupported => !Platform.isMacOS;
-  static bool get isIntercomSupported => !Platform.isMacOS;
-  static bool get isMixpanelSupported => !Platform.isMacOS;
-  static bool get isInstabugSupported => !Platform.isMacOS;
+  static bool get isWindows => Platform.isWindows;
+  static bool get isDesktop => isWindows || isMacOS;
+  static bool get isAnalyticsSupported => !(Platform.isMacOS || Platform.isWindows);
+  static bool get isNotificationSupported => !(Platform.isMacOS || Platform.isWindows);
+  static bool get isIntercomSupported => !(Platform.isMacOS || Platform.isWindows);
+  static bool get isMixpanelSupported => !(Platform.isMacOS || Platform.isWindows);
+  static bool get isInstabugSupported => !(Platform.isMacOS || Platform.isWindows);
 
   /// Execute a function only if the platform supports it
   static T? executeIfSupported<T>(bool isSupported, T Function() function, {T? fallback}) {
