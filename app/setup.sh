9157b4ea399e73d16b07b9271cc31cbf8801852c
Mohammed Mohsin <mdmohsin_1@hotmail.com>
2025-06-30 07:54:53 +0530

    update setup script to support macos

--- app/setup.sh
+++ app/setup.sh
@@ -53,17 +53,20 @@ API_BASE_URL=https://backend-dt5lrfkkoa-uc.a.run.app/
 # Setup Firebase with prebuilt configs
 ######################################
 function setup_firebase() {
-  mkdir -p android/app/src/dev/ ios/Config/Dev/ ios/Runner/
+  mkdir -p android/app/src/dev/ ios/Config/Dev/ ios/Runner/ macos/ macos/Config/Dev
   cp setup/prebuilt/firebase_options.dart lib/firebase_options_dev.dart
   cp setup/prebuilt/google-services.json android/app/src/dev/
   cp setup/prebuilt/GoogleService-Info.plist ios/Config/Dev/
   cp setup/prebuilt/GoogleService-Info.plist ios/Runner/
+  cp setup/prebuilt/GoogleService-Info.plist macos/
+  cp setup/prebuilt/GoogleService-Info.plist macos/Config/Dev/
 
   # Warn: Mocking, should remove
-  mkdir -p android/app/src/prod/ ios/Config/Prod/
+  mkdir -p android/app/src/prod/ ios/Config/Prod/ macos/Config/Prod
   cp setup/prebuilt/firebase_options.dart lib/firebase_options_prod.dart
   cp setup/prebuilt/google-services.json android/app/src/prod/
   cp setup/prebuilt/GoogleService-Info.plist ios/Config/Prod/
+  cp setup/prebuilt/GoogleService-Info.plist macos/Config/Prod/
 }
 
 ##########################################
@@ -72,27 +75,33 @@ function setup_firebase() {
 function setup_firebase_with_service_account() {
   dart pub global activate flutterfire_cli
   flutterfire config \
-    --platforms="android,ios,web" \
+    --platforms="android,ios,macos,web" \
     --out=lib/firebase_options_dev.dart \
     --ios-bundle-id=com.friend-app-with-wearable.ios12.development \
+    --macos-bundle-id=com.friend-app-with-wearable.macos12.development \
     --android-app-id=com.friend.ios.dev \
     --android-out=android/app/src/dev/  \
     --ios-out=ios/Config/Dev/ \
+    --macos-out=macos/Config/Dev/ \
     --service-account="$FIREBASE_SERVICE_ACCOUNT_KEY" \
     --project="based-hardware-dev" \
     --ios-target="Runner" \
+    --macos-target="Runner" \
     --yes
 
   flutterfire config \
-    --platforms="android,ios,web" \
+    --platforms="android,ios,macos,web" \
     --out=lib/firebase_options_prod.dart \
     --ios-bundle-id=com.friend-app-with-wearable.ios12 \
+    --macos-bundle-id=com.friend-app-with-wearable.macos12 \
     --android-app-id=com.friend.ios.dev \
     --android-out=android/app/src/prod/ \
     --ios-out=ios/Config/Prod/ \
+    --macos-out=macos/Config/Prod/ \
     --service-account="$FIREBASE_SERVICE_ACCOUNT_KEY" \
     --project="based-hardware-dev" \
     --ios-target="Runner" \
+    --macos-target="Runner" \
     --yes
 }
 
