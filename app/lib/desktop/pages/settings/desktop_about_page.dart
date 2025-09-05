2e50e7296e7fb1fcf6dc8acdce04ea7eca8ab10d
Krushna Kanta Rout <129386740+krushnarout@users.noreply.github.com>
2025-09-06 01:23:02 +0530

    fix: discord invite link

--- app/lib/desktop/pages/settings/desktop_about_page.dart
+++ app/lib/desktop/pages/settings/desktop_about_page.dart
@@ -62,11 +62,11 @@ class _DesktopAboutOmiPageState extends State<DesktopAboutOmiPage> {
             ListTile(
               contentPadding: const EdgeInsets.fromLTRB(4, 0, 24, 0),
               title: const Text('Join the community!', style: TextStyle(color: Colors.white)),
-              subtitle: const Text('7000+ members and counting.', style: TextStyle(color: Colors.white70)),
+              subtitle: const Text('8000+ members and counting.', style: TextStyle(color: Colors.white70)),
               trailing: const Icon(Icons.discord, color: Colors.purple, size: 20),
               onTap: () {
                 MixpanelManager().pageOpened('About Join Discord');
-                launchUrl(Uri.parse('https://discord.gg/omi'));
+                launchUrl(Uri.parse('http://discord.omi.me'));
               },
             ),
           ],
