4b7db9ecd00cbef02d7b16a45898ffe5e594e439
Thinh <ngocthinhdp@gmail.com>
2025-09-13 06:47:27 +0700

    Add support HEAD to /health check (#2985)

--- backend/routers/other.py
+++ backend/routers/other.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.get("/v1/health")
+@router.api_route("/v1/health", methods=["GET", "HEAD"])
 async def health_check():
     """
     Health check endpoint.
