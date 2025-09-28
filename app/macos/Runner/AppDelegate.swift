9703c22acb64eb89b9de08d4ebaf39888823fb4c
Mohammed Mohsin <mdmohsin_1@hotmail.com>
2025-09-28 22:05:06 +0530

    set custom scheme and app links for auth

--- app/macos/Runner/AppDelegate.swift
+++ app/macos/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Cocoa
 import FlutterMacOS
+import app_links
 
 @main
 class AppDelegate: FlutterAppDelegate {
@@ -14,4 +15,25 @@ class AppDelegate: FlutterAppDelegate {
             }
         }
     }
+
+    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+      return true
+    }
+  
+    override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+      return true
+    }
+
+    public override func application(_ application: NSApplication,
+                                 continue userActivity: NSUserActivity,
+                                 restorationHandler: @escaping ([any NSUserActivityRestoring]) -> Void) -> Bool {
+
+    guard let url = AppLinks.shared.getUniversalLink(userActivity) else {
+      return false
+    }
+  
+    AppLinks.shared.handleLink(link: url.absoluteString)
+  
+    return false // Returning true will stop the propagation to other packages
+  }
 }
