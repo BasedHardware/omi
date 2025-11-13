8a963788948109b8254c38a175ae064d0f9f035e
Tu Nguyen <tunvemb@gmail.com>
2025-11-13 14:36:02 +0700

    add watchdog timer with a 30s timeout

---
+++ omi/firmware/omi/src/wdog_facade.h
@@ -0,0 +1,16 @@
+#ifndef _WDOG_FACADE_H_
+#define _WDOG_FACADE_H_
+
+/**
+ * @brief Feed (kick) the watchdog to prevent system reset.
+ */
+void watchdog_feed(void);
+
+/**
+ * @brief Initialize the watchdog timer.
+ *
+ * @return 0 on success, negative error code on failure to initialize.
+ */
+int watchdog_init(void);
+
+#endif /*_WDOG_FACADE_H_*/
