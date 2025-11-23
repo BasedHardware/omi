f424e99f8c19fb78229423b3404eae050f54c8da
Tu Nguyen <tunvemb@gmail.com>
2025-11-23 13:27:22 +0700

    refactor SD card to prevent conflict

--- omi/firmware/omi/src/lib/core/storage.c
+++ omi/firmware/omi/src/lib/core/storage.c
@@ -55,8 +55,6 @@ static ssize_t storage_read_characteristic(struct bt_conn *conn,
 K_THREAD_STACK_DEFINE(storage_stack, 4096);
 static struct k_thread storage_thread;
 
-extern uint8_t file_count;
-extern uint32_t file_num_array[MAX_AUDIO_FILES];
 void broadcast_storage_packet(struct k_work *work_item);
 
 static struct bt_gatt_attr storage_service_attr[] = {
@@ -103,9 +101,9 @@ static ssize_t storage_read_characteristic(struct bt_conn *conn,
 {
     k_msleep(10);
     uint32_t amount[2] = {0};
-    for (int i = 0; i < 2; i++) {
-        amount[i] = file_num_array[i];
-    }
+    amount[0] = get_file_size();
+    amount[1] = get_offset();
+    LOG_INF("Storage read requested: file size %u, offset %u", amount[0], amount[1]);
     ssize_t result = bt_gatt_attr_read(conn, attr, buf, len, offset, amount, 2 * sizeof(uint32_t));
     return result;
 }
@@ -122,7 +120,6 @@ static uint8_t current_packet_size = 0;
 static uint8_t tx_buffer_size = 0;
 static uint8_t stop_started = 0;
 static uint8_t delete_started = 0;
-static uint8_t current_read_num = 1;
 uint32_t remaining_length = 0;
 
 static int setup_storage_tx()
@@ -130,21 +127,8 @@ static int setup_storage_tx()
     transport_started = (uint8_t) 0;
     LOG_INF("about to transmit storage\n");
     k_msleep(1000);
-    int res = move_read_pointer(current_read_num);
-    if (res) {
-        LOG_INF("bad pointer");
-        transport_started = 0;
-        current_read_num = 1;
-        remaining_length = 0;
-        return -1;
-    }
 
-    LOG_INF("current read ptr %d", current_read_num);
-
-    uint32_t file_size = file_num_array[current_read_num - 1];
-    if (current_read_num == file_count) {
-        file_size = get_file_size(file_count);
-    }
+    uint32_t file_size = get_file_size();
 
     // Validate offset against file size
     if (offset >= file_size) {
@@ -156,7 +140,6 @@ static int setup_storage_tx()
 
     LOG_INF("remaining length: %d", remaining_length);
     LOG_INF("offset: %d", offset);
-    LOG_INF("file: %d", current_read_num);
     LOG_INF("file size: %d", file_size);
 
     return 0;
@@ -173,41 +156,30 @@ static uint8_t parse_storage_command(void *buf, uint16_t len)
     }
     const uint8_t command = ((uint8_t *) buf)[0];
     const uint8_t file_num = ((uint8_t *) buf)[1];
-    uint32_t size = 0;
+    uint32_t request_offset = 0;
     if (len == 6) {
-        size =
+        request_offset =
             ((uint8_t *) buf)[2] << 24 | ((uint8_t *) buf)[3] << 16 | ((uint8_t *) buf)[4] << 8 | ((uint8_t *) buf)[5];
     }
-    LOG_PRINTK("command successful: command: %d file: %d size: %d \n", command, file_num, size);
+    LOG_INF("command successful: command: %d file: %d offset: %d \n", command, file_num, request_offset);
 
-    if (file_num == 0) {
+    // only support file 1 for now
+    if (file_num != 1) {
         LOG_INF("invalid file count 0");
         return INVALID_FILE_SIZE;
     }
-    if (file_num > file_count) // invalid file count
-    {
-        LOG_INF("invalid file count");
-        return INVALID_FILE_SIZE;
-        // add audio all?
-    }
+
     if (command == READ_COMMAND) // read
     {
-        uint32_t temp = file_num_array[file_num - 1];
-        if (file_num == (file_count)) {
-            LOG_INF("file_count == final file");
-            offset = size - (size % SD_BLE_SIZE); // round down to nearest SD_BLE_SIZE
-            current_read_num = file_num;
-            transport_started = 1;
-        } else if (temp == 0) {
-            LOG_INF("file size is 0");
+        uint32_t file_size = get_file_size();
+        if (request_offset >= file_size) {
+            LOG_WRN("requested offset is too large");
+            return INVALID_FILE_SIZE;
+        } else if (file_size == 0) {
+            LOG_WRN("file size is 0");
             return ZERO_FILE_SIZE;
-        } else if (size > temp) {
-            LOG_INF("requested size is too large");
-            return 5;
         } else {
-            LOG_INF("valid command, setting up ");
-            offset = size - (size % SD_BLE_SIZE);
-            current_read_num = file_num;
+            offset = request_offset - (request_offset % SD_BLE_SIZE);
             transport_started = 1;
         }
     } else if (command == DELETE_COMMAND) {
@@ -248,32 +220,6 @@ static ssize_t storage_write_handler(struct bt_conn *conn,
     return len;
 }
 
-// static void write_to_gatt(struct bt_conn *conn)
-// {
-//     uint32_t id = packet_next_index++;
-//     index = 0;
-//     storage_write_buffer[0] = id & 0xFF;
-//     storage_write_buffer[1] = (id >> 8) & 0xFF;
-//     storage_write_buffer[2] = index;
-
-//     const uint32_t packet_size = MIN(remaining_length,OPUS_ENTRY_LENGTH);
-
-//     int r = read_audio_data(storage_write_buffer+FRAME_PREFIX_LENGTH,packet_size,offset);
-//     offset = offset + packet_size;
-
-//     index++;
-
-//     int err = bt_gatt_notify(conn, &storage_service.attrs[1], &storage_write_buffer,packet_size+FRAME_PREFIX_LENGTH);
-//     if (err)
-//     {
-//         LOG_PRINTK("error writing to gatt: %d\n",err);
-//     }
-//     else
-//     {
-//     remaining_length = remaining_length - OPUS_ENTRY_LENGTH;
-//     }
-// }
-
 static void write_to_gatt(struct bt_conn *conn)
 { // unsafe. designed for max speeds. udp?
 
@@ -293,7 +239,6 @@ static void write_to_gatt(struct bt_conn *conn)
     } else {
         remaining_length = remaining_length - packet_size; // FIX: Use packet_size, not SD_BLE_SIZE
     }
-    // LOG_PRINTK("wrote to gatt %d\n",err);
 }
 
 void storage_write(void)
@@ -302,15 +247,13 @@ void storage_write(void)
         struct bt_conn *conn = get_current_connection();
 
         if (transport_started) {
-            LOG_INF("transpor started in side : %d", transport_started);
+            LOG_INF("transport started in side : %d", transport_started);
             setup_storage_tx();
         }
         // probably prefer to implement using work orders for delete,nuke,etc...
         if (delete_started) {
             LOG_INF("delete:%d\n", delete_started);
-            int err = clear_audio_file(1);
-            offset = 0;
-            save_offset(offset);
+            int err = clear_audio_directory();
 
             if (err) {
                 LOG_PRINTK("error clearing\n");
@@ -325,7 +268,6 @@ void storage_write(void)
         }
         if (nuke_started) {
             clear_audio_directory();
-            save_offset(0);
             nuke_started = 0;
         }
         if (stop_started) {
@@ -334,10 +276,8 @@ void storage_write(void)
             save_offset(offset);
         }
         if (heartbeat_count == MAX_HEARTBEAT_FRAMES) {
-            LOG_PRINTK("no heartbeat sent\n");
+            LOG_INF("no heartbeat sent\n");
             save_offset(offset);
-            // k_yield();
-            // continue;
         }
 
         if (remaining_length > 0) {
@@ -352,8 +292,9 @@ void storage_write(void)
             // LOG_PRINTK("remaining length: %d\n",remaining_length);
 
             write_to_gatt(conn);
-            heartbeat_count = (heartbeat_count + 1) % (MAX_HEARTBEAT_FRAMES + 1);
 
+            heartbeat_count = (heartbeat_count + 1) % (MAX_HEARTBEAT_FRAMES + 1);
+            k_msleep(100);
             transport_started = 0;
             if (remaining_length == 0) {
                 if (stop_started) {
@@ -361,8 +302,10 @@ void storage_write(void)
                 } else {
                     LOG_PRINTK("done. attempting to download more files\n");
                     uint8_t stop_result[1] = {100};
+                    // TBD: why is this sleep needed?
+                    k_msleep(500);
                     int err = bt_gatt_notify(get_current_connection(), &storage_service.attrs[1], &stop_result, 1);
-                    k_sleep(K_MSEC(10));
+                    k_msleep(10);
                 }
             }
         }
