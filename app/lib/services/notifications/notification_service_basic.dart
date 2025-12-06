58f003de2b8dddc99ddb2eb8acb7742078881558
Krushna Kanta Rout <129386740+krushnarout@users.noreply.github.com>
2025-12-06 20:02:39 +0530

    fix check permission before sending notifications

--- app/lib/services/notifications/notification_service_basic.dart
+++ app/lib/services/notifications/notification_service_basic.dart
@@ -63,7 +63,11 @@ class _BasicNotificationService implements NotificationInterface {
     bool wakeUpScreen = false,
     NotificationSchedule? schedule,
     NotificationLayout layout = NotificationLayout.Default,
-  }) {
+  }) async {
+    final allowed = await _awesomeNotifications.isNotificationAllowed();
+    if (!allowed) {
+      return;
+    }
     _awesomeNotifications.createNotification(
       content: NotificationContent(
         id: id,
--- app/lib/services/notifications/notification_service_fcm.dart
+++ app/lib/services/notifications/notification_service_fcm.dart
@@ -80,7 +80,11 @@ class _FCMNotificationService implements NotificationInterface {
     bool wakeUpScreen = false,
     NotificationSchedule? schedule,
     NotificationLayout layout = NotificationLayout.Default,
-  }) {
+  }) async {
+    final allowed = await _awesomeNotifications.isNotificationAllowed();
+    if (!allowed) {
+      return;
+    }
     _awesomeNotifications.createNotification(
       content: NotificationContent(
         id: id,
