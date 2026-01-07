71992f48ea24f3850fbbf64cbdf0f06fe46b8008
Krushna Kanta Rout <129386740+krushnarout@users.noreply.github.com>
2026-01-07 21:53:39 +0530

    fix according to docs

--- app/android/app/src/main/java/com/friend/ios/MyApp.kt
+++ app/android/app/src/main/java/com/friend/ios/MyApp.kt
@@ -7,7 +7,7 @@ import io.maido.intercom.IntercomFlutterPlugin
 class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        Intercom.initialize(this,  BuildConfig.INTERCOM_APP_ID, BuildConfig.INTERCOM_ANDROID_API_KEY)
+        Intercom.initialize(this, BuildConfig.INTERCOM_ANDROID_API_KEY, BuildConfig.INTERCOM_APP_ID)
         IntercomFlutterPlugin.initSdk(this, appId = BuildConfig.INTERCOM_APP_ID, androidApiKey = BuildConfig.INTERCOM_ANDROID_API_KEY)
     }
 }
