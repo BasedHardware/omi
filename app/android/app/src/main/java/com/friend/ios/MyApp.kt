7b1badd78d63e52b55f18d083a292aebd412c6c3
Krushna Kanta Rout <129386740+krushnarout@users.noreply.github.com>
2026-01-07 21:43:03 +0530

    fix initialize intercom sdk in application class

--- app/android/app/src/main/java/com/friend/ios/MyApp.kt
+++ app/android/app/src/main/java/com/friend/ios/MyApp.kt
@@ -1,11 +1,13 @@
 package com.friend.ios
 
 import android.app.Application
+import io.intercom.android.sdk.Intercom
 import io.maido.intercom.IntercomFlutterPlugin
 
 class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        Intercom.initialize(this,  BuildConfig.INTERCOM_APP_ID, BuildConfig.INTERCOM_ANDROID_API_KEY)
         IntercomFlutterPlugin.initSdk(this, appId = BuildConfig.INTERCOM_APP_ID, androidApiKey = BuildConfig.INTERCOM_ANDROID_API_KEY)
     }
 }
