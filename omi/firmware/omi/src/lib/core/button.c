3e8fa010327d3869631ea2dcb4767d66579a520e
Tu Nguyen <tunvemb@gmail.com>
2026-01-27 15:16:43 +0700

    revert PR 4372 - long press to turn off is back

--- omi/firmware/omi/src/lib/core/button.c
+++ omi/firmware/omi/src/lib/core/button.c
@@ -152,7 +152,7 @@ static inline void notify_long_tap()
 
 #define TAP_THRESHOLD 300     // 300 ms for single tap
 #define DOUBLE_TAP_WINDOW 600 // 600 ms maximum for double-tap
-#define LONG_PRESS_TIME 1000  // 1000 ms for long press
+#define LONG_PRESS_TIME 3000  // 3000 ms for long press (power off)
 
 typedef enum {
     BUTTON_EVENT_NONE,
@@ -221,7 +221,7 @@ void check_button_level(struct k_work *work_item)
         LOG_INF("single tap detected\n");
         btn_last_event = event;
 
-        turnoff_all();
+        notify_tap();
     }
 
     // Double tap
@@ -235,7 +235,7 @@ void check_button_level(struct k_work *work_item)
     if (event == BUTTON_EVENT_LONG_PRESS && btn_last_event != BUTTON_EVENT_LONG_PRESS) {
         LOG_INF("long press detected\n");
         btn_last_event = event;
-        notify_long_tap();
+        turnoff_all();
     }
 
     // Releases, one time event
