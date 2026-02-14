ce72a823c6309dca4ad5483e90cc2c9cc18558b9
Barrett Jacobsen <github@bj.sent.com>
2026-02-14 16:39:14 -0600

    feat: add widget URL routing for complication tap actions

--- app/ios/omiWatchApp/omiwatchApp.swift
+++ app/ios/omiWatchApp/omiwatchApp.swift
@@ -1,10 +1,40 @@
 import SwiftUI
 
+enum AppLaunchMode: Equatable {
+    case home
+    case record
+    case deviceRecord
+    case ask
+}
+
 @main
 struct omiwatch_Watch_AppApp: App {
+    @State private var launchMode: AppLaunchMode = .home
+
     var body: some Scene {
         WindowGroup {
-            WatchRecorderView(viewModel: WatchAudioRecorderViewModel())
+            switch launchMode {
+            case .home:
+                WatchRecorderView(viewModel: WatchAudioRecorderViewModel())
+            case .record:
+                WatchRecorderView(viewModel: WatchAudioRecorderViewModel(), autoStartRecording: true)
+            case .deviceRecord:
+                WatchRecorderView(viewModel: WatchAudioRecorderViewModel(), autoStartDeviceRecording: true)
+            case .ask:
+                AskQuestionView()
+            }
+        }
+        .onOpenURL { url in
+            switch url.host {
+            case "record":
+                launchMode = .record
+            case "device-record":
+                launchMode = .deviceRecord
+            case "ask":
+                launchMode = .ask
+            default:
+                launchMode = .home
+            }
         }
     }
 }
