912af432aa4bbcdf4e0c6d17fb0d0381ee666894
Barrett Jacobsen <github@bj.sent.com>
2026-02-14 17:26:46 -0600

    chore: regenerate Pigeon Swift code with onDeviceRecordingRequested

--- app/ios/Runner/FlutterCommunicator.g.swift
+++ app/ios/Runner/FlutterCommunicator.g.swift
@@ -346,6 +346,7 @@ protocol WatchRecorderFlutterAPIProtocol {
   func onMicrophonePermissionResult(granted grantedArg: Bool, completion: @escaping (Result<Void, PigeonError>) -> Void)
   func onMainAppMicrophonePermissionResult(granted grantedArg: Bool, completion: @escaping (Result<Void, PigeonError>) -> Void)
   func onWatchBatteryUpdate(batteryLevel batteryLevelArg: Double, batteryState batteryStateArg: Int64, completion: @escaping (Result<Void, PigeonError>) -> Void)
+  func onDeviceRecordingRequested(completion: @escaping (Result<Void, PigeonError>) -> Void)
 }
 class WatchRecorderFlutterAPI: WatchRecorderFlutterAPIProtocol {
   private let binaryMessenger: FlutterBinaryMessenger
@@ -487,6 +488,24 @@ class WatchRecorderFlutterAPI: WatchRecorderFlutterAPIProtocol {
     let channelName: String = "dev.flutter.pigeon.watch.WatchRecorderFlutterAPI.onWatchBatteryUpdate\(messageChannelSuffix)"
     let channel = FlutterBasicMessageChannel(name: channelName, binaryMessenger: binaryMessenger, codec: codec)
     channel.sendMessage([batteryLevelArg, batteryStateArg] as [Any?]) { response in
+      guard let listResponse = response as? [Any?] else {
+        completion(.failure(createConnectionError(withChannelName: channelName)))
+        return
+      }
+      if listResponse.count > 1 {
+        let code: String = listResponse[0] as! String
+        let message: String? = nilOrValue(listResponse[1])
+        let details: String? = nilOrValue(listResponse[2])
+        completion(.failure(PigeonError(code: code, message: message, details: details)))
+      } else {
+        completion(.success(()))
+      }
+    }
+  }
+  func onDeviceRecordingRequested(completion: @escaping (Result<Void, PigeonError>) -> Void) {
+    let channelName: String = "dev.flutter.pigeon.watch.WatchRecorderFlutterAPI.onDeviceRecordingRequested\(messageChannelSuffix)"
+    let channel = FlutterBasicMessageChannel(name: channelName, binaryMessenger: binaryMessenger, codec: codec)
+    channel.sendMessage(nil) { response in
       guard let listResponse = response as? [Any?] else {
         completion(.failure(createConnectionError(withChannelName: channelName)))
         return
