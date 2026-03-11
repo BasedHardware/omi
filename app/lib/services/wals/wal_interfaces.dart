d7c8655fb82b314bfa09e3f07fc5be37525d7e97
Mohammed Mohsin <mdmohsin_1@hotmail.com>
2026-03-11 22:48:09 +0530

    feat(app): add deleteAllSynced and deleteAllPending to WalSyncBase interface

--- app/lib/services/wals/wal_interfaces.dart
+++ app/lib/services/wals/wal_interfaces.dart
@@ -68,6 +68,7 @@ abstract class LocalWalSync implements IWalSync {
   Future<void> addExternalWal(Wal wal);
   Future<List<Wal>> getAllWals();
   Future<void> deleteAllSyncedWals();
+  Future<void> deleteAllPendingWals();
   void onByteStream(List<int> value);
   void onBytesSync(List<int> value);
   Future onAudioCodecChanged(BleAudioCodec codec);
@@ -78,6 +79,7 @@ abstract class SDCardWalSync implements IWalSync {
   void setLocalSync(LocalWalSync localSync);
   void setDevice(BtDevice? device);
   Future<void> deleteAllSyncedWals();
+  Future<void> deleteAllPendingWals();
   bool get isSyncing;
   double get currentSpeedKBps;
 
@@ -96,5 +98,6 @@ abstract class FlashPageWalSync implements IWalSync {
   void setDevice(BtDevice? device);
   void setLocalSync(LocalWalSync localSync);
   Future<void> deleteAllSyncedWals();
+  Future<void> deleteAllPendingWals();
   bool get isSyncing;
 }
