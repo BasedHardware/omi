54472d50c491efa661459649bd81ffc474cef698
Aarav Garg <aaravgarg@users.noreply.github.com>
2026-03-19 18:10:46 +0000

    docs: simplify cleanup logic

--- app/lib/pages/settings/developer.dart
+++ app/lib/pages/settings/developer.dart
@@ -1905,8 +1905,8 @@ class _ManualFirmwareFlashPage extends StatefulWidget {
 }
 
 class _ManualFirmwareFlashPageState extends State<_ManualFirmwareFlashPage> with FirmwareMixin {
-  bool _confirmed = false; 
-  String? _error;
+  bool _confirmed = false;
+  String? _error; 
 
   @override
   void initState() {
