7f0b4811501d9319b07bccb7cd4d7c73d3dc4db3
Aarav Garg <aaravgarg@users.noreply.github.com>
2026-03-26 22:04:41 +0000

    test: tweak text styling

--- app/lib/pages/settings/developer.dart
+++ app/lib/pages/settings/developer.dart
@@ -1941,8 +1941,8 @@ class _ManualFirmwareFlashPageState extends State<_ManualFirmwareFlashPage> with
         backgroundColor: Colors.black,
         title: const Text('Flash Firmware', style: TextStyle(color: Colors.white)),
         iconTheme: const IconThemeData(color: Colors.white),
-        elevation: 0, 
-      ),
+        elevation: 0,
+      ), 
       body: Padding(
         padding: const EdgeInsets.all(24),
         child: Column(
