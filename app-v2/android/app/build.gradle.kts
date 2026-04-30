f5fb5664d32e44914e31f5a0dd0e899e543f1ded
Matheus Oliveira <matheus.oliveira@weconnectllc.com>
2026-04-30 14:45:08 -0400

    app-v2: align Android applicationId to legacy dev + apply google-services plugin

--- app-v2/android/app/build.gradle.kts
+++ app-v2/android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -20,8 +21,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.togodynamics.nooto_v2"
+        applicationId = "com.togodynamics.nooto.dev"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
