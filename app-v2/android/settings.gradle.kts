fbd5294e888673e523a8e7ebe61c726ee291af8b
Matheus Oliveira <matheus.oliveira@weconnectllc.com>
2026-04-30 14:45:08 -0400

    app-v2: declare google-services Gradle plugin

--- app-v2/android/settings.gradle.kts
+++ app-v2/android/settings.gradle.kts
@@ -21,6 +21,7 @@ plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.11.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 include(":app")
