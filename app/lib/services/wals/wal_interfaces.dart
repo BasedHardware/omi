cb37509296f711180bc8cfa33250912ab5507f87
Mohammed Mohsin <mdmohsin_1@hotmail.com>
2026-05-20 01:32:49 +0530

    feat(sync): export uploadLocalFilesV2/fetchSyncJobStatus from wal barrel

--- app/lib/services/wals/wal_interfaces.dart
+++ app/lib/services/wals/wal_interfaces.dart
@@ -5,7 +5,15 @@ import 'package:omi/services/audio_sources/audio_source.dart';
 import 'package:omi/services/wals/wal.dart';
 
 // Re-export for convenience
-export 'package:omi/backend/http/api/conversations.dart' show syncLocalFilesV2, SyncJobPollCallback;
+export 'package:omi/backend/http/api/conversations.dart'
+    show
+        syncLocalFilesV2,
+        SyncJobPollCallback,
+        uploadLocalFilesV2,
+        UploadFilesResult,
+        fetchSyncJobStatus,
+        SyncJobFetch,
+        SyncJobFetchOutcome;
 
 abstract class IWalSyncProgressListener {
   void onWalSyncedProgress(
