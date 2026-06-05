42629e91e78fed6728c799d8a855eb981982536d
THESOHAMDATTA <SOHAMKARANDE00@GMAIL.COM>
2026-06-05 23:52:03 +0530

    Update product name to Omi and price to \ in product banner types

--- web/frontend/src/app/components/product-banner/types.ts
+++ web/frontend/src/app/components/product-banner/types.ts
@@ -19,8 +19,8 @@ export interface CategoryBannerProps extends ProductBannerBase {
 }
 
 export const PRODUCT_INFO = {
-  name: 'OMI Necklace',
-  price: '$69.99',
+  name: 'Omi',
+  price: '$89',
   url: 'https://www.omi.me/products/omi-dev-kit-2?ref=omi_marketplace&utm_source=h.omi.me&utm_campaign=omi_marketplace_app_detail_page',
   shipping: 'Ships Worldwide',
   images: {
