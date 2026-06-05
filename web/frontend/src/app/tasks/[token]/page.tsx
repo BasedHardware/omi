47f142b24b5d9a37279eda55129342a7f7f2a2b9
THESOHAMDATTA <SOHAMKARANDE00@GMAIL.COM>
2026-06-06 00:42:08 +0530

    Delegate platform redirect logic to PRODUCT_CONFIG on tasks page

--- web/frontend/src/app/tasks/[token]/page.tsx
+++ web/frontend/src/app/tasks/[token]/page.tsx
@@ -1,5 +1,6 @@
 import getSharedTasks from '@/src/actions/tasks/get-shared-tasks';
 import envConfig from '@/src/constants/envConfig';
+import { PRODUCT_CONFIG } from '@/src/constants/product';
 import { Metadata, ResolvingMetadata } from 'next';
 import { headers } from 'next/headers';
 import Image from 'next/image';
@@ -61,19 +62,7 @@ export async function generateMetadata(
 }
 
 function getPlatformLink(userAgent: string, token: string) {
-  const isAndroid = /android/i.test(userAgent);
-  const isIOS = /iphone|ipad|ipod/i.test(userAgent);
-
-  // iOS: Use custom URL scheme because Universal Links don't trigger for same-domain navigation
-  // (user is already on h.omi.me, so tapping https://h.omi.me/... just reloads the page)
-  // Android: Use intent:// with fallback to Google Play if app not installed
-  return isAndroid
-    ? `intent://h.omi.me/tasks/${token}#Intent;scheme=https;package=com.friend.ios;S.browser_fallback_url=${encodeURIComponent(
-        'https://play.google.com/store/apps/details?id=com.friend.ios',
-      )};end`
-    : isIOS
-      ? `omi://h.omi.me/tasks/${token}`
-      : 'https://www.omi.me';
+  return PRODUCT_CONFIG.getPlatformLink(userAgent, token, 'tasks');
 }
 
 function formatDueDate(dateStr: string): string {
