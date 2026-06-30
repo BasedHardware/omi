758f919cf445d416a0c5ba030e673c1b30aef445
David Zhang <9387252+Git-on-my-level@users.noreply.github.com>
2026-06-30 07:48:29 +0700

    fix backend runtime config validation

--- backend/database/_client.py
+++ backend/database/_client.py
@@ -1,38 +1,16 @@
-import json
-import os
 from threading import Lock
 
 from google.cloud import firestore
 
 from database.document_ids import document_id_from_seed
+from utils.google_credentials import prepare_google_credentials
 
 _firestore_client = None
 _firestore_client_lock = Lock()
-_credentials_file_lock = Lock()
-_credentials_file_prepared = False
-
-
-def _prepare_service_account_credentials():
-    global _credentials_file_prepared
-
-    if _credentials_file_prepared:
-        return
-
-    with _credentials_file_lock:
-        if _credentials_file_prepared:
-            return
-
-        service_account_json = os.environ.get('SERVICE_ACCOUNT_JSON')
-        if service_account_json:
-            service_account_info = json.loads(service_account_json)
-            with open('google-credentials.json', 'w') as f:
-                json.dump(service_account_info, f)
-
-        _credentials_file_prepared = True
 
 
 def _build_firestore_client():
-    _prepare_service_account_credentials()
+    prepare_google_credentials()
     return firestore.Client()
 
 
