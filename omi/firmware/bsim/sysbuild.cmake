c7e2dde409e62c0951e0a8c0bf774423c43b8396
Max Carter 祁明思 <undivisible@vk.com>
2026-07-25 19:31:00 -0400

    fix(firmware): use Zephyr controller in BabbleSim
    Failure-Class: none

--- omi/firmware/bsim/sysbuild.cmake
+++ omi/firmware/bsim/sysbuild.cmake
@@ -8,5 +8,8 @@ ExternalZephyrProject_Add(
     BOARD ${SB_CONFIG_NET_CORE_BOARD}
 )
 
+set_config_bool(hci_ipc CONFIG_BT_LL_SOFTDEVICE n)
+set_config_bool(hci_ipc CONFIG_BT_LL_SW_SPLIT y)
+
 native_simulator_set_child_images(${DEFAULT_IMAGE} hci_ipc)
 native_simulator_set_final_executable(${DEFAULT_IMAGE})
