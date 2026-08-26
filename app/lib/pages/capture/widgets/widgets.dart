a2b4bd968afcebfc266b45b9190c71d605c70265
David Zhang <david.d.zhang@gmail.com>
2026-08-26 00:29:49 -0400

    fix(app): finish photo viewer consolidation

--- app/lib/pages/capture/widgets/widgets.dart
+++ app/lib/pages/capture/widgets/widgets.dart
@@ -16,7 +16,7 @@ import 'package:omi/providers/device_provider.dart';
 import 'package:omi/providers/home_provider.dart';
 import 'package:omi/utils/l10n_extensions.dart';
 import 'package:omi/utils/other/temp.dart';
-import 'package:omi/widgets/photo_viewer_page.dart';
+import 'package:omi/widgets/conversation_photo_image.dart';
 import 'package:omi/widgets/photos_grid.dart';
 import 'package:omi/widgets/transcript.dart';
 
